c355afffd600f7ad84f6d6f6c024fab486f3799f
thomaspfeiffer-git <thomaspfeiffer-git@users.noreply.github.com>
2015-04-03 18:24:59 +0200

    just writing code

--- projects/weather.py
+++ projects/weather.py
@@ -27,6 +27,7 @@ DS_TEMPOUTDOOR = "temp_outdoor"
 DS_HUMIINDOOR  = "humi_indoor"
 DS_HUMIOUTDOOR = "humi_outdoor"
 DS_AIRPRESSURE = "air_pressure"
+DS_TEMPCPU     = "temp_cpu"
 
 
 
@@ -65,34 +66,61 @@ def GetCPUTemperature():
 ################################################################################
 # Main #########################################################################
 def Main():
-   global state_led_big
    global bmp
 
-   i = 1
+   humi_indoor = ERROR
+
+   i = 1                         # outdoor #
    while (i <= 5):
       try:
          print("Try #{}".format(i))
-         t, h = dhtreader.read(22,pin_sensor_bcm)
+         temp_outdoor, humi_outdoor = dhtreader.read(22,pin_sensor_bcm) 
       except TypeError:
-         t = h = -99.99
+         temp_outdoor = humi_outdoor = ERROR
          i += 1
          continue
       break
 
-   t_bmp = bmp.readTemperature()
-   p_bmp = bmp.readPressure()
-   CPUTemp = GetCPUTemperature()
+   temp_indoor = bmp.readTemperature()  # indoor #
+   pressure    = bmp.readPressure()
+
+   temp_cpu    = GetCPUTemperature()
 
 # rrdtool update speed.rrd N:$speed
 
-#   rrd_writestring = "N:{}".format(
+
+# DS_TEMPINDOOR  = "temp_indoor"   # Besser: Hash mit {DS:...; Name: "..."}
+# DS_TEMPOUTDOOR = "temp_outdoor"
+# DS_HUMIINDOOR  = "humi_indoor"
+# DS_HUMIOUTDOOR = "humi_outdoor"
+# DS_AIRPRESSURE = "air_pressure"
+# DS_TEMPCPU     = "temp_cpu"
 
 
-   print("CPU Temperatur: {:.2f} °C".format(CPUTemp))
-   print("Temperatur DHT: {:.2f} °C".format(t))
-   print("Luftfeuchtigkeit DHT: {:.2f} %".format(h))
-   print("Temperatur BMP: {:.2f} °C".format(t_bmp))
-   print("Luftdruck BMP: {:.2f} hPa".format(p_bmp / 100.0))
+
+   rrd_template    = DS_TEMPINDOOR  + ":" + \
+                     DS_TEMPOUTDOOR + ":" + \
+                     DS_HUMIINDOOR  + ":" + \
+                     DS_HUMIOUTDOOR + ":" + \
+                     DS_AIRPRESSURE + ":" + \
+                     DS_TEMPCPU
+   rrd_writestring = "N:{:.2f}".format(temp_indoor)      + \
+                      ":{:.2f}".format(temp_outdoor)     + \
+                      ":{:.2f}".format(humi_indoor)      + \
+                      ":{:.2f}".format(humi_outdoor)     + \
+                      ":{:.2f}".format(pressure / 100.0) + \
+                      ":{:.2f}".format(temp_cpu)
+
+
+
+   print rrd_template
+   print rrd_writestring
+
+   print("CPU Temperatur: {:.2f} °C".format(temp_cpu))
+   print("Temperatur DHT: {:.2f} °C".format(temp_outdoor))
+   print("Luftfeuchtigkeit DHT: {:.2f} %".format(humi_outdoor))
+   print("Temperatur BMP: {:.2f} °C".format(temp_indoor))
+   print("Luftdruck BMP: {:.2f} hPa".format(pressure / 100.0))
 
 
 ################################################################################
