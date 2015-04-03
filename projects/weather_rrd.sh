915803c28f0ca8ced603f44a62b0ffdcfe94977e
thomaspfeiffer-git <thomaspfeiffer-git@users.noreply.github.com>
2015-04-03 07:10:17 +0200

    initial checkin

---
+++ projects/weather_rrd.sh
@@ -0,0 +1,21 @@
+#/bin/bash
+
+HEARTBEAT=650
+
+
+rrdtool create weather.rrd --step 300 \
+DS:temp_indoor:GAUGE:$HEARTBEAT:-40:80 \
+DS:temp_outdoor:GAUGE:$HEARTBEAT:-40:80 \
+DS:humi_indoor:GAUGE:$HEARTBEAT:0:100 \
+DS:humi_outdoor:GAUGE:$HEARTBEAT:0:100 \
+DS:air_pressure:GAUGE:$HEARTBEAT:900:1100 \
+DS:temp_3:GAUGE:$HEARTBEAT:-40:80 \
+DS:temp_4:GAUGE:$HEARTBEAT:-40:80 \
+DS:temp_cpu:GAUGE:$HEARTBEAT:-40:80 \
+RRA:AVERAGE:0.5:1:288 \
+RRA:AVERAGE:0.5:6:480 \
+RRA:AVERAGE:0.5:12:480 \
+RRA:AVERAGE:0.5:24:480 \
+RRA:AVERAGE:0.5:288:3650 \
+
+
