aee0c93f3ecf2744e7bc65a18ca1d16b45185c25
thomaspfeiffer-git <thomaspfeiffer-git@users.noreply.github.com>
2015-04-10 20:48:00 +0200

    refactoring: moved duplicate code to functions

--- projects/weather_graph.sh
+++ projects/weather_graph.sh
@@ -18,145 +18,128 @@ WATERMARK=`date  "+%e. %B %Y, %H:%M:%S"`
 
 
 
-rrdtool graph $PNG_TEMP_D    \
---title "Temperatur [°C]" \
---end now --start end-36h  \
--w $WIDTH -h $(($HEIGHT*2)) -a PNG       \
---watermark "$WATERMARK" \
---right-axis 1:0         \
-DEF:temp_outdoor=$RRD:temp_outdoor:AVERAGE        \
-DEF:temp_indoor=$RRD:temp_indoor:AVERAGE          \
-LINE1:temp_outdoor#0000FF:"Temperatur außen"    \
-GPRINT:temp_outdoor:LAST:"\t Aktuell\: %5.2lf °C" \
-GPRINT:temp_outdoor:AVERAGE:"Mittelwert\: %5.2lf °C" \
-GPRINT:temp_outdoor:MAX:"Max\: %5.2lf °C" \
-GPRINT:temp_outdoor:MIN:"Min\: %5.2lf °C\n"            \
-LINE2:temp_indoor#FF0000:"Temperatur innen"      \
-GPRINT:temp_indoor:LAST:"\t Aktuell\: %5.2lf °C" \
-GPRINT:temp_indoor:AVERAGE:"Mittelwert\: %5.2lf °C" \
-GPRINT:temp_indoor:MAX:"Max\: %5.2lf °C" \
-GPRINT:temp_indoor:MIN:"Min\: %5.2lf °C\n" \
+
+#######################################################################
+# Parameter:                                                          #
+# $1 Time Range                                                       #
+# $2 Filename                                                         #
+#######################################################################
+printTemp ()
+  {
+    rrdtool graph $2                                     \
+    --title "Temperatur [°C]"                            \
+    --end now --start end-$1                             \
+    -w $WIDTH -h $(($HEIGHT*2)) -a PNG                   \
+    --watermark "$WATERMARK"                             \
+    --right-axis 1:0                                     \
+    DEF:temp_outdoor=$RRD:temp_outdoor:AVERAGE           \
+    DEF:temp_indoor=$RRD:temp_indoor:AVERAGE             \
+    LINE1:temp_outdoor#0000FF:"Temperatur außen"         \
+    GPRINT:temp_outdoor:LAST:"\t Aktuell\: %5.2lf °C"    \
+    GPRINT:temp_outdoor:AVERAGE:"Mittelwert\: %5.2lf °C" \
+    GPRINT:temp_outdoor:MAX:"Max\: %5.2lf °C"            \
+    GPRINT:temp_outdoor:MIN:"Min\: %5.2lf °C\n"          \
+    LINE2:temp_indoor#FF0000:"Temperatur innen"          \
+    GPRINT:temp_indoor:LAST:"\t Aktuell\: %5.2lf °C"     \
+    GPRINT:temp_indoor:AVERAGE:"Mittelwert\: %5.2lf °C"  \
+    GPRINT:temp_indoor:MAX:"Max\: %5.2lf °C"             \
+    GPRINT:temp_indoor:MIN:"Min\: %5.2lf °C\n" 
+ }
 
 
-rrdtool graph $PNG_CPU_D    \
---title "Temperatur Raspberry Pi [°C]" \
---end now --start end-36h  \
--w $WIDTH -h $HEIGHT -a PNG       \
---watermark "$WATERMARK" \
---right-axis 1:0         \
-DEF:temp_cpu=$RRD:temp_cpu:AVERAGE                \
-LINE1:temp_cpu#FF00FF:"Temperatur Raspberry Pi"      
+#######################################################################
+# printCPUTemp()                                                      #
+# Parameter:                                                          #
+# $1 Time Range                                                       #
+# $2 Filename                                                         #
+#######################################################################
+printCPUTemp ()
+  {
+    rrdtool graph $2                                     \
+    --title "Temperatur Raspberry Pi [°C]"               \
+    --end now --start end-$1                             \
+    -w $WIDTH -h $HEIGHT -a PNG                          \
+    --watermark "$WATERMARK"                             \
+    --right-axis 1:0                                     \
+    DEF:temp_cpu=$RRD:temp_cpu:AVERAGE                   \
+    LINE1:temp_cpu#FF00FF:"Temperatur Raspberry Pi"      
+ }
 
 
-
-rrdtool graph $PNG_HUMI_D    \
---title "Luftfeuchtigkeit [%]" \
---end now --start end-36h  \
--w $WIDTH -h $HEIGHT -a PNG       \
---watermark "$WATERMARK" \
---right-axis 1:0                                     \
-DEF:humi_outdoor=$RRD:humi_outdoor:AVERAGE           \
-DEF:humi_indoor=$RRD:humi_indoor:AVERAGE             \
-LINE1:humi_outdoor#0000FF:"Luftfeuchtigkeit außen"   \
-GPRINT:humi_outdoor:LAST:"\t Aktuell\: %5.2lf %%" \
-GPRINT:humi_outdoor:AVERAGE:"Mittelwert\: %5.2lf %%" \
-GPRINT:humi_outdoor:MAX:"Max\: %5.2lf %%" \
-GPRINT:humi_outdoor:MIN:"Min\: %5.2lf %%\n"            \
-LINE2:humi_indoor#FF0000:"Luftfeuchtigkeit innen"     \
-GPRINT:humi_indoor:LAST:"\t Aktuell\: %5.2lf %%" \
-GPRINT:humi_indoor:AVERAGE:"Mittelwert\: %5.2lf %%"    \
-GPRINT:humi_indoor:MAX:"Max\: %5.2lf %%"               \
-GPRINT:humi_indoor:MIN:"Min\: %5.2lf %%\n"            \
-
-
-
-rrdtool graph $PNG_PRES_D    \
---title "Luftdruck [hPa]" \
---end now --start end-36h  \
--w $WIDTH -h $HEIGHT -a PNG       \
---alt-autoscale \
---alt-y-grid \
---rigid \
---watermark "$WATERMARK" \
---right-axis 1:0         \
---right-axis-format "%4.0lf" \
-DEF:air_pressure=$RRD:air_pressure:AVERAGE        \
-LINE1:air_pressure#40FF00:"Luftdruck"             \
-GPRINT:air_pressure:LAST:"\t Aktuell\: %5.2lf hPa" \
-GPRINT:air_pressure:AVERAGE:"Mittelwert\: %5.2lf hPa" \
-GPRINT:air_pressure:MAX:"Max\: %5.2lf hPa" \
-GPRINT:air_pressure:MIN:"Min\: %5.2lf hPa\n"            \
-
-#--left-axis-format "%4.0lf" \
+#######################################################################
+# printHumidity()                                                     #
+# Parameter:                                                          #
+# $1 Time Range                                                       #
+# $2 Filename                                                         #
+#######################################################################
+printHumidity ()
+  {
+    rrdtool graph $2                                     \
+    --title "Luftfeuchtigkeit [%]"                       \
+    --end now --start end-$1                             \
+    -w $WIDTH -h $HEIGHT -a PNG                          \
+    --watermark "$WATERMARK"                             \
+    --right-axis 1:0                                     \
+    DEF:humi_outdoor=$RRD:humi_outdoor:AVERAGE           \
+    DEF:humi_indoor=$RRD:humi_indoor:AVERAGE             \
+    LINE1:humi_outdoor#0000FF:"Luftfeuchtigkeit außen"   \
+    GPRINT:humi_outdoor:LAST:"\t Aktuell\: %5.2lf %%"    \
+    GPRINT:humi_outdoor:AVERAGE:"Mittelwert\: %5.2lf %%" \
+    GPRINT:humi_outdoor:MAX:"Max\: %5.2lf %%"            \
+    GPRINT:humi_outdoor:MIN:"Min\: %5.2lf %%\n"          \
+    LINE2:humi_indoor#FF0000:"Luftfeuchtigkeit innen"    \
+    GPRINT:humi_indoor:LAST:"\t Aktuell\: %5.2lf %%"     \
+    GPRINT:humi_indoor:AVERAGE:"Mittelwert\: %5.2lf %%"  \
+    GPRINT:humi_indoor:MAX:"Max\: %5.2lf %%"             \
+    GPRINT:humi_indoor:MIN:"Min\: %5.2lf %%\n"            
+ }
 
 
+#######################################################################
+# printAirPressure()                                                  #
+# Parameter:                                                          #
+# $1 Time Range                                                       #
+# $2 Filename                                                         #
+#######################################################################
+printAirPressure ()
+  {
+    rrdtool graph $2                                     \
+    --title "Luftdruck [hPa]"                            \
+    --end now --start end-$1                             \
+    -w $WIDTH -h $HEIGHT -a PNG                          \
+    --alt-autoscale                                      \
+    --alt-y-grid                                         \
+    --rigid                                              \
+    --watermark "$WATERMARK"                             \
+    --right-axis 1:0                                     \
+    --right-axis-format "%4.0lf"                         \
+    DEF:air_pressure=$RRD:air_pressure:AVERAGE           \
+    LINE1:air_pressure#40FF00:"Luftdruck"                \
+    GPRINT:air_pressure:LAST:"\t Aktuell\: %5.2lf hPa"   \
+    GPRINT:air_pressure:AVERAGE:"Mittelwert\: %5.2lf hPa" \
+    GPRINT:air_pressure:MAX:"Max\: %5.2lf hPa"           \
+    GPRINT:air_pressure:MIN:"Min\: %5.2lf hPa\n"
 
-rrdtool graph $PNG_TEMP_W    \
---title "Temperatur [°C]" \
---end now --start end-7d  \
--w $WIDTH -h $(($HEIGHT*2)) -a PNG       \
---watermark "$WATERMARK" \
---right-axis 1:0         \
-DEF:temp_outdoor=$RRD:temp_outdoor:AVERAGE        \
-DEF:temp_indoor=$RRD:temp_indoor:AVERAGE          \
-LINE1:temp_outdoor#0000FF:"Temperatur außen"    \
-GPRINT:temp_outdoor:LAST:"\t Aktuell\: %5.2lf °C" \
-GPRINT:temp_outdoor:AVERAGE:"Mittelwert\: %5.2lf °C" \
-GPRINT:temp_outdoor:MAX:"Max\: %5.2lf °C" \
-GPRINT:temp_outdoor:MIN:"Min\: %5.2lf °C\n"            \
-LINE2:temp_indoor#FF0000:"Temperatur innen"      \
-GPRINT:temp_indoor:LAST:"\t Aktuell\: %5.2lf °C" \
-GPRINT:temp_indoor:AVERAGE:"Mittelwert\: %5.2lf °C" \
-GPRINT:temp_indoor:MAX:"Max\: %5.2lf °C" \
-GPRINT:temp_indoor:MIN:"Min\: %5.2lf °C\n" \
-
-
-rrdtool graph $PNG_CPU_W    \
---title "Temperatur Raspberry Pi [°C]" \
---end now --start end-7d  \
--w $WIDTH -h $HEIGHT -a PNG       \
---watermark "$WATERMARK" \
---right-axis 1:0         \
-DEF:temp_cpu=$RRD:temp_cpu:AVERAGE                \
-LINE1:temp_cpu#FF00FF:"Temperatur Raspberry Pi"      
-
-
-
-rrdtool graph $PNG_HUMI_W    \
---title "Luftfeuchtigkeit [%]" \
---end now --start end-7d  \
--w $WIDTH -h $HEIGHT -a PNG       \
---watermark "$WATERMARK" \
---right-axis 1:0                                     \
-DEF:humi_outdoor=$RRD:humi_outdoor:AVERAGE           \
-DEF:humi_indoor=$RRD:humi_indoor:AVERAGE             \
-LINE1:humi_outdoor#0000FF:"Luftfeuchtigkeit außen"   \
-GPRINT:humi_outdoor:LAST:"\t Aktuell\: %5.2lf %%" \
-GPRINT:humi_outdoor:AVERAGE:"Mittelwert\: %5.2lf %%" \
-GPRINT:humi_outdoor:MAX:"Max\: %5.2lf %%" \
-GPRINT:humi_outdoor:MIN:"Min\: %5.2lf %%\n"            \
-LINE2:humi_indoor#FF0000:"Luftfeuchtigkeit innen"     \
-GPRINT:humi_indoor:LAST:"\t Aktuell\: %5.2lf %%" \
-GPRINT:humi_indoor:AVERAGE:"Mittelwert\: %5.2lf %%"    \
-GPRINT:humi_indoor:MAX:"Max\: %5.2lf %%"               \
-GPRINT:humi_indoor:MIN:"Min\: %5.2lf %%\n"            \
+    #--left-axis-format "%4.0lf" \
+ }
 
 
 
-rrdtool graph $PNG_PRES_W    \
---title "Luftdruck [hPa]" \
---end now --start end-7d  \
--w $WIDTH -h $HEIGHT -a PNG       \
---alt-autoscale \
---alt-y-grid \
---rigid \
---watermark "$WATERMARK" \
---right-axis 1:0         \
-DEF:air_pressure=$RRD:air_pressure:AVERAGE        \
-LINE1:air_pressure#40FF00:"Luftdruck"             \
-GPRINT:air_pressure:LAST:"\t Aktuell\: %5.2lf hPa" \
-GPRINT:air_pressure:AVERAGE:"Mittelwert\: %5.2lf hPa" \
-GPRINT:air_pressure:MAX:"Max\: %5.2lf hPa" \
-GPRINT:air_pressure:MIN:"Min\: %5.2lf hPa\n"            \
+printTemp "36h", "$PNG_TEMP_D"
+printTemp "7d",  "$PNG_TEMP_W"
+
+printCPUTemp "36h", "$PNG_CPU_D"
+printCPUTemp "7d", "$PNG_CPU_W"
+
+printHumidity "36h", "$PNG_HUMI_D"
+printHumidity "7d", "$PNG_HUMI_W"
+
+printAirPressure "36h", "$PNG_PRES_D"
+printAirPressure "7d", "$PNG_PRES_W"
+
+
+
+
+
 
 
