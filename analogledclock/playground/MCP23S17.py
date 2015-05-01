ef9960ef11395596a31c99483feaf9ef6fdd79cc
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-05-01 10:58:10 +0200

    code beautification

--- analogledclock/playground/MCP23S17.py
+++ analogledclock/playground/MCP23S17.py
@@ -1,58 +1,57 @@
 import RPi.GPIO as io
 from MCP23x17 import MCP23x17
+from SPI_const import SPI_const
+
+
+# Taken from http://erik-bartmann.de/
 
 class MCP23S17:
    SPI_SLAVE_ADDR_BASE  = 0x40
-
-   SPI_SCLK = 23
-   SPI_MOSI = 19
-   SPI_MISO = 21
-   SPI_CS = 26
 
    def __sendValue(self, value):
      v = value
 
      for i in range(8):
         if (v & 0x80):
-             io.output(self.SPI_MOSI, io.HIGH)
+             io.output(SPI_const.MOSI, io.HIGH)
         else:
-             io.output(self.SPI_MOSI, io.LOW)
+             io.output(SPI_const.MOSI, io.LOW)
 
          # Negative Flanke des Clocksignals generieren
-        io.output(self.SPI_SCLK, io.HIGH)
-        io.output(self.SPI_SCLK, io.LOW)
+        io.output(SPI_const.SCLK, io.HIGH)
+        io.output(SPI_const.SCLK, io.LOW)
         v <<= 1 # Bitfolge eine Position nach links schieben
-
 
 
    def send(self, device, addr, data):   # TODO: rename "addr" to "bank"
       # CS aktive (LOW-Aktiv)
-      io.output(self.SPI_CS, io.LOW)
+      io.output(self.cs, io.LOW)
 
       self.__sendValue(device|self.SPI_SLAVE_ADDR_BASE)
       self.__sendValue(addr)
       self.__sendValue(data)
 
       # CS nicht aktiv
-      io.output(self.SPI_CS, io.HIGH)
+      io.output(self.cs, io.HIGH)
 
 
 
-   def __init__ (self, devices):
+   def __init__ (self, cs, devices):
       self.devices = devices
+      self.cs      = cs
 
       io.setmode(io.BOARD)
       io.setwarnings(False)
 
       # Pin-Programmierung
-      io.setup(self.SPI_SCLK, io.OUT)
-      io.setup(self.SPI_MOSI, io.OUT)
-      io.setup(self.SPI_MISO, io.IN)
-      io.setup(self.SPI_CS,   io.OUT)
+      io.setup(SPI_const.SCLK, io.OUT)
+      io.setup(SPI_const.MOSI, io.OUT)
+      io.setup(SPI_const.MISO, io.IN)
+      io.setup(self.cs,   io.OUT)
 
       # Pegel vorbereiten
-      io.output(self.SPI_CS,   io.HIGH)
-      io.output(self.SPI_SCLK, io.LOW)
+      io.output(self.cs,   io.HIGH)
+      io.output(SPI_const.SCLK, io.LOW)
 
       # MCP23S17 needs hardware addressing explicitly enabled.
       self.send(0x00, MCP23x17.IOCONA, 0b00001000) # Set HAEN to 1.
