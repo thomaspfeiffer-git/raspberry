0d5ecde57d5209cc50e5765112593dc7359902db
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-05-03 22:05:37 +0200

    improved OO

--- analogledclock/Lightness.py
+++ analogledclock/Lightness.py
@@ -12,6 +12,37 @@ from SPI_const import SPI_const
 from MCP3008   import MCP3008
 
 
+class Value (object):
+   def __init__(self, v):
+      self.value = v
+
+   @property
+   def value(self):
+      return self.__value
+
+   @value.setter
+   def value(self,v):
+      # print "Setter v:", v
+      if (v >= 1023):
+         self.__value = 1023
+      elif (v <= 0):
+         self.__value = 0
+      else:
+         self.__value = int(v)
+
+
+   def __add__(self, other):
+      if not isinstance(other, Value):
+         other = Value(other)
+      return Value(self.value + other.value)
+
+   def __sub__(self, other):
+      if not isinstance(other, Value):
+         other = Value(other)
+      return Value(self.value - other.value)
+
+
+
 class Lightness (threading.Thread):
    def __init__(self):
       threading.Thread.__init__(self)
@@ -26,54 +57,26 @@ class Lightness (threading.Thread):
       self.running = True
 
 
-   def __boundaries(value):
-      if (value >= 1023):
-         return 1023
-      elif (value <= 0):
-         return 0
-      else:
-         return value
-
-
-   @property
-   def actual(self):
-      return self.__actual
-
-   @actual.setter
-   def actual(self, value):
-      self.__actual == __boundaries(value)
-
-
-   @property
-   def target(self):
-      return self.__target
-
-   @target.setter
-   def setter(self,value):      
-      self.__target = __boundaries(value)
-
-
    def run(self):
-      target = 1024
+      target = Value(1024)
       while (self.running):
-         actual = self.__adc.read() 
+         actual = Value(self.__adc.read())
 
-         if (actual > target+25):
+         if (actual.value > target.value+25):
             target += 25
-         elif (actual < target-25):
+         elif (actual.value < target.value-25):
             target -= 25
-         if (actual > target+10):
+         if (actual.value > target.value+10):
             target += 10
-         elif (actual < target-10):
+         elif (actual.value < target.value-10):
             target -= 10
-         elif (actual > target):
+         elif (actual.value > target.value):
             target += 1
-         elif (actual < target):
+         elif (actual.value < target.value):
             target -= 1
 
-         target = (target // 10) * 10
-         wipi.pwmWrite(12,1024-target)
-         # print("Lightness: {}/{}".format(actual,target))
+         wipi.pwmWrite(12,1024-target.value)
+         print("Lightness: {}/{}".format(actual.value,target.value))
          time.sleep(0.1)
 
 
