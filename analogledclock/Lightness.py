2be277fced17dcbe1c2e955f975a6185b4603ad6
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-05-04 21:14:45 +0200

    some code beautification

--- analogledclock/Lightness.py
+++ analogledclock/Lightness.py
@@ -41,6 +41,9 @@ class Value (object):
          other = Value(other)
       return Value(self.value - other.value)
 
+   def __rsub__(self,other):
+      return Value(other-self.value)
+
    def __lt__(self, other):
       if not isinstance(other, Value):
          other = Value(other)
@@ -50,6 +53,9 @@ class Value (object):
       if not isinstance(other, Value):
          other = Value(other)
       return self.value > other.value
+
+   def __int__(self):
+      return int(self.value)
 
    def __str__(self):
       return str(self.value)
@@ -81,18 +87,18 @@ class Lightness (threading.Thread):
       wipi.wiringPiSetupPhys()
       wipi.pinMode(12,2)
 
-      self.running = True
+      self.__running = True
 
 
    def run(self):
       target = Value(1023)
       measurements = Measurements()
 
-      while (self.running):
+      while (self.__running):
          actual = Value(self.__adc.read())
          measurements.add(actual)
          
-         # Target is quite constant in intervall [avg-10, avg+10]. Thus lights do not bump
+         # Target is quite constant in intervall [avg-10, avg+10]. Thus lights do not jitter
          # on small changes of lightness.
          avg = measurements.avg()
          if (avg > target+10):
@@ -100,13 +106,13 @@ class Lightness (threading.Thread):
          elif (avg < target-10):
             target -= 10
 
-         wipi.pwmWrite(12,1024-target.value)
+         wipi.pwmWrite(12,int(1024-target))
          print("Lightness (actual/avg/target): {}/{}/{}".format(actual,avg,target))
          time.sleep(0.1)
 
 
    def stop(self):
-      self.running = False
+      self.__running = False
 
 ### eof ###
 
