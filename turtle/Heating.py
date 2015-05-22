c2f523208f67fc1649fe4cfe1bf335f3b05eac63
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-05-22 17:18:59 +0200

    initial checkin

---
+++ turtle/Heating.py
@@ -0,0 +1,44 @@
+###############################################################################################
+# Heating.py                                                                                  #
+# Control heating                                                                             #
+# (c) https://github.com/thomaspfeiffer-git May 2015                                          #
+###############################################################################################
+
+import RPi.GPIO as io
+from time import time
+
+
+class Heating:
+   def __init__ (self, pin):
+      self.__pin = pin
+      self.__lastchanged = 0
+
+      io.setmode(io.BOARD)
+      io.setup(self.__pin,io.OUT)
+
+   def __delayperiod (self):
+      t = time()
+      print t
+      if (t >= self.__lastchanged + 100): 
+         self.__lastchanged = t
+         return True
+      else:
+         return False
+
+   def cleanup (self):
+      io.cleanup()
+
+   def on (self):
+      if (self.__delayperiod()):
+         io.output(self.__pin,io.HIGH)
+         print "on"
+
+   def off (self):
+      if (self.__delayperiod()):
+         io.output(self.__pin,io.LOW)
+         print "off"
+
+
+
+### eof ###
+
