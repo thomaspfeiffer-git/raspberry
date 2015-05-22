c08d3eee2f7effa5e1db876e30b6fcb6f66bc779
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-05-22 10:08:56 +0200

    added avg calculation

--- turtle/DS1820.py
+++ turtle/DS1820.py
@@ -4,30 +4,33 @@
 # (c) https://github.com/thomaspfeiffer-git May 2015                                          #
 ###############################################################################################
 
+import re
 
 class DS1820:
    def __init__ (self, id):
       self.__id = id
    
+   def __read_sensor (self):
+     value = "U"
+     try:
+       f = open(self.__id, "r")
+       line = f.readline()
+       if re.match(r"([0-9a-f]{2} ){9}: crc=[0-9a-f]{2} YES", line):
+         line = f.readline()
+         m = re.match(r"([0-9a-f]{2} ){9}t=([+-]?[0-9]+)", line)
+         if m:
+           value = float(m.group(2)) / 1000.0
+       f.close()
+     except (IOError), e:
+       print time.strftime("%x %X"), "Error reading", self.__id, ": ", e
+     return value
 
    def read (self):
-      pass
+      sum = 0
+      for i in range(0, 3):
+         sum += self.__read_sensor()
 
+      return sum/3
 
-
-def read_sensor(path):
-  value = "U"
-  try:
-    f = open(path, "r")
-    line = f.readline()
-    if re.match(r"([0-9a-f]{2} ){9}: crc=[0-9a-f]{2} YES", line):
-      line = f.readline()
-      m = re.match(r"([0-9a-f]{2} ){9}t=([+-]?[0-9]+)", line)
-      if m:
-        value = str(float(m.group(2)) / 1000.0)
-    f.close()
-  except (IOError), e:
-    print time.strftime("%x %X"), "Error reading", path, ": ", e
-  return value
-
+### eof ###
 
