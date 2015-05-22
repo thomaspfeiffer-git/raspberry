bace604681e3a085f6bc1474002d163cc356fef5
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-05-22 11:50:11 +0200

    small code beautifications

--- turtle/DHT22_AM2302.py
+++ turtle/DHT22_AM2302.py
@@ -4,11 +4,10 @@
 # (c) https://github.com/thomaspfeiffer-git May 2015                                          #
 ###############################################################################################
 
-
 import dhtreader
 
-
 class DHT22_AM2302:
+   __ERROR = -999.99
    def __init__ (self, pin):
       self.__pin = pin
       dhtreader.init()
@@ -19,12 +18,11 @@ class DHT22_AM2302:
          try:
             t, h = dhtreader.read(22,self.__pin) 
          except TypeError:
-            t = h = -999.99
+            t = h = __ERROR
             i += 1
             continue
          break
       return [t,h]
-
 
    def read (self):
       sum_t = sum_h = 0
@@ -32,6 +30,7 @@ class DHT22_AM2302:
          t, h = self.__read_sensor()
          sum_t += t
          sum_h += h
+         # TODO: Better handling for ERROR needed
       return [sum_t/3, sum_h/3]
 
 ### eof ###
