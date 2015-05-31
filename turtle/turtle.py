ab4a28f238bb834c54f3b98b961f9e987f27f6e3
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-05-31 14:40:28 +0200

    improved handling of measurements

--- turtle/turtle.py
+++ turtle/turtle.py
@@ -29,8 +29,8 @@ DS_HUMI    = "turtle_humi"
 DS_HEATING = "turtle_heating"
 
 
-# t0        = DS1820("/sys/bus/w1/devices/28-000006b4eb31/w1_slave")
-# t1        = DS1820("/sys/bus/w1/devices/28-000006b58b12/w1_slave")
+# t1        = DS1820("/sys/bus/w1/devices/28-000006b4eb31/w1_slave")
+# t2        = DS1820("/sys/bus/w1/devices/28-000006b58b12/w1_slave")
 th        = DHT22_AM2302(21)   # BCM 21 = PIN 40
 tc        = CPU()
 heatlamp  = Heating(HEATING_PIN, HEATING_LATENCY)
@@ -74,20 +74,25 @@ def Main():
    schedule[15][0:59] = [18 for m in range(60)]
    schedule[16][0:29] = [17 for m in range(30)]
 
-   m = [Measurements() for i in range(3)]
-
+   m = {DS_TEMP1:   Measurements(), \
+        DS_TEMP2:   Measurements(), \
+        DS_TEMP3:   Measurements(), \
+        DS_TEMPCPU: Measurements(), \
+        DS_HUMI:    Measurements()}
+ 
    while (True):
 #      heatlamp.on()
       hh, mm  = localtime()[3:5]
-      # t[0].append(t0.read())
-      # t[1].append(t0.read())
-      m[0].append(-99.9)
-      m[1].append(-99.9)
-      _t2, _h = th.read()
-      m[2].append(_t2)
-      _tc     = tc.read()
+      # m[DS_TEMP1].append(t1.read())
+      # m[DS_TEMP2].append(t2.read())
+      m[DS_TEMP1].append(-99.9)
+      m[DS_TEMP2].append(-99.9)
+      _t3, _h = th.read()
+      m[DS_TEMP3].append(_t3)
+      m[DS_HUMI].append(_h)
+      m[DS_TEMPCPU].append(tc.read())
 
-      if (schedule[hh][mm] > m[2].avg()):
+      if (schedule[hh][mm] > m[DS_TEMP3].avg()):
          heatlamp.on()
       else:
          heatlamp.off()
@@ -98,11 +103,11 @@ def Main():
                      DS_TEMPCPU + ":" + \
                      DS_HUMI    + ":" + \
                      DS_HEATING
-      rrd_data     = "N:{:.2f}".format(m[0].last()) + \
-                      ":{:.2f}".format(m[1].last()) + \
-                      ":{:.2f}".format(m[2].last()) + \
-                      ":{:.2f}".format(_tc) + \
-                      ":{:.2f}".format(_h) + \
+      rrd_data     = "N:{:.2f}".format(m[DS_TEMP1].last()) + \
+                      ":{:.2f}".format(m[DS_TEMP2].last()) + \
+                      ":{:.2f}".format(m[DS_TEMP3].last()) + \
+                      ":{:.2f}".format(m[DS_TEMPCPU].last()) + \
+                      ":{:.2f}".format(m[DS_HUMI].last()) + \
                       ":{:}".format(heatlamp.status())
       print strftime("%H:%M:%S", localtime()), rrd_data
       rrdtool.update(RRDFILE, "--template", rrd_template, rrd_data) 
