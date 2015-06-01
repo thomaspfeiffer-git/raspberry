bb431918ea1aef127fd272e56f89a60dba6d77d8
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-06-01 21:31:36 +0200

    improved exception handling; pull resistors seem to not work

--- analogledclock/MCP23017.py
+++ analogledclock/MCP23017.py
@@ -5,6 +5,8 @@
 ###############################################################################################
 
 import smbus
+from time import localtime
+
 from MCP23x17 import MCP23x17
 
 class MCP23017:
@@ -18,10 +20,16 @@ class MCP23017:
          self.send(d, MCP23x17.IODIRB, 0b00000000)
 
    def send(self, device, bank, pattern):
-      try:
-         self.__bus.write_byte_data(device,bank,pattern)
-      except IOError:
-         print "Device:", hex(device), "Bank:", hex(bank), "Pattern:", hex(pattern)
+      i = 0
+      while (i < 3):
+         try:
+            self.__bus.write_byte_data(device,bank,pattern)
+         except IOError:
+            print localtime()[3:6], "Device:", hex(device), "Bank:", hex(bank), "Pattern:", bin(pattern)
+            i += 1
+            print "Retry:", i
+            continue
+         break
 
 ### eof ###
 
