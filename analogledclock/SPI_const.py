6c8f240d8ee23cca13032ec3e43f9146b0a09a16
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-06-15 13:12:56 +0200

    improved CS handling

--- analogledclock/SPI_const.py
+++ analogledclock/SPI_const.py
@@ -10,8 +10,10 @@ class SPI_const:
    SCLK       = 23  # Serial-Clock
    MOSI       = 19  # Master-Out-Slave-In
    MISO       = 21  # Master-In-Slave-Out
-   CS0        = 24  # Chip-Select
-   CS1        = 26
+   CS0_pin    = 24  # Chip-Select
+   CS1_pin    = 26
+   CS0        = 0
+   CS1        = 1
 
 ### eof ###
 
