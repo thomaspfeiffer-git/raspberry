a2d448193d99046f569c57cd27f2ffc85a051ab6
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-06-16 20:47:40 +0200

    increased avg calculation

--- turtle/DHT22_AM2302.py
+++ turtle/DHT22_AM2302.py
@@ -5,6 +5,7 @@
 ###############################################################################################
 
 import dhtreader
+import numpy as np
 
 class DHT22_AM2302:
    ERROR = -999.99
@@ -26,18 +27,21 @@ class DHT22_AM2302:
       return [t,h]
 
    def read (self):
-      sum_t = sum_h = 0
-      counter = 0
-      for i in range(0, 3):
-         t, h = self.__read_sensor()
-         if (t == self.ERROR):
+      t = [self.ERROR]
+      h = [self.ERROR]
+      for i in range(0, 10):
+         __t, __h = self.__read_sensor()
+         if (__t == self.ERROR):
             continue
+         t.append(__t)
+         h.append(__h)
 
-         sum_t   += t
-         sum_h   += h
-         counter += 1
+      t.sort()
+      h.sort()
 
-      return [sum_t/counter, sum_h/counter]
+      t_avg = np.mean(t[int(len(t)/3):int(len(t)/3)*2])
+      h_avg = np.mean(h[int(len(h)/3):int(len(h)/3)*2])
+      return [t_avg, h_avg]
 
 ### eof ###
 
