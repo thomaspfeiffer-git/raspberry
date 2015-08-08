731df7d8892617d28536e60d90db483fb898f936
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-08-08 20:42:41 +0200

    some refactoring in Schedules.py and turtle.py

--- turtle/Schedules.py
+++ turtle/Schedules.py
@@ -8,46 +8,87 @@ import datetime
 from time import *
 
 
-class heat:
-   schedule = [[5 for m in range(60)] for h in range(24)]
+class __schedule_base:
+   def ts (self):
+      hh, mm  = localtime()[3:5]
+      dy      = datetime.datetime.utcnow().isocalendar()[1]
+      return [dy,hh,mm]
 
-   schedule[ 7][0:59] = [25 for m in range(60)]
-   schedule[ 8][0:59] = [25 for m in range(60)]
-   schedule[ 9][0:59] = [25 for m in range(60)]
-   schedule[10][0:59] = [30 for m in range(60)]
-   schedule[11][0:59] = [30 for m in range(60)]
-   schedule[12][0:59] = [30 for m in range(60)]
-   schedule[13][0:59] = [25 for m in range(60)]
-   schedule[14][0:59] = [25 for m in range(60)]
-   schedule[15][0:59] = [20 for m in range(60)]
-   schedule[16][0:29] = [18 for m in range(30)]
+
+class HeatControl (__schedule_base):
+   __schedule = [[5 for m in range(60)] for h in range(24)]
+
+   __schedule[ 7][0:59] = [25 for m in range(60)]
+   __schedule[ 8][0:59] = [25 for m in range(60)]
+   __schedule[ 9][0:59] = [25 for m in range(60)]
+   __schedule[10][0:59] = [30 for m in range(60)]
+   __schedule[11][0:59] = [30 for m in range(60)]
+   __schedule[12][0:59] = [30 for m in range(60)]
+   __schedule[13][0:59] = [25 for m in range(60)]
+   __schedule[14][0:59] = [25 for m in range(60)]
+   __schedule[15][0:59] = [20 for m in range(60)]
+   __schedule[16][0:29] = [18 for m in range(30)]
  
 
+   def __on (self,value):
+      dy, hh, mm = self.ts()
 
-class light:
-   schedule = [[[5 for m in range(60)] for h in range(24)] for M in range(53)]
+      if (self.__schedule[hh][mm] > value):
+         return True
+      else:
+         return False 
 
-   schedule[32][ 8][0:59] = [35 for m in range(60)]
-   schedule[32][ 9][0:59] = [35 for m in range(60)]
-   schedule[32][10][0:59] = [35 for m in range(60)]
-   schedule[32][11][0:59] = [35 for m in range(60)]
-   schedule[32][12][0:59] = [35 for m in range(60)]
-   schedule[32][13][0:59] = [35 for m in range(60)]
 
-   schedule[33][ 8][0:59] = [35 for m in range(60)]
-   schedule[33][ 9][0:59] = [35 for m in range(60)]
-   schedule[33][10][0:59] = [35 for m in range(60)]
-   schedule[33][11][0:59] = [35 for m in range(60)]
-   schedule[33][12][0:59] = [35 for m in range(60)]
-   schedule[33][13][0:59] = [35 for m in range(60)]
+   def switch (self, lamp, value):
+      if (self.__on(value)):
+         lamp.on()
+      else:
+         lamp.off()
 
-   schedule[34][ 8][0:59] = [35 for m in range(60)]
-   schedule[34][ 9][0:59] = [35 for m in range(60)]
-   schedule[34][10][0:59] = [35 for m in range(60)]
-   schedule[34][11][0:59] = [35 for m in range(60)]
-   schedule[34][12][0:59] = [35 for m in range(60)]
-   schedule[34][13][0:59] = [35 for m in range(60)]
 
+
+class LightControl (__schedule_base):
+   __schedule = [[[5 for m in range(60)] for h in range(24)] for M in range(53)]
+
+   __tmax = 40
+   __schedule[32][ 8][0:59] = [__tmax for m in range(60)]
+   __schedule[32][ 9][0:59] = [35 for m in range(60)]
+   __schedule[32][10][0:59] = [35 for m in range(60)]
+   __schedule[32][11][0:59] = [35 for m in range(60)]
+   __schedule[32][12][0:59] = [35 for m in range(60)]
+   __schedule[32][13][0:59] = [35 for m in range(60)]
+   __schedule[32][20][0:59] = [35 for m in range(60)]
+
+   __schedule[33][ 8][0:59] = [35 for m in range(60)]
+   __schedule[33][ 9][0:59] = [35 for m in range(60)]
+   __schedule[33][10][0:59] = [35 for m in range(60)]
+   __schedule[33][11][0:59] = [35 for m in range(60)]
+   __schedule[33][12][0:59] = [35 for m in range(60)]
+   __schedule[33][13][0:59] = [35 for m in range(60)]
+
+   __schedule[34][ 8][0:59] = [35 for m in range(60)]
+   __schedule[34][ 9][0:59] = [35 for m in range(60)]
+   __schedule[34][10][0:59] = [35 for m in range(60)]
+   __schedule[34][11][0:59] = [35 for m in range(60)]
+   __schedule[34][12][0:59] = [35 for m in range(60)]
+   __schedule[34][13][0:59] = [35 for m in range(60)]
+
+
+   def __on (self, value):
+      dy, hh, mm = self.ts()
+
+      if (self.__schedule[dy][hh][mm] > value):
+         return True
+      else:
+         return False 
+
+
+   def switch (self, lamp, value):
+      if (self.__on(value)):
+         lamp.on()
+      else:
+         lamp.off()
 
 
 ### eof ###
+
