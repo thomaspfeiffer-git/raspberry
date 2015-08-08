f2e0aa6a7bd61669ad2dc92d7a9eb8dd004f3241
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-08-08 21:03:42 +0200

    some refactoring in Schedules.py and turtle.py

--- turtle/Schedules.py
+++ turtle/Schedules.py
@@ -8,14 +8,24 @@ import datetime
 from time import *
 
 
-class __schedule_base:
-   def ts (self):
+class __schedule_base(object):
+   def __on(self, value):
+      pass
+
+   def __ts (self):
       hh, mm  = localtime()[3:5]
       dy      = datetime.datetime.utcnow().isocalendar()[1]
       return [dy,hh,mm]
 
 
-class HeatControl (__schedule_base):
+   def switch (self, lamp, value):
+      if (self.__on(value)):
+         lamp.on()
+      else:
+         lamp.off()
+
+
+class HeatControl(__schedule_base):
    __schedule = [[5 for m in range(60)] for h in range(24)]
 
    __schedule[ 7][0:59] = [25 for m in range(60)]
@@ -31,7 +41,7 @@ class HeatControl (__schedule_base):
  
 
    def __on (self,value):
-      dy, hh, mm = self.ts()
+      dy, hh, mm = self.__ts()
 
       if (self.__schedule[hh][mm] > value):
          return True
@@ -40,42 +50,38 @@ class HeatControl (__schedule_base):
 
 
    def switch (self, lamp, value):
-      if (self.__on(value)):
-         lamp.on()
-      else:
-         lamp.off()
-
+      super(HeatControl,self).switch(lamp,value)
 
 
 class LightControl (__schedule_base):
-   __schedule = [[[5 for m in range(60)] for h in range(24)] for M in range(53)]
+   __schedule = [[[5 for m in range(60)] for h in range(24)] for w in range(53)]
 
    __tmax = 40
    __schedule[32][ 8][0:59] = [__tmax for m in range(60)]
-   __schedule[32][ 9][0:59] = [35 for m in range(60)]
-   __schedule[32][10][0:59] = [35 for m in range(60)]
-   __schedule[32][11][0:59] = [35 for m in range(60)]
-   __schedule[32][12][0:59] = [35 for m in range(60)]
-   __schedule[32][13][0:59] = [35 for m in range(60)]
-   __schedule[32][20][0:59] = [35 for m in range(60)]
+   __schedule[32][ 9][0:59] = [__tmax for m in range(60)]
+   __schedule[32][10][0:59] = [__tmax for m in range(60)]
+   __schedule[32][11][0:59] = [__tmax for m in range(60)]
+   __schedule[32][12][0:59] = [__tmax for m in range(60)]
+   __schedule[32][13][0:59] = [__tmax for m in range(60)]
+   __schedule[32][21][0:59] = [__tmax for m in range(60)]
 
-   __schedule[33][ 8][0:59] = [35 for m in range(60)]
-   __schedule[33][ 9][0:59] = [35 for m in range(60)]
-   __schedule[33][10][0:59] = [35 for m in range(60)]
-   __schedule[33][11][0:59] = [35 for m in range(60)]
-   __schedule[33][12][0:59] = [35 for m in range(60)]
-   __schedule[33][13][0:59] = [35 for m in range(60)]
+   __schedule[33][ 8][0:59] = [__tmax for m in range(60)]
+   __schedule[33][ 9][0:59] = [__tmax for m in range(60)]
+   __schedule[33][10][0:59] = [__tmax for m in range(60)]
+   __schedule[33][11][0:59] = [__tmax for m in range(60)]
+   __schedule[33][12][0:59] = [__tmax for m in range(60)]
+   __schedule[33][13][0:59] = [__tmax for m in range(60)]
 
-   __schedule[34][ 8][0:59] = [35 for m in range(60)]
-   __schedule[34][ 9][0:59] = [35 for m in range(60)]
-   __schedule[34][10][0:59] = [35 for m in range(60)]
-   __schedule[34][11][0:59] = [35 for m in range(60)]
-   __schedule[34][12][0:59] = [35 for m in range(60)]
-   __schedule[34][13][0:59] = [35 for m in range(60)]
+   __schedule[34][ 8][0:59] = [__tmax for m in range(60)]
+   __schedule[34][ 9][0:59] = [__tmax for m in range(60)]
+   __schedule[34][10][0:59] = [__tmax for m in range(60)]
+   __schedule[34][11][0:59] = [__tmax for m in range(60)]
+   __schedule[34][12][0:59] = [__tmax for m in range(60)]
+   __schedule[34][13][0:59] = [__tmax for m in range(60)]
 
 
    def __on (self, value):
-      dy, hh, mm = self.ts()
+      dy, hh, mm = self.__ts()
 
       if (self.__schedule[dy][hh][mm] > value):
          return True
@@ -84,11 +90,7 @@ class LightControl (__schedule_base):
 
 
    def switch (self, lamp, value):
-      if (self.__on(value)):
-         lamp.on()
-      else:
-         lamp.off()
-
+      super(LightControl,self).switch(lamp,value)
 
 ### eof ###
 
