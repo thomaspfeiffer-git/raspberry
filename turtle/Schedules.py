1252774f6560090bce9c677bfa07a7ce6d7eed32
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-08-08 19:14:47 +0200

    initial checkin

---
+++ turtle/Schedules.py
@@ -0,0 +1,20 @@
+###############################################################################################
+# Schedules.py                                                                                #
+# Schedule data for heating and light                                                         #
+# (c) https://github.com/thomaspfeiffer-git 2015                                              #
+###############################################################################################
+
+import datetime
+from time import *
+
+
+class heat:
+   pass
+
+
+
+class light:
+   pass
+
+
+### eof ###
