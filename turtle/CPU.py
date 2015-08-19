19cd9ce6d6a37e48c7753839a0786893a265b5c0
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-08-19 20:59:51 +0200

    doing some pylint stuff

--- turtle/CPU.py
+++ turtle/CPU.py
@@ -1,15 +1,19 @@
-###############################################################################################
-# CPU.py                                                                                      #
-# Encapsulates CPU temp stuff                                                                 #
-# (c) https://github.com/thomaspfeiffer-git May 2015                                          #
-###############################################################################################
+##############################################################################
+# CPU.py                                                                     #
+# Encapsulates CPU temp stuff                                                #
+# (c) https://github.com/thomaspfeiffer-git 2015                             #
+##############################################################################
+"""encapsulates CPU temp stuff"""
 
 import os
 
 class CPU:
-   def read(self):
-      res = os.popen('vcgencmd measure_temp').readline()
-      return(float(res.replace("temp=","").replace("'C\n","")))
+    def __init__(self):
+        pass
+
+    def read(self):
+        res = os.popen('vcgencmd measure_temp').readline()
+        return(float(res.replace("temp=","").replace("'C\n","")))
 
 ### eof ###
 
