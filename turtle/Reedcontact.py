cda9938e9dd2f21382d47b98dd139d97c1fd8b17
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-09-13 20:40:28 +0200

    just continuing

--- turtle/Reedcontact.py
+++ turtle/Reedcontact.py
@@ -7,18 +7,27 @@
 import RPi.GPIO as io
 
 class Reedcontact (object):
-    __callback(pin):
+    def __callback(pin):
         print "callback:", pin
 
-    __init__(self, pin):
+    def __init__(self, pin):
         self.__pin = pin
+        self.__status = 0
 
         io.setmode(io.BOARD)
         io.setup(self.__pin,io.IN)
+
+        # GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
+        # GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
+
         io.add_event_detect(self.__pin,io.BOTH)
         io.add_event_callback(seld.__pin,self.__callback)
+
+
         
 
+    status (self):
+        return self.__status
 
 
 ### eof ###
