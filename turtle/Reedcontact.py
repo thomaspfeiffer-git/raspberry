bda741cdd591cd9a91e800b2af577ff793722992
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-09-19 18:04:36 +0200

    added callback and pullup resistor

--- turtle/Reedcontact.py
+++ turtle/Reedcontact.py
@@ -8,25 +8,23 @@ import RPi.GPIO as io
 
 class Reedcontact (object):
 
-    def __callback(pin):
-        print "callback:", pin
-    # TODO: Check http://stackoverflow.com/questions/8472299/python-callback-inside-class
 
     def __init__(self, pin):
         self.__pin = pin
         self.__status = 0
 
+        def ___callback(pin):
+            self.__status = io.input(pin)
+
         io.setmode(io.BOARD)
         io.setup(self.__pin,io.IN)
-
-        # GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)  
-        # GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)  
-
         io.add_event_detect(self.__pin,io.BOTH)
-        io.add_event_callback(self.__pin,self.__callback)
+        io.add_event_callback(self.__pin,___callback)
+        io.setup(self.__pin, io.IN, pull_up_down=io.PUD_UP)  
 
 
     def __del__(self):
+        print "in Reedcontact.__del__()"
         io.remove_event_detect(self.__pin)
 
 
