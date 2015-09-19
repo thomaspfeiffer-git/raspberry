ef5f9b2dcc8d645f0b83fb3484156267b3ac9de1
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2015-09-19 20:35:44 +0200

    added functionality for status enlargement

--- turtle/Reedcontact.py
+++ turtle/Reedcontact.py
@@ -2,17 +2,25 @@
 # Reedcontact.py                                                            #
 # (c) https://github.com/thomaspfeiffer-git 2015                            #
 #############################################################################
-""""""
+"""Provides some functionality for a reed contact"""
 
 import RPi.GPIO as io
+from time import time
+
 
 class Reedcontact (object):
-    def __init__(self, pin):
+    def __init__ (self, pin, stretch):
         self.__pin = pin
         self.__status = False
+        self.__stretch = stretch
+        self.__timestretched = time()
 
-        def ___callback(pin):
-            self.__status = io.input(pin)
+        def ___callback (pin):
+            if (io.input(pin)): 
+                self.__status = True
+            else:
+                self.__status = False
+                self.__timestretched = time() + self.__stretch
 
         io.setmode(io.BOARD)
         io.setup(self.__pin,io.IN)
@@ -21,12 +29,24 @@ class Reedcontact (object):
         io.setup(self.__pin, io.IN, pull_up_down=io.PUD_UP)  
 
 
-    def cleanup(self):
+    def cleanup (self):
         io.remove_event_detect(self.__pin)
 
 
     def status (self):
         return 1 if self.__status else 0
 
+
+    def status_stretched (self):
+        """enlarge interval of being "on"; otherwise if door is only 
+           opened for a short period of time, it would not be seen in RRD"""
+        if (self.__status):
+            return 1
+        else:
+            if (time() <= self.__timestretched):
+                return 1
+            else:
+                return 0
+              
 ### eof ###
 
