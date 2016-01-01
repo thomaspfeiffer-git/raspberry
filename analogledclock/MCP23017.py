2b98c46be368c58ba1a39fa9c90eb7aabd86a998
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-01 09:02:54 +0100

    doing some pylint stuff

--- analogledclock/MCP23017.py
+++ analogledclock/MCP23017.py
@@ -1,8 +1,9 @@
-###############################################################################################
-# MCP23017                                                                                    #
-# Communication with MCP23017                                                                 #
-# (c) https://github.com/thomaspfeiffer-git May 2015                                          #
-###############################################################################################
+################################################################################
+# MCP23017                                                                     #
+# Communication with MCP23017                                                  #
+# (c) https://github.com/thomaspfeiffer-git May 2015                           #
+################################################################################
+"""control component MCP23017"""
 
 import smbus
 from time import localtime
@@ -10,26 +11,28 @@ from time import localtime
 from MCP23x17 import MCP23x17
 
 class MCP23017:
-   def __init__ (self, devices):
-      self.__bus = smbus.SMBus(1)
-      self.devices = devices
+    """control component MCP23017"""
+    def __init__ (self, devices):
+        self.__bus = smbus.SMBus(1)
+        self.devices = devices
 
-      # Set port direction to output (0b00000000)
-      for d in self.devices:
-         self.send(d, MCP23x17.IODIRA, 0b00000000)
-         self.send(d, MCP23x17.IODIRB, 0b00000000)
+        # Set port direction to output (0b00000000)
+        for d in self.devices:
+            self.send(d, MCP23x17.IODIRA, 0b00000000)
+            self.send(d, MCP23x17.IODIRB, 0b00000000)
 
-   def send(self, device, bank, pattern):
-      i = 0
-      while (i < 3):
-         try:
-            self.__bus.write_byte_data(device,bank,pattern)
-         except IOError:
-            print localtime()[3:6], "Device:", hex(device), "Bank:", hex(bank), "Pattern:", bin(pattern)
-            i += 1
-            print "Retry:", i
-            continue
-         break
+    def send(self, device, bank, pattern):
+        """send pattern to bank of device"""
+        i = 0
+        while (i < 3):
+            try:
+                self.__bus.write_byte_data(device, bank, pattern)
+            except IOError:
+                print localtime()[3:6], "Device:", hex(device), "Bank:", hex(bank), "Pattern:", bin(pattern)
+                i += 1
+                print "Retry:", i
+                continue
+            break
 
 ### eof ###
 
