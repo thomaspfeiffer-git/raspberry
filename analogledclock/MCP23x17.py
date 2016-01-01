c86364366dcc3296c45c1d29221abeb9f785c89c
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-01 09:12:45 +0100

    doing some pylint stuff

--- analogledclock/MCP23x17.py
+++ analogledclock/MCP23x17.py
@@ -1,19 +1,20 @@
-###############################################################################################
-# MCP23x17                                                                                    #
-# PINs for MCP23x17 components                                                                #
-# Use with io.setmode(io.BOARD)                                                               #
-# (c) https://github.com/thomaspfeiffer-git May 2015                                          #
-###############################################################################################
+################################################################################
+# MCP23x17                                                                     #
+# PINs for MCP23x17 components                                                 #
+# Use with io.setmode(io.BOARD)                                                #
+# (c) https://github.com/thomaspfeiffer-git May 2015                           #
+################################################################################
+"""PINs for MCP23x17 components"""
 
 class MCP23x17:
-   IODIRA      = 0x00 # Pin direction register
-   IODIRB      = 0x01 # Pin direction register
-   IOCONA      = 0x0A # MCP23S17 needs hardware addressing explicitly enabled.
-   IOCONB      = 0x0B # MCP23S17 needs hardware addressing explicitly enabled.
-   OLATA       = 0x14 # Register for outputs
-   OLATB       = 0x15 # Register for outputs
+    IODIRA      = 0x00 # Pin direction register
+    IODIRB      = 0x01 # Pin direction register
+    IOCONA      = 0x0A # MCP23S17 needs hardware addressing explicitly enabled.
+    IOCONB      = 0x0B # MCP23S17 needs hardware addressing explicitly enabled.
+    OLATA       = 0x14 # Register for outputs
+    OLATB       = 0x15 # Register for outputs
 
-   HAEN        = 0b00001000
+    HAEN        = 0b00001000
 
 
 ### eof ###
