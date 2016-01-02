91512aada823df95937c4f4495362c1a725cfd2a
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-02 21:19:13 +0100

    initial checkin

---
+++ playground/ipc/server.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+
+
+from multiprocessing.managers import BaseManager
+import Queue
+queue = Queue.Queue()
+class QueueManager(BaseManager): pass
+QueueManager.register('get_queue', callable=lambda:queue)
+m = QueueManager(address=('', 50000), authkey='abracadabra')
+s = m.get_server()
+s.serve_forever()
