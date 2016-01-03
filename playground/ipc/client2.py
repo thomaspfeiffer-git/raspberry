3171ad52202e6a883838be55e6bd39a62838c936
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-03 08:17:43 +0100

    added looping

--- playground/ipc/client2.py
+++ playground/ipc/client2.py
@@ -9,7 +9,8 @@ QueueManager.register('get_queue')
 m = QueueManager(address=('pia', 50000), authkey='abracadabra')
 m.connect()
 queue = m.get_queue()
-sv = pickle.loads(queue.get())
 
-sv.showContent()
+while (True):
+    sv = pickle.loads(queue.get())
+    sv.showContent()
 
