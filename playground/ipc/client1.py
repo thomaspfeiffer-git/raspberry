79b961f88c50d17aa2843edec56e90d410982d65
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-03 15:48:34 +0100

    refactoring: using module SensorQueue

--- playground/ipc/client1.py
+++ playground/ipc/client1.py
@@ -1,23 +1,17 @@
 #!/usr/bin/python
 
-from multiprocessing.managers import BaseManager
-import pickle
 from time import sleep
 
+from SensorQueue import SensorQueueClient
 from sensorvalue import SensorValue
 
+sq = SensorQueueClient()
 sv = SensorValue("TempWohnzimmer","temp")
 
-class QueueManager(BaseManager): pass
-
-QueueManager.register('get_queue')
-m = QueueManager(address=('pia', 50000), authkey='abracadabra')
-m.connect()
-queue = m.get_queue()
 
 i = 0
 while (True):
     sv.setValue(i)
-    queue.put(pickle.dumps(sv))
+    sq.write(sv)
     i += 1
     sleep(55)
