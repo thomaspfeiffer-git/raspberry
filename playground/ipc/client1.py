63e14d1c29a25b4f35569e3675b97dae84fa43a8
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-03 07:28:09 +0100

    added class SensorValue and pickle

--- playground/ipc/client1.py
+++ playground/ipc/client1.py
@@ -1,9 +1,17 @@
 #!/usr/bin/python
 
 from multiprocessing.managers import BaseManager
+import pickle
+
+from sensorvalue import SensorValue
+
+sv = SensorValue("TempWohnzimmer","temp")
+sv.setValue(22.17)
+
 class QueueManager(BaseManager): pass
+
 QueueManager.register('get_queue')
 m = QueueManager(address=('pia', 50000), authkey='abracadabra')
 m.connect()
 queue = m.get_queue()
-queue.put('hello')
+queue.put(pickle.dumps(sv))
