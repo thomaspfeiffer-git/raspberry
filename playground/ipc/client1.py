bf28308f5126562dec3e8b4c89473ad25c7d8d3b
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-03 20:02:24 +0100

    use of setter for SensorValue

--- playground/ipc/client1.py
+++ playground/ipc/client1.py
@@ -3,7 +3,7 @@
 from time import sleep
 
 from SensorQueue import SensorQueueClient
-from sensorvalue import SensorValue
+from SensorValue import SensorValue
 
 sq = SensorQueueClient()
 sv = SensorValue("TempWohnzimmer","temp")
@@ -11,7 +11,7 @@ sv = SensorValue("TempWohnzimmer","temp")
 
 i = 0
 while (True):
-    sv.setValue(i)
+    sv.value = i
     sq.write(sv)
     i += 1
     sleep(55)
