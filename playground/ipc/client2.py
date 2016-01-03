eeb23fcb3e545c5ba28cea2c44834368299373c5
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-03 07:32:41 +0100

    added import of module pickle

--- playground/ipc/client2.py
+++ playground/ipc/client2.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 
 from multiprocessing.managers import BaseManager
+import pickle
+
+
 class QueueManager(BaseManager): pass
 QueueManager.register('get_queue')
 m = QueueManager(address=('pia', 50000), authkey='abracadabra')
