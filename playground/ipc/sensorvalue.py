6a6d5b6914f3f279e771e3f6e964d145109a5576
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-03 07:44:13 +0100

    added timestamp

--- playground/ipc/sensorvalue.py
+++ playground/ipc/sensorvalue.py
@@ -1,15 +1,19 @@
+import datetime
+
 class SensorValue (object):
     def __init__ (self, name, kind):
         self.__name = name
         self.__kind = kind
 
     def setValue (self, value):
-        self.__value = value
+        self.__value     = value
+        self.__timestamp = datetime.now()
 
     def showContent (self):
         print "Name:", self.__name
         print "Kind:", self.__kind
         print "Value:", self.__value
+        print "Timestamp:", self.__timestamp
 
 # eof
 
