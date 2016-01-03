35e1d0171e94342cbdcbcfa62d97ff3f27ff5de3
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-03 15:28:26 +0100

    various playing around

--- playground/ipc/server.py
+++ playground/ipc/server.py
@@ -8,9 +8,9 @@ from SensorQueue import SensorQueueServer
 # Exit ########################################################################
 def Exit():
     """stuff to be done on exit"""
+    print("Exit")
     SensorServer.stop()
     SensorServer.join()
-    print("Exit")
     sys.exit()
 
 
@@ -23,23 +23,27 @@ def _Exit(__s, __f):
 
 
 ###############################################################################
-signal.signal(signal.SIGTERM, _Exit)
-SensorServer = SensorQueueServer()
+if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, _Exit)
+    SensorServer = SensorQueueServer()
 
-try:
-    SensorServer.start()
+    try:
+        SensorServer.start()
+        print "server started"
+        while (True):
+            pass
 
-except KeyboardInterrupt:
-    Exit()
+    except KeyboardInterrupt:
+        Exit()
 
-except SystemExit:                  # Done in signal handler (method _Exit()) #
-    pass
+    except SystemExit:                  # Done in signal handler (method _Exit()) #
+        pass
 
-except:
-    print(traceback.print_exc())
+    except:
+        print(traceback.print_exc())
 
-finally:        # All cleanup is done in KeyboardInterrupt or signal handler. #
-    pass
+    finally:        # All cleanup is done in KeyboardInterrupt or signal handler. #
+        pass
 
 ### eof ###
 
