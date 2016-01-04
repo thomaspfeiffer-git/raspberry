babf29a1dc06510056455f0f3ff1840f54153635
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-04 14:45:42 +0100

    added proper signal handling for thread

--- playground/ipc/client1.py
+++ playground/ipc/client1.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python
 
+import signal
+import sys
 from time import strftime, localtime, sleep
 from threading import Lock
+import traceback
 
 from SensorQueue import SensorQueueClient
 from SensorValue import SensorValue, SensorValueLock
@@ -25,16 +28,56 @@ class Sensor (object):   # eg DS1820
 
 
 
-sv = SensorValueLock("TempWohnzimmer", "temp", Lock())
-sq  = SensorQueueClient(sv)
-sq.start()
+###############################################################################
+# Exit ########################################################################
+def Exit():
+    """stuff to be done on exit"""
+    sq.stop()
+    sq.join()
+    print("Exit")
+    sys.exit()
+
+def _Exit(__s, __f):
+    """exit for signal handler"""
+    print("_Exit")
+    Exit()
 
 
-i = 0
-sensor = Sensor(sv)
+###############################################################################
+# Main ########################################################################
+def Main():
+    i = 0
+    sensor = Sensor(sv)
 
-while (True):
-    value = sensor.read()
-    Log("client1, value = sensor.read(): %s" % value)
-    i += 1
-    sleep(25)
+    while (True):
+        value = sensor.read()
+        Log("client1, value = sensor.read(): %s" % value)
+        i += 1
+        sleep(25)
+
+
+###############################################################################
+###############################################################################
+if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, _Exit)
+    sv = SensorValueLock("TempWohnzimmer", "temp", Lock())
+    sq = SensorQueueClient(sv)
+    sq.start()
+
+    try:
+        Main()
+
+    except KeyboardInterrupt:
+        Exit()
+
+    except SystemExit:                  # Done in signal handler (method _Exit()) #
+        pass
+
+    except:
+        print(traceback.print_exc())
+
+    finally:        # All cleanup is done in KeyboardInterrupt or signal handler. #
+        pass
+
+# eof #
+
