8b9b6b401fb49f7e3971c5416020cbbd30b590ff
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-04 14:05:44 +0100

    making SensorQueueClient.write() a thread

--- playground/ipc/client1.py
+++ playground/ipc/client1.py
@@ -1,17 +1,40 @@
 #!/usr/bin/python
 
-from time import sleep
+from time import strftime, localtime, sleep
+from threading import Lock
 
 from SensorQueue import SensorQueueClient
-from SensorValue import SensorValue
+from SensorValue import SensorValue, SensorValueLock
 
-sq = SensorQueueClient()
-sv = SensorValue("TempWohnzimmer","temp")
+
+
+def Log (logstr):
+    """improved log output"""
+    print strftime("%Y%m%d %H:%M:%S", localtime()), logstr
+
+
+class Sensor (object):   # eg DS1820
+    def __init__ (self, sensorvalue=None):
+        self.__sv = sensorvalue
+
+    def read (self):
+        value = 22.2
+        if self.__sv is not None:
+            self.__sv.value = value 
+        return value
+
+
+
+sv = SensorValueLock("TempWohnzimmer", "temp", Lock())
+sq  = SensorQueueClient(sv)
+sq.start()
 
 
 i = 0
+sensor = Sensor(sv)
+
 while (True):
-    sv.value = i
-    sq.write(sv)
+    value = sensor.read()
+    Log("client1, value = sensor.read(): %s" % value)
     i += 1
     sleep(25)
