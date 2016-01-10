ea5eb520662e6572795325da0326f0ec7f3f1b9d
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-10 11:13:03 +0100

    copied from real project

--- playground/ipc/SensorValue.py
+++ playground/ipc/SensorValue.py
@@ -4,11 +4,11 @@
 # (c) https://github.com/thomaspfeiffer-git 2016                               #
 ################################################################################
 
-
 import time, datetime
 
 class SensorValue (object):
-    def __init__ (self, name, kind):
+    def __init__ (self, v_id, name, kind):
+        self.__v_id      = v_id
         self.__name      = name
         self.__kind      = kind
         self.__value     = None
@@ -23,19 +23,22 @@ class SensorValue (object):
         self.__value     = v
         self.__timestamp = time.time()
 
+    def getID (self):
+        return self.__v_id
+
     def __str__ (self):
-        return "Name:      %s" % self.__name      + "\n" + \
+        return "ID:        %s" % self.getID()     + "\n" + \
+               "Name:      %s" % self.__name      + "\n" + \
                "Kind:      %s" % self.__kind      + "\n" + \
                "Value:     %s" % self.__value     + "\n" + \
                "Timestamp: %s" % self.__timestamp + "\n" + \
                "Timestamp: %s" % datetime.datetime.fromtimestamp(self.__timestamp).strftime('%Y-%m-%d %H:%M:%S')
 
 
-
 class SensorValueLock (object):
-    def __init__ (self, name, kind, lock):
+    def __init__ (self, v_id, name, kind, lock):
         self.lock = lock
-        self.sensorvalue = SensorValue(name, kind)
+        self.sensorvalue = SensorValue(v_id, name, kind)
 
     @property
     def value (self):
