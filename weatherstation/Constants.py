1086eeafa84953888b9017d8787175c1c9948173
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-10 08:14:55 +0100

    initial checkin

---
+++ weatherstation/Constants.py
@@ -0,0 +1,14 @@
+
+
+class CONSTANTS:
+    """constant strings"""
+    DAYOFWEEK = {'0': 'Sonntag',    \
+                 '1': 'Montag',     \
+                 '2': 'Dienstag',   \
+                 '3': 'Mittwoch',   \
+                 '4': 'Donnerstag', \
+                 '5': 'Freitag',    \
+                 '6': 'Samstag'}
+
+# eof #
+
