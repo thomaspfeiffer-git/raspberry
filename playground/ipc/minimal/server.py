c0870cbafc47c4ede227ba990da8cd3c8efa1de2
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-10 14:04:16 +0100

    initial checkin

---
+++ playground/ipc/minimal/server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+from multiprocessing.managers import BaseManager
+
+try:
+    import queue
+except ImportError:
+    import Queue as queue
+
+class QueueManager(BaseManager): 
+    pass
+
+q = queue.Queue()
+QueueManager.register('get_queue', callable=lambda:q)
+manager = QueueManager(address=('', 50001), authkey="hallo".encode('latin1'))
+server = manager.get_server()
+server.serve_forever()
+
