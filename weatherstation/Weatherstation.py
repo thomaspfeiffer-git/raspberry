92ec833110e4631ae0b9b99b149a566d948ac3d7
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2016-01-16 20:02:08 +0100

    added first code fragments of class Lightness

--- weatherstation/Weatherstation.py
+++ weatherstation/Weatherstation.py
@@ -9,33 +9,11 @@
 
 """TODO: Backlight on/off
 
-import RPi.GPIO as io
-from time import sleep
-
-io.setmode(io.BCM)
-io.setup(18,io.OUT)
-
 while True:
     io.output(18,1)
     sleep(1)
     io.output(18,0)
     sleep(1)
-
-
-class Lightness (def):
-    def __init__(self):
-        pass
-
-    def Touched (self):
-        if user touches screen:
-            if light == off: 
-                light_on()
-                after n seconds
-                light_off()
-
-    todos:
-        a) based on lightness switch backlight automatically on/off
-        b) based on time of day switch backlight automatically on/off
 
 
 """
@@ -45,9 +23,10 @@ class Lightness (def):
 import os
 import pygame
 from pygame.locals import QUIT
+import RPi.GPIO as io
 import signal
 import sys
-from time import time
+from time import sleep, time
 import traceback
 
 sys.path.append('../libs')
@@ -98,6 +77,42 @@ class AllSensorValues (dict):
             # if timestamp <= now() + 1 h: sensorvalue[x] = "n/a"
             # self.__sensorvalues[v.getID()] = v.value.encode('latin-1')
             self[v.getID()] = v.value.encode('latin-1')
+
+
+###############################################################################
+###############################################################################
+class Lightness (object):
+    def __init__ (self):
+        self.__pin = 18  # GPIO 18
+
+        io.setmode(io.BCM)
+        io.setup(self.__pin,io.OUT)
+
+    def run (self):
+        pass
+
+        """
+        if (time > 22:00) and (time < 6:00):
+            if (time > time_to_switch_off):
+                backlight_off()
+        else
+            backlight_on()
+           
+        """
+
+    def key_pressed (self):
+        pass
+        
+        """
+        if (backlight_is_off): 
+            backlight_on()
+            set time for switch off, eg time() + 60 s
+            return True
+        else:
+            return False  # event pygame.MOUSEBUTTONDOWN has to processed by caller
+
+        """
+
 
 
 ###############################################################################
