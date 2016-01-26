cf1d33eb616c62cc2d818b2c9c2efb892c702407
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-01-26 18:57:00 +0100

    added (almost) real timings

--- playground/trafficlight.py
+++ playground/trafficlight.py
@@ -6,9 +6,9 @@ import RPi.GPIO as io
 from time import sleep
 
 
-pin_red    = 1
-pin_orange = 2
-pin_green  = 3
+pin_red    = 15
+pin_orange = 36
+pin_green  = 33
 
 
 io.setmode(io.BOARD)
@@ -16,25 +16,29 @@ io.setup(pin_red, io.OUT)
 io.setup(pin_orange, io.OUT)
 io.setup(pin_green, io.OUT)
 
+io.output(pin_green, 0)
+io.output(pin_orange, 0)
+io.output(pin_red, 0)
+
 
 def GoRed ():
     io.output(pin_green, 1)
-    sleep(1)
+    sleep(0.5)
     io.output(pin_green, 0)
-    sleep(1)
+    sleep(0.5)
     io.output(pin_green, 1)
-    sleep(1)
+    sleep(0.5)
     io.output(pin_green, 0)
-    sleep(1)
+    sleep(0.5)
     io.output(pin_green, 1)
-    sleep(1)
+    sleep(0.5)
     io.output(pin_green, 0)
-    sleep(1)
+    sleep(0.5)
     io.output(pin_green, 1)
-    sleep(1)
+    sleep(0.5)
     io.output(pin_green, 0)
     io.output(pin_orange, 1)
-    sleep(1)
+    sleep(2)
     io.output(pin_orange, 0)
     io.output(pin_red, 1)
 
@@ -42,7 +46,7 @@ def GoRed ():
 
 def GoGreen ():
     io.output(pin_orange, 1)
-    sleep(1)
+    sleep(2)
     io.output(pin_orange, 0)
     io.output(pin_red, 0)
     io.output(pin_green, 1)
