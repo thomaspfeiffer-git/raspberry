05c5807a5a074018aaa7d47a8ba8ca8a4dbfb57d
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-02-07 21:08:37 +0100

    some code polishing

--- playground/PCF8591.py
+++ playground/PCF8591.py
@@ -7,8 +7,6 @@
 """controls A/D converter PCF8591"""
 
 
-
-
 import smbus
 from threading import Lock
 import time
@@ -30,30 +28,32 @@ class I2C (object):
                 raise ValueError("Lock already set!")
 
 
-# Taken from https://github.com/jacotronix/net.jacotronix.python.PCF8591P/blob/master/PCF8591P/PCF8591P.py
 class PCF8591 (I2C):
     def __init__ (self, address, lock=None):
         super().__init__(lock)
         self._address = address
 
-    def readchannel (self, channel=0):
+    def read (self, channel=0):
         with self._lock:
             self._bus.write_byte(self._address, 0x40|channel)
             ack = self._bus.read_byte(self._address)  # don't use ack
             return self._bus.read_byte(self._address) 
+
+    def write (self, value):
+        raise NotImplementedError
 
 
 if __name__ == '__main__':
     adc = PCF8591(0x48)
 
     while True:
-        result = adc.readchannel(0)
+        result = adc.read(channel=0)
         print("Result 0: %s" % result)
-        result = adc.readchannel(1)
+        result = adc.read(channel=1)
         print("Result 1: %s" % result)
-        result = adc.readchannel(2)
+        result = adc.read(channel=2)
         print("Result 2: %s" % result)
-        result = adc.readchannel(3)
+        result = adc.read(channel=3)
         print("Result 3: %s" % result)
         print("--------------------")
         time.sleep(0.5)
