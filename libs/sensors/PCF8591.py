12dd0d7a78a29adbdda1bf5645a188d774b9a6f7
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-02-14 07:56:43 +0100

    added exception handling

--- libs/sensors/PCF8591.py
+++ libs/sensors/PCF8591.py
@@ -6,17 +6,28 @@
 """controls A/D converter PCF8591"""
 
 from i2c import I2C
+from time import localtime
 
 class PCF8591 (I2C):
     def __init__ (self, address, lock=None):
         super().__init__(lock)
-        self._address = address
+        self._address    = address
+        self.__lastvalue = 0
 
     def read (self, channel=0):
         with I2C._lock:
-            I2C._bus.write_byte(self._address, 0x40|channel)
-            ack = I2C._bus.read_byte(self._address)  # don't use ack
-            return I2C._bus.read_byte(self._address) 
+            try:
+                I2C._bus.write_byte(self._address, 0x40|channel)
+                _ = I2C._bus.read_byte(self._address) # seems to be a quite useless acknowledge
+                res = I2C._bus.read_byte(self._address) 
+                self.__lastvalue = res
+
+            except (IOError, OSError):
+                print(localtime()[3:6], "error reading/writing i2c bus")
+
+            finally:
+                return self.__lastvalue
+
 
     def write (self, value):
         raise NotImplementedError
