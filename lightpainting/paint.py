4ed11301bfc1be64f56d753191fc89653837ef3a
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-02-26 21:37:13 +0100

    added resizeing

--- lightpainting/paint.py
+++ lightpainting/paint.py
@@ -4,38 +4,48 @@
 # Adafruit Digital Addressable RGB LED flex strip.
 # ----> http://adafruit.com/products/306 
 
-
 import RPi.GPIO as GPIO, Image, time
+
+
+LED_COUNT = 64
+
  
 # Configurable values
-filename  = "images/wh_logo_64px_blackcyan.png"
+# filename  = "images/wh_logo_64px_blackcyan.png"
+filename  = "images/DonaldDuck.png"
 dev       = "/dev/spidev0.0"
  
 # Open SPI device, load image in RGB format and get dimensions:
 spidev    = file(dev, "wb")
-print "Loading..."
+print "Loading ..."
 img       = Image.open(filename).convert("RGB")
 pixels    = img.load()
 width     = img.size[0]
 height    = img.size[1]
-print "%dx%d pixels" % img.size
-# To do: add resize here if image is not desired height
+print "   %dx%d pixels" % img.size
+
+print "Resizing ..."
+newWidth = float(img.size[0])/float(img.size[1])*LED_COUNT
+img = img.resize( (int(newWidth), LED_COUNT))
+print "   %dx%d pixels" % img.size
+
  
 # Calculate gamma correction table.  This includes
 # LPD8806-specific conversion (7-bit color w/high bit set).
+print "Gamma correcting ..."
 gamma = bytearray(256)
 for i in range(256):
         gamma[i] = 0x80 | int(pow(float(i) / 255.0, 2.5) * 127.0 + 0.5)
  
 # Create list of bytearrays, one for each column of image.
 # R, G, B byte per pixel, plus extra '0' byte at end for latch.
-print "Allocating..."
+print "Allocating ..."
 column = [0 for x in range(width)]
 for x in range(width):
         column[x] = bytearray(height * 3 + 1)
  
 # Convert 8-bit RGB image into column-wise GRB bytearray list.
-print "Converting..."
+print "Converting ..."
 for x in range(width):
         for y in range(height):
                 value = pixels[x, y]
@@ -46,7 +56,7 @@ for x in range(width):
  
  
 # Then it's a trivial matter of writing each column to the SPI port.
-print "Displaying..."
+print "Displaying ..."
 while True:
         for x in range(width):
                 spidev.write(column[x])
