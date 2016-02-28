dcd519379b20227a75fbf2f63fbed9e932f8ce83
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-02-28 16:16:52 +0100

    some code polishing

--- lightpainting/paint.py
+++ lightpainting/paint.py
@@ -9,7 +9,16 @@ import RPi.GPIO as GPIO, Image, time
 
 LED_COUNT = 64
 
- 
+# Sleep time between columns. Has to be adjusted according speed of LED bar.
+COLUMN_SLEEP  = 0.1
+
+# Repeat display of picture. Mainly used for testing.
+PICTURE_REPEAT = True
+
+# Sleep time between pictures.
+PICTURE_SLEEP  = 1.0
+
+
 # Configurable values
 filename = "images/testbild.png"
 # filename  = "images/wh_logo_64px_blackcyan.png"
@@ -20,12 +29,14 @@ dev       = "/dev/spidev0.0"
 # Open SPI device, load image in RGB format and get dimensions:
 spidev    = file(dev, "wb")
 
+
 print "Loading ..."
 img     = Image.open(filename).convert("RGB")
 img     = img.rotate(180)
 width   = img.size[0]
 height  = img.size[1]
 print "   %dx%d pixels" % (width, height)
+
 
 print "Resizing ..."
 newWidth = float(img.size[0])/float(img.size[1])*LED_COUNT
@@ -34,40 +45,45 @@ width   = img.size[0]
 height  = img.size[1]
 pixels  = img.load()
 print "   %dx%d pixels" % (width, height)
+
  
 # Calculate gamma correction table.  This includes
 # LPD8806-specific conversion (7-bit color w/high bit set).
 print "Gamma correcting ..."
 gamma = bytearray(256)
 for i in range(256):
-        gamma[i] = 0x80 | int(pow(float(i) / 255.0, 2.5) * 127.0 + 0.5)
+    gamma[i] = 0x80 | int(pow(float(i) / 255.0, 2.5) * 127.0 + 0.5)
+
  
 # Create list of bytearrays, one for each column of image.
 # R, G, B byte per pixel, plus extra '0' byte at end for latch.
 print "Allocating ..."
 column = [0 for x in range(width)]
 for x in range(width):
-        column[x] = bytearray(height * 3 + 1)
+    column[x] = bytearray(height * 3 + 1)
+
  
 # Convert 8-bit RGB image into column-wise GRB bytearray list.
 print "Converting ..."
 for x in range(width):
-        for y in range(height):
-                value = pixels[x, y]
-                y3 = y * 3
-                column[x][y3]     = gamma[value[1]]
-                column[x][y3 + 1] = gamma[value[0]]
-                column[x][y3 + 2] = gamma[value[2]]
+    for y in range(height):
+        value = pixels[x, y]
+        y3 = y * 3
+        column[x][y3]     = gamma[value[1]]
+        column[x][y3 + 1] = gamma[value[0]]
+        column[x][y3 + 2] = gamma[value[2]]
  
- 
+
 # Then it's a trivial matter of writing each column to the SPI port.
 print "Displaying ..."
 while True:
-        for x in range(width):
-                spidev.write(column[x])
-                spidev.flush()
-                time.sleep(0.1)
-        time.sleep(0.5)
+    for x in range(width):
+        spidev.write(column[x])
+        spidev.flush()
+        time.sleep(COLUMN_SLEEP)
+    time.sleep(PICTURE_SLEEP)
+    if not PICTURE_REPEAT then:
+        break
 
-
+# eof #
 
