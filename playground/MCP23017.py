d4c24656b9509a59c48236ce3b409a57c0f3c30b
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-03-10 21:34:22 +0100

    some code polishing

--- playground/MCP23017.py
+++ playground/MCP23017.py
@@ -1,6 +1,8 @@
 ################################################################################
 # MCP23017                                                                     #
 # Communication with MCP23017                                                  #
+# Parts of this source code come from                                          #
+# https://github.com/BLavery/lib_mcp23017/blob/master/lib_mcp23017.py          #
 # (c) https://github.com/thomaspfeiffer-git 2015, 2016                         #
 ################################################################################
 """control component MCP23017"""
@@ -12,30 +14,51 @@ from MCP23x17 import MCP23x17
 
 class MCP23017 (I2C):
     """control component MCP23017"""
-    def __init__ (self, address, lock=None):
+    """ioA, ioB: 8 bits, 0: port is output
+                         1: port is input"""
+    def __init__ (self, address, ioA, ioB, lock=None):
         super().__init__(lock)
         self._address = address
 
-        # Set port direction to output (0b00000000)
-        self.send(MCP23x17.IODIRA, 0b00000000)
-        self.send(MCP23x17.IODIRB, 0b00000000)
+        # Set port directions
+        self.send(MCP23x17.IODIRA, ioA)
+        self.send(MCP23x17.IODIRB, ioB)
+
+        # Set pullup resistors
+        self.send(MCP23x17.GPPUA, ioA)
+        self.send(MCP23x17.GPPUB, ioB)
+
+    @staticmethod
+    def _BV(x):
+        return 1<<x
 
     def send (self, bank, pattern):
         """send pattern to bank of device"""
+        # TODO: Test what happens if writing to a bank with 
+        # mixed input/output bits.
+        # TODO: switch to bits/patterns without dedicated banks
         i = 0
         while (i < 3):
             try:
                 with I2C._lock:
                     I2C._bus.write_byte_data(self._address, bank, pattern)
+                    break
             except IOError:
                 print(localtime()[3:6], "Device:", hex(device), "Bank:", hex(bank), "Pattern:", bin(pattern))
                 i += 1
                 print("Retry: %i" % i)
                 continue
-            break
 
-    def read (self):
-        raise NotImplementedError
+    def read (self, bit):
+        """reads bit; bit is int 0..15"""
+
+        port = (bit >> 3) + MCP23x17.GPIOA 
+        bit &= 7           # = always 0-7
+        try:
+            with I2C._lock:
+                return (I2C._bus.read_byte_data(self._address, port) & self._BV(bit)) == 0
+        except IOError:
+            print("Could not read MCP23017 device at address", self._address)
 
 ### eof ###
 
