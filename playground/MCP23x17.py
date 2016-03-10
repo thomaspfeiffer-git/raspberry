948f543b0db9a5e9423ccde65e7048a9b578b65e
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-03-10 19:28:12 +0100

    added some MCP23x17 constants

--- playground/MCP23x17.py
+++ playground/MCP23x17.py
@@ -1,21 +1,24 @@
 ################################################################################
 # MCP23x17                                                                     #
-# PINs for MCP23x17 components                                                 #
+# Constantss for MCP23x17 components                                           #
 # Use with io.setmode(io.BOARD)                                                #
-# (c) https://github.com/thomaspfeiffer-git May 2015                           #
+# (c) https://github.com/thomaspfeiffer-git 2016                               #
 ################################################################################
-"""PINs for MCP23x17 components"""
+"""Constants for MCP23x17 components"""
 
 class MCP23x17:
     IODIRA      = 0x00 # Pin direction register
     IODIRB      = 0x01 # Pin direction register
     IOCONA      = 0x0A # MCP23S17 needs hardware addressing explicitly enabled.
     IOCONB      = 0x0B # MCP23S17 needs hardware addressing explicitly enabled.
+    GPPUA       = 0x0C # Use internal pullup resistor
+    GPPUB       = 0x0D # use internal pullup resistor
+    GPIOA       = 0x12 # Register for inputs
+    GPIOA       = 0x13 # Register for inputs
     OLATA       = 0x14 # Register for outputs
     OLATB       = 0x15 # Register for outputs
 
     HAEN        = 0b00001000
-
 
 ### eof ###
 
