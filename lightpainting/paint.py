605a6114a09dcda734dc9c766628e195f0361991
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-03-18 21:37:48 +0100

    added signal handling for ctrl-c and some code polishing

--- lightpainting/paint.py
+++ lightpainting/paint.py
@@ -4,11 +4,15 @@
 # Adafruit Digital Addressable RGB LED flex strip.
 # ----> http://adafruit.com/products/306 
 
-import getopt, sys
-import RPi.GPIO as GPIO, Image, time
+import getopt 
+# import Image
+import RPi.GPIO as GPIO
+import sys
+import time
 
+
+# Number of LEDs in the LED bar
 LED_COUNT = 64
-
 
 # Wait time until first iteration starts.
 # Needed to move from camera to led strip. :-)
@@ -21,14 +25,13 @@ DELAYTOSTART = 5.0
 COLUMN_SLEEP  = 0.1
 
 # Repeat display of picture. 
-# Mainly used for testing.
+# Can be set using the command line parameter -l
 PICTURE_REPEAT = False
 
 # Sleep time between pictures (if PICTURE_REPEAT == True).
 PICTURE_SLEEP  = 1.0
 
 
-# Configurable values
 # filename = "images/testbild.png"
 # filename = "images/wh_logo_64px_blackcyan.png"
 # filename = "images/DonaldDuck.png"
@@ -36,7 +39,6 @@ PICTURE_SLEEP  = 1.0
 
 
 dev = "/dev/spidev0.0"
-spidev = file(dev, "wb")
 
 
 def usage():
@@ -102,70 +104,105 @@ def blackColumn():
     writeColumn(blackcolumn)
 
 
-
-#### main ####
-
-filename, delaytostart, column_sleep, picture_repeat = readCommandLine()
-
-print "Loading ..."
-img     = Image.open(filename).convert("RGB")
-img     = img.rotate(180)
-width   = img.size[0]
-height  = img.size[1]
-print "   %dx%d pixels" % (width, height)
-
-
-print "Resizing ..."
-newWidth = float(img.size[0])/float(img.size[1])*LED_COUNT
-img = img.resize( (int(newWidth), LED_COUNT))
-width   = img.size[0]
-height  = img.size[1]
-pixels  = img.load()
-print "   %dx%d pixels" % (width, height)
-
- 
-# Calculate gamma correction table.  This includes
-# LPD8806-specific conversion (7-bit color w/high bit set).
-print "Gamma correcting ..."
-gamma = bytearray(256)
-for i in range(256):
-    gamma[i] = 0x80 | int(pow(float(i) / 255.0, 2.5) * 127.0 + 0.5)
-
- 
-# Create list of bytearrays, one for each column of image.
-# R, G, B byte per pixel, plus extra '0' byte at end for latch.
-print "Allocating ..."
-column = [0 for x in range(width)]
-for x in range(width):
-    column[x] = bytearray(height * 3 + 1)
-
- 
-# Convert 8-bit RGB image into column-wise GRB bytearray list.
-print "Converting ..."
-for x in range(width):
-    for y in range(height):
-        value = pixels[x, y]
-        y3 = y * 3
-        column[x][y3]     = gamma[value[1]]
-        column[x][y3 + 1] = gamma[value[0]]
-        column[x][y3 + 2] = gamma[value[2]]
- 
-
-print "Waiting for start ..."
-time.sleep(delaytostart)
-
-# Then it's a trivial matter of writing each column to the SPI port.
-print "Displaying ..."
-while True:
-    for x in range(width):
-        writeColumn(column[x])
-        time.sleep(column_sleep)
-        print "Looping %i for picture %s" % (x, filename)
-
+###############################################################################
+# Exit ########################################################################
+def Exit():
+    """stuff to be done on exit"""
     blackColumn()
-    if not picture_repeat:
-        break
-    time.sleep(PICTURE_SLEEP)
+    sys.exit()
+
+def _Exit(__s, __f):
+    """exit for signal handler"""
+    Exit()
+
+
+###############################################################################
+# Main ########################################################################
+def Main();
+
+    filename, delaytostart, column_sleep, picture_repeat = readCommandLine()
+    spidev = file(dev, "wb")
+
+    print "Loading ..."
+    img     = Image.open(filename).convert("RGB")
+    img     = img.rotate(180)
+    width   = img.size[0]
+    height  = img.size[1]
+    print "   %dx%d pixels" % (width, height)
+
+
+    print "Resizing ..."
+    newWidth = float(img.size[0])/float(img.size[1])*LED_COUNT
+    img = img.resize( (int(newWidth), LED_COUNT))
+    width   = img.size[0]
+    height  = img.size[1]
+    pixels  = img.load()
+    print "   %dx%d pixels" % (width, height)
+
+ 
+    # Calculate gamma correction table.  This includes
+    # LPD8806-specific conversion (7-bit color w/high bit set).
+    print "Gamma correcting ..."
+    gamma = bytearray(256)
+    for i in range(256):
+        gamma[i] = 0x80 | int(pow(float(i) / 255.0, 2.5) * 127.0 + 0.5)
+
+ 
+    # Create list of bytearrays, one for each column of image.
+    # R, G, B byte per pixel, plus extra '0' byte at end for latch.
+    print "Allocating ..."
+    column = [0 for x in range(width)]
+    for x in range(width):
+        column[x] = bytearray(height * 3 + 1)
+
+ 
+    # Convert 8-bit RGB image into column-wise GRB bytearray list.
+    print "Converting ..."
+    for x in range(width):
+        for y in range(height):
+            value = pixels[x, y]
+            y3 = y * 3
+            column[x][y3]     = gamma[value[1]]
+            column[x][y3 + 1] = gamma[value[0]]
+            column[x][y3 + 2] = gamma[value[2]]
+ 
+
+    print "Waiting for start ..."
+    time.sleep(delaytostart)
+
+    # Then it's a trivial matter of writing each column to the SPI port.
+    print "Displaying ..."
+    while True:
+        for x in range(width):
+            writeColumn(column[x])
+            time.sleep(column_sleep)
+            print "Looping %i for picture %s" % (x, filename)
+
+        blackColumn()
+        if not picture_repeat:
+            break
+        time.sleep(PICTURE_SLEEP)
+
+
+###############################################################################
+###############################################################################
+if __name__ == '__main__':
+    signal.signal(signal.SIGTERM, _Exit)
+
+try:
+    Main()
+
+    except KeyboardInterrupt:
+        Exit()
+
+    except SystemExit:              # Done in signal handler (method _Exit()) #
+        pass
+
+    except:
+        print(traceback.print_exc())
+
+    finally:    # All cleanup is done in KeyboardInterrupt or signal handler. #
+        pass
 
 # eof #
 
