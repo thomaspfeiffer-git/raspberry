015f217ebfdecc408ac45bbf95ddfed463438e54
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-09-15 18:07:06 +0200

    some small first steps

--- kollerberg/weather.py
+++ kollerberg/weather.py
@@ -1,1 +1,19 @@
-kkkk
+#!/usr/bin/python3
+
+from socket import gethostname
+
+
+# Hosts where this app runs
+pik_i = "pik_i"
+pik_a = "pik_a"
+pik_k = "pik_k"
+PIs = [pik_i, pik_a, pik_k]
+
+
+# Datenstruktur fuer diverse Namen, zB in der rrd erstellen
+
+
+this_PI = gethostname()
+
+if this_PI not in PIs:
+   print("falscher host!")
