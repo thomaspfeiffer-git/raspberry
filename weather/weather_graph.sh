2cdd2f2870e943edaf737801a9f662bbd07703cb
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2016-09-17 18:21:26 +0200

    added weather data from Kollerberg

--- weather/weather_graph.sh
+++ weather/weather_graph.sh
@@ -3,6 +3,7 @@
 RRDPATH=/schild/weather/
 RRD=$RRDPATH/weather.rrd
 RRD_K=$RRDPATH/weather_kidsroom.rrd
+RRD_KB=$RRDPATH/weather_kollerberg.rrd
 
 
 PNG_TEMP_D=$RRDPATH/weather_temp_d.png
@@ -57,12 +58,15 @@ printTemp ()
     DEF:temp_indoor=$RRD:temp_indoor:AVERAGE                 \
     DEF:temp_window=$RRD:temp_4:AVERAGE                      \
     DEF:kidsroom_temp1=$RRD_K:kidsroom_temp1:AVERAGE         \
-    LINE1:temp_outdoor#0000FF:"Temperatur außen          "   \
+    DEF:kb_i_t1=$RRD_KB:kb_i_t1:AVERAGE                      \
+    DEF:kb_a_t1=$RRD_KB:kb_a_t1:AVERAGE                      \
+    DEF:kb_k_t1=$RRD_KB:kb_k_t1:AVERAGE                      \
+    LINE1:temp_outdoor#0000FF:"Temperatur Wien außen          "   \
     GPRINT:temp_outdoor:LAST:"Aktuell\: %5.2lf °C"           \
     GPRINT:temp_outdoor:AVERAGE:"Mittelwert\: %5.2lf °C"     \
     GPRINT:temp_outdoor:MAX:"Max\: %5.2lf °C"                \
     GPRINT:temp_outdoor:MIN:"Min\: %5.2lf °C\n"              \
-    LINE1:temp_realoutdoor#666633:"Temperatur ganz außen     " \
+    LINE1:temp_realoutdoor#666633:"Temperatur Wien ganz außen     " \
     GPRINT:temp_realoutdoor:LAST:"Aktuell\: %5.2lf °C"       \
     GPRINT:temp_realoutdoor:AVERAGE:"Mittelwert\: %5.2lf °C" \
     GPRINT:temp_realoutdoor:MAX:"Max\: %5.2lf °C"            \
@@ -81,7 +85,22 @@ printTemp ()
     GPRINT:kidsroom_temp1:LAST:"Aktuell\: %5.2lf °C"         \
     GPRINT:kidsroom_temp1:AVERAGE:"Mittelwert\: %5.2lf °C"   \
     GPRINT:kidsroom_temp1:MAX:"Max\: %5.2lf °C"              \
-    GPRINT:kidsroom_temp1:MIN:"Min\: %5.2lf °C\n"
+    GPRINT:kidsroom_temp1:MIN:"Min\: %5.2lf °C\n"            \
+    LINE1:kb_i_t1#40FF00:"Temperatur Kollerberg innen   "    \
+    GPRINT:kb_i_t1:LAST:"Aktuell\: %5.2lf °C"                \
+    GPRINT:kb_i_t1:AVERAGE:"Mittelwert\: %5.2lf °C"          \
+    GPRINT:kb_i_t1:MAX:"Max\: %5.2lf °C"                     \
+    GPRINT:kb_i_t1:MIN:"Min\: %5.2lf °C\n"                   \
+    LINE1:kb_a_t1#009900:"Temperatur Kollerberg außen   "    \
+    GPRINT:kb_a_t1:LAST:"Aktuell\: %5.2lf °C"                \
+    GPRINT:kb_a_t1:AVERAGE:"Mittelwert\: %5.2lf °C"          \
+    GPRINT:kb_a_t1:MAX:"Max\: %5.2lf °C"                     \
+    GPRINT:kb_a_t1:MIN:"Min\: %5.2lf °C\n"                   \
+    LINE1:kb_k_t1#663300:"Temperatur Kollerberg Keller  "    \
+    GPRINT:kb_k_t1:LAST:"Aktuell\: %5.2lf °C"                \
+    GPRINT:kb_k_t1:AVERAGE:"Mittelwert\: %5.2lf °C"          \
+    GPRINT:kb_k_t1:MAX:"Max\: %5.2lf °C"                     \
+    GPRINT:kb_k_t1:MIN:"Min\: %5.2lf °C\n"
  }
 
 
@@ -101,16 +120,34 @@ printCPUTemp ()
     --right-axis 1:0                                                     \
     DEF:temp_cpu=$RRD:temp_cpu:AVERAGE                                   \
     DEF:kidsroom_tempcpu=$RRD_K:kidsroom_tempcpu:AVERAGE                 \
-    LINE1:temp_cpu#FF00FF:"Temperatur Raspberry Pi Wohnzimmer  "         \
-    GPRINT:temp_cpu:LAST:"\t Aktuell\: %5.2lf %%"                        \
-    GPRINT:temp_cpu:AVERAGE:"Mittelwert\: %5.2lf %%"                     \
-    GPRINT:temp_cpu:MAX:"Max\: %5.2lf %%"                                \
-    GPRINT:temp_cpu:MIN:"Min\: %5.2lf %%\n"                              \
-    LINE1:kidsroom_tempcpu#00ccff:"Temperatur Raspberry Pi Kinderzimmer" \
-    GPRINT:kidsroom_tempcpu:LAST:"\t Aktuell\: %5.2lf %%"                \
-    GPRINT:kidsroom_tempcpu:AVERAGE:"Mittelwert\: %5.2lf %%"             \
-    GPRINT:kidsroom_tempcpu:MAX:"Max\: %5.2lf %%"                        \
-    GPRINT:kidsroom_tempcpu:MIN:"Min\: %5.2lf %%\n" 
+    DEF:kb_i_tcpu=$RRD_KB:kb_i_tcpu:AVERAGE                              \
+    DEF:kb_a_tcpu=$RRD_KB:kb_a_tcpu:AVERAGE                              \
+    DEF:kb_k_tcpu=$RRD_KB:kb_k_tcpu:AVERAGE                              \
+    LINE1:temp_cpu#FF00FF:"Temperatur Raspberry Pi Wohnzimmer       "    \
+    GPRINT:temp_cpu:LAST:"\t Aktuell\: %5.2lf °C"                        \
+    GPRINT:temp_cpu:AVERAGE:"Mittelwert\: %5.2lf °C"                     \
+    GPRINT:temp_cpu:MAX:"Max\: %5.2lf °C"                                \
+    GPRINT:temp_cpu:MIN:"Min\: %5.2lf °C\n"                              \
+    LINE1:kidsroom_tempcpu#00ccff:"Temperatur Raspberry Pi Kinderzimmer     " \
+    GPRINT:kidsroom_tempcpu:LAST:"\t Aktuell\: %5.2lf °C"                \
+    GPRINT:kidsroom_tempcpu:AVERAGE:"Mittelwert\: %5.2lf °C"             \
+    GPRINT:kidsroom_tempcpu:MAX:"Max\: %5.2lf °C"                        \
+    GPRINT:kidsroom_tempcpu:MIN:"Min\: %5.2lf °C\n"                      \
+    LINE1:kb_i_tcpu#40FF00:"Temperatur Raspberry Pi Kollerberg innen "   \
+    GPRINT:kb_i_tcpu:LAST:"\t Aktuell\: %5.2lf °C"                       \
+    GPRINT:kb_i_tcpu:AVERAGE:"Mittelwert\: %5.2lf °C"                    \
+    GPRINT:kb_i_tcpu:MAX:"Max\: %5.2lf °C"                               \
+    GPRINT:kb_i_tcpu:MIN:"Min\: %5.2lf °C\n"                             \
+    LINE1:kb_a_tcpu#009900:"Temperatur Raspberry Pi Kollerberg außen "   \
+    GPRINT:kb_a_tcpu:LAST:"\t Aktuell\: %5.2lf °C"                       \
+    GPRINT:kb_a_tcpu:AVERAGE:"Mittelwert\: %5.2lf °C"                    \
+    GPRINT:kb_a_tcpu:MAX:"Max\: %5.2lf °C"                               \
+    GPRINT:kb_a_tcpu:MIN:"Min\: %5.2lf °C\n"                             \
+    LINE1:kb_k_tcpu#663300:"Temperatur Raspberry Pi Kollerberg Keller"   \
+    GPRINT:kb_k_tcpu:LAST:"\t Aktuell\: %5.2lf °C"                       \
+    GPRINT:kb_k_tcpu:AVERAGE:"Mittelwert\: %5.2lf °C"                    \
+    GPRINT:kb_k_tcpu:MAX:"Max\: %5.2lf °C"                               \
+    GPRINT:kb_k_tcpu:MIN:"Min\: %5.2lf °C\n" 
  }
 
 
@@ -132,7 +169,10 @@ printHumidity ()
     DEF:humi_outdoor=$RRD:humi_outdoor:AVERAGE                 \
     DEF:humi_indoor=$RRD:humi_indoor:AVERAGE                   \
     DEF:kidsroom_humi=$RRD_K:kidsroom_humi:AVERAGE             \
-    LINE1:humi_outdoor#0000FF:"Luftfeuchtigkeit außen       "  \
+    DEF:kb_i_humi=$RRD_KB:kb_i_humi:AVERAGE                    \
+    DEF:kb_a_humi=$RRD_KB:kb_a_humi:AVERAGE                    \
+    DEF:kb_k_humi=$RRD_KB:kb_k_humi:AVERAGE                    \
+    LINE1:humi_outdoor#0000FF:"Luftfeuchtigkeit Wien außen  "  \
     GPRINT:humi_outdoor:LAST:"\t Aktuell\: %5.2lf %%"          \
     GPRINT:humi_outdoor:AVERAGE:"Mittelwert\: %5.2lf %%"       \
     GPRINT:humi_outdoor:MAX:"Max\: %5.2lf %%"                  \
@@ -146,7 +186,22 @@ printHumidity ()
     GPRINT:kidsroom_humi:LAST:"\t Aktuell\: %5.2lf %%"         \
     GPRINT:kidsroom_humi:AVERAGE:"Mittelwert\: %5.2lf %%"      \
     GPRINT:kidsroom_humi:MAX:"Max\: %5.2lf %%"                 \
-    GPRINT:kidsroom_humi:MIN:"Min\: %5.2lf %%\n"
+    GPRINT:kidsroom_humi:MIN:"Min\: %5.2lf %%\n"               \
+    LINE1:kb_i_humi#40FF00:"Luftfeuchtigkeit Kollerberg innen" \
+    GPRINT:kb_i_humi:LAST:"\t Aktuell\: %5.2lf %%"             \
+    GPRINT:kb_i_humi:AVERAGE:"Mittelwert\: %5.2lf %%"          \
+    GPRINT:kb_i_humi:MAX:"Max\: %5.2lf %%"                     \
+    GPRINT:kb_i_humi:MIN:"Min\: %5.2lf %%\n"                   \
+    LINE1:kb_a_humi#009900:"Luftfeuchtigkeit Kollerberg außen" \
+    GPRINT:kb_a_humi:LAST:"\t Aktuell\: %5.2lf %%"             \
+    GPRINT:kb_a_humi:AVERAGE:"Mittelwert\: %5.2lf %%"          \
+    GPRINT:kb_a_humi:MAX:"Max\: %5.2lf %%"                     \
+    GPRINT:kb_a_humi:MIN:"Min\: %5.2lf %%\n"                   \
+    LINE1:kb_k_humi#663300:"Luftfeuchtigkeit Kollerberg Keller" \
+    GPRINT:kb_k_humi:LAST:"\t Aktuell\: %5.2lf %%"             \
+    GPRINT:kb_k_humi:AVERAGE:"Mittelwert\: %5.2lf %%"          \
+    GPRINT:kb_k_humi:MAX:"Max\: %5.2lf %%"                     \
+    GPRINT:kb_k_humi:MIN:"Min\: %5.2lf %%\n"
  }
 
 
@@ -169,11 +224,17 @@ printAirPressure ()
     --right-axis 1:0                                     \
     --right-axis-format "%4.0lf"                         \
     DEF:air_pressure=$RRD:air_pressure:AVERAGE           \
-    LINE1:air_pressure#40FF00:"Luftdruck"                \
+    DEF:kb_i_press=$RRD_KB:kb_i_press:AVERAGE            \
+    LINE1:air_pressure#0000FF:"Luftdruck Wien       "    \
     GPRINT:air_pressure:LAST:"\t Aktuell\: %5.2lf hPa"   \
     GPRINT:air_pressure:AVERAGE:"Mittelwert\: %5.2lf hPa" \
     GPRINT:air_pressure:MAX:"Max\: %5.2lf hPa"           \
-    GPRINT:air_pressure:MIN:"Min\: %5.2lf hPa\n"
+    GPRINT:air_pressure:MIN:"Min\: %5.2lf hPa\n"         \
+    LINE1:kb_i_press#40FF00:"Luftdruck Kollerberg "      \
+    GPRINT:kb_i_press:LAST:"\t Aktuell\: %5.2lf hPa"     \
+    GPRINT:kb_i_press:AVERAGE:"Mittelwert\: %5.2lf hPa"  \
+    GPRINT:kb_i_press:MAX:"Max\: %5.2lf hPa"             \
+    GPRINT:kb_i_press:MIN:"Min\: %5.2lf hPa\n"
  }
 
 
