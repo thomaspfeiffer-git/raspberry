b089b4847da407d2975342f8bc5699b6b0ec41a4
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2016-09-17 15:10:05 +0200

    added 'writeData()'

--- kollerberg/weather.py
+++ kollerberg/weather.py
@@ -6,10 +6,12 @@
 #############################################################################
 """Weather station at our summer cottage"""
 
+import datetime
+import rrdtool
 import signal
 from socket import gethostname
 import sys
-from time import sleep
+from time import strftime, localtime, sleep, time
 import traceback
 
 sys.path.append('../libs')
@@ -25,9 +27,9 @@ pik_i = "pik_i"
 pik_a = "pik_a"
 pik_k = "pik_k"
 PIs = [pik_i, pik_a, pik_k]
-
 this_PI = gethostname()
-if this_PI == pik_i:
+
+if this_PI == pik_i:   # BMP085 installed only at pik_i
     from BMP085 import BMP085
 
 
@@ -35,11 +37,7 @@ AddressesDS1820 = { pik_i: "/sys/bus/w1/devices/w1_bus_master1/28-000006de80e2/w
                     pik_a: "/sys/bus/w1/devices/w1_bus_master1/28-000006dd6ac1/w1_slave",
                     pik_k: "" }
 
-
 DHT22_AM2302_PIN = 14
-
-
-
 
 
 # Misc for rrdtool
@@ -68,6 +66,20 @@ DS = { pik_i: { DS_TEMP1: 'kb_i_t1',
                 DS_PRESS: 'kb_k_press' }
      } 
 
+DATAFILES = { pik_i: "/share/kb_i_weather.data",
+              pik_a: "/share/kb_a_weather.data",
+              pik_k: "/share/kb_k_weather.data" }
+
+
+###############################################################################
+# writeData ###################################################################
+def writeData(rrd_data):
+    """write various data to a file which is transferred to schild.smtp.at with crontab"""
+    with open(DATAFILES[this_PI], 'w') as f:
+        ts = time()
+        f.write(datetime.datetime.fromtimestamp(ts).strftime("%Y%m%d%H%M%S:") + str(ts) + ":" + rrd_data)
+
+
 
 ###############################################################################
 # Main ########################################################################
@@ -78,12 +90,11 @@ def main():
         print("wrong host!")
         _exit()
 
-
-    tempdht  = DHT22_AM2302(DHT22_AM2302_PIN)
-    tempds   = DS1820(AddressesDS1820[this_PI])
-    tempcpu  = CPU()
+    tempdht = DHT22_AM2302(DHT22_AM2302_PIN)
+    tempds  = DS1820(AddressesDS1820[this_PI])
+    tempcpu = CPU()
     if this_PI == pik_i:
-        bmp085  = BMP085()
+        bmp085 = BMP085()
 
     rrd_template = DS[this_PI][DS_TEMP1] + ":" + \
                    DS[this_PI][DS_TEMP2] + ":" + \
@@ -106,7 +117,9 @@ def main():
                     ":{:.2f}".format(pressure)
         # rrdtool.update(DATAFILE, "--template", rrd_template, rrd_data) 
         print(rrd_template)
-        print(rrd_data) 
+        print("%s %s" % (strftime("%Y%m%d%H%M%S", localtime()), rrd_data))
+
+        writeData(rrd_data)
 
         sleep(30)
 
