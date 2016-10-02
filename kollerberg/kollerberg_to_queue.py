1216ac7ea2f45da47fcb8a6ff1116473312b8e55
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2016-10-02 15:31:08 +0200

    checkin for safety reasons (aka backup)

--- kollerberg/kollerberg_to_queue.py
+++ kollerberg/kollerberg_to_queue.py
@@ -10,13 +10,51 @@
 
 import signal
 import sys
+from threading import Lock
 from time import sleep
 import traceback
+
+sys.path.append('../libs')
+from SensorQueue import SensorQueueClient_write
+from SensorValue import SensorValueLock, SensorValue
+
+
+
+pik_i = "pik_i"
+pik_a = "pik_a"
+pik_k = "pik_k"
+PIs = [pik_i, pik_a, pik_k]
+
+DATAFILES = { pik_i: "/schild/weather/kb_i_weather.data",
+              pik_a: "/schild/weather/kb_a_weather.data",
+              pik_k: "/schild/weather/kb_k_weather.data" }
+
+
+
+def getDataFromFile (filename):
+    with open(filename, 'r') as f:
+        return f.read().split(':')
 
 
 ################################################################################
 # Main #########################################################################
 def Main():
+    qv_kb_i_t = SensorValueLock("ID_2?", "Temp KB indoor", SensorValue.Types.Temp, u'°C', Lock())
+    qv_kb_i_h = SensorValueLock("ID_2?", "Humi KB indoor", SensorValue.Types.Humi, u'% rF', Lock())
+    qv_kb_p   = SensorValueLock("ID_2?", "Pressure KB",    SensorValue.Types.Pressure, u'hPa', Lock())
+
+    qv_kb_a_t = SensorValueLock("ID_2?", "Temp KB outdoor", SensorValue.Types.Temp, u'°C', Lock())
+    qv_kb_a_h = SensorValueLock("ID_2?", "Humi KB outdoor", SensorValue.Types.Humi, u'% rF', Lock())
+
+    qv_kb_k_t = SensorValueLock("ID_2?", "Temp KB basement", SensorValue.Types.Temp, u'°C', Lock())
+    qv_kb_k_h = SensorValueLock("ID_2?", "Humi KB basement", SensorValue.Types.Humi, u'% rF', Lock())
+
+
+    sq.register(qvalue_temp)
+    sq.register(qvalue_humi)
+    sq.start()
+
+
     while True:
         # read data from files:
         #   ID_21: temp outdoor
@@ -29,7 +67,21 @@ def Main():
 
         # write data into queue
 
-        sleep 60
+        data = getDataFromFile(DATAFILES[pik_i])
+        kb_i_t = data[3]
+        kb_i_h = data[6]
+        kb_i_p = data[7]
+
+        data = getDataFromFile(DATAFILES[pik_a])
+        kb_a_t = data[3]
+        kb_a_h = data[6]
+
+        data = getDataFromFile(DATAFILES[pik_k])
+        kb_k_t = data[3]
+        kb_k_h = data[6]
+
+
+        sleep(60)
 
 
 
@@ -37,6 +89,8 @@ def Main():
 # Exit ########################################################################
 def Exit():
     """cleanup stuff"""
+    sq.stop()
+    sq.join()
     sys.exit()
 
 def _Exit(__s, __f):
@@ -50,6 +104,7 @@ if __name__ == '__main__':
     signal.signal(signal.SIGTERM, _Exit)
 
     try:
+        sq = SensorQueueClient_write()
         Main()
 
     except KeyboardInterrupt:
