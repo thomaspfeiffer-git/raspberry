aa90deb0f8ec41b02bc1b163e2e25b3c7cd5f183
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2016-10-20 20:29:34 +0200

    very initial checkin

---
+++ libs/sensors/BMP280.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+################################################################################
+# BMP280.py                                                                    #
+# (c) https://github.com/thomaspfeiffer-git 2016                               #
+################################################################################
+"""control temperature and air pressure sensor BMP280."""
+
+# https://github.com/cudacode/Python-BMP280/blob/master/BMP280.py
+
+# eof #
+
