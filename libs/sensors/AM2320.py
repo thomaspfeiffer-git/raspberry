294a38049132af106000d801e2aaf582c0e906b8
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2016-10-20 20:21:28 +0200

    very initial checkin

---
+++ libs/sensors/AM2320.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+################################################################################
+# AM2320.py                                                                    #
+# (c) https://github.com/thomaspfeiffer-git 2016                               #
+################################################################################
+"""control temperature and humidity sensor AM2320."""
+
+# https://www.raspberrypi.org/forums/viewtopic.php?f=28&t=152148
+
+
+# eof #
+
