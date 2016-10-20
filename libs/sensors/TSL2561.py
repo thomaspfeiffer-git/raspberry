45f0930c8630d2c12bc003c076eb76c07d09afbd
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2016-10-20 20:36:36 +0200

    very initial checkin

---
+++ libs/sensors/TSL2561.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+################################################################################
+# TSL2561.py                                                                   #
+# (c) https://github.com/thomaspfeiffer-git 2016                               #
+################################################################################
+"""control lightness sensor TSL2561."""
+
+# http://www.mogalla.net/201502/lichtsensor-tsl2561-am-raspberry
+# https://github.com/seanbechhofer/raspberrypi/blob/master/python/TSL2561.py
+# https://github.com/janheise/TSL2561/blob/master/TSL2561.py
+
+
+# eof #
+
