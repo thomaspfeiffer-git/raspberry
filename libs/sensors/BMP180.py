998e2745e9fa372408317af313ed4bf469268aad
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2016-10-22 12:55:50 +0200

    added read_temperature()

--- libs/sensors/BMP180.py
+++ libs/sensors/BMP180.py
@@ -19,12 +19,12 @@ class BMP180 (I2C):
         if sys.version_info >= (3,0):
             super().__init__(lock)
         else:
-            super(BMP085, self).__init__(lock)
+            super(BMP180, self).__init__(lock)
 
         self.__bmp    = Adafruit_BMP085.BMP085() 
         self.__qvalue = qvalue
 
-    def readpressure (self):
+    def read_pressure (self):
         """read pressure and return measured value"""
         value = -1
         with I2C._lock:
@@ -40,10 +40,18 @@ class BMP180 (I2C):
 
                 return value
 
+    def read_emperature (self):
+        """read temperature and return measured value"""
+        value = -1
+        with I2C._lock:
+            try:
+                value = self.__bmp.read_temperature()
+    
+            except (IOError, OSError):
+                print(localtime()[3:6], "error reading/writing i2c bus")
 
-
-
-
+            finally:
+                return value
 
 # eof #
 
