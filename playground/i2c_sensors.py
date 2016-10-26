16edeeddfd46886b1b063dffbc345820b91f8f02
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2016-10-26 19:34:27 +0200

    --

--- playground/i2c_sensors.py
+++ playground/i2c_sensors.py
@@ -17,15 +17,21 @@ import BMP180
 import MCP9808
 
 bmp180  = BMP180.BMP180()
-mcp9808 = MCP9808.MCP9808()
+
+
+MCP9808_1_ADDR = 0x18
+MCP9808_2_ADDR = 0x19
+mcp9808_1 = MCP9808.MCP9808(address=MCP9808_1_ADDR)
+mcp9808_2 = MCP9808.MCP9808(address=MCP9808_2_ADDR)
 
 
 while True:
      print(strftime("%H:%M:%S"))
-     print("Sensor  | Größe | Messwert | Einheit |")
-     print("BMP180  | Druck | {:>8.2f} | hPa     |".format(bmp180.read_pressure()/100.0))
-     print("BMP180  | Temp  | {:>8.2f} | °C      |".format(bmp180.read_temperature()))
-     print("MCP9808 | Temp  | {:>8.2f} | °C      |".format(mcp9808.read_temperature()))
+     print("Sensor     | Größe | Messwert | Einheit |")
+     print("BMP180     | Druck | {:>8.2f} | hPa     |".format(bmp180.read_pressure()/100.0))
+     print("BMP180     | Temp  | {:>8.2f} | °C      |".format(bmp180.read_temperature()))
+     print("MCP9808 #1 | Temp  | {:>8.2f} | °C      |".format(mcp9808_1.read_temperature()))
+     print("MCP9808 #2 | Temp  | {:>8.2f} | °C      |".format(mcp9808_2.read_temperature()))
      print("")
 
      sleep(10)
