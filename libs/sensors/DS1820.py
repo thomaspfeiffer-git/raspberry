9be817d20390a8fb9556380bd71829092bdf65c4
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2016-10-28 21:16:28 +0200

    improved error handling and some code optimizations

--- libs/sensors/DS1820.py
+++ libs/sensors/DS1820.py
@@ -2,7 +2,7 @@
 ###############################################################################################
 # DS1820.py                                                                                   #
 # Communication with DS1820                                                                   #
-# (c) https://github.com/thomaspfeiffer-git May 2015                                          #
+# (c) https://github.com/thomaspfeiffer-git May 2015, 2016                                    #
 ###############################################################################################
 
 import re
@@ -14,26 +14,31 @@ class DS1820:
       self.__qvalue = qvalue
    
    def __read_sensor (self):
-     value = 99 # TODO: improve error handling
-     try:
-       f = open(self.__id, "r")
-       line = f.readline()
-       if re.match(r"([0-9a-f]{2} ){9}: crc=[0-9a-f]{2} YES", line):
-         line = f.readline()
-         m = re.match(r"([0-9a-f]{2} ){9}t=([+-]?[0-9]+)", line)
-         if m:
-           value = float(m.group(2)) / 1000.0
-       f.close()
-     except (IOError), e:
-       print time.strftime("%x %X"), "Error reading", self.__id, ": ", e
-     return value
+      try:
+         with open(self.__id, "r") as f:
+            line = f.readline()
+            if re.match(r"([0-9a-f]{2} ){9}: crc=[0-9a-f]{2} YES", line):
+               line = f.readline()
+               m = re.match(r"([0-9a-f]{2} ){9}t=([+-]?[0-9]+)", line)
+               if m:
+                  return float(m.group(2)) / 1000.0
+
+      except (IOError), e:
+         print time.strftime("%x %X"), "Error reading", self.__id, ": ", e
+
+      return None
+
 
    def read (self):
-      sum = 0
-      for i in range(0, 3): # TODO: improve error handling
-         sum += self.__read_sensor()
+      sum = 0.0
+      no_reads = 0
+      for i in range(3):
+         value = self.__read_sensor()
+         if value:
+            sum += value
+            no_reads += 1
 
-      sum = sum/3.0
+      sum = sum/no_reads
 
       if self.__qvalue is not None:
           value = "%.1f" % (sum)
