44b015e1ba418bc7450b8c392778f797b5dedb80
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2016-10-29 16:48:27 +0200

    deleted re-read stuff

--- libs/sensors/DS1820.py
+++ libs/sensors/DS1820.py
@@ -17,10 +17,8 @@ class DS1820:
       try:
          with open(self.__id, "r") as f:
             line = f.readline()
-            print "DS1820, Line #1:", time.strftime("%x %X"), "---", line
             if re.match(r"([0-9a-f]{2} ){9}: crc=[0-9a-f]{2} YES", line):
                line = f.readline()
-               print "DS1820, Line #2:", time.strftime("%x %X"), "---", line
                m = re.match(r"([0-9a-f]{2} ){9}t=([+-]?[0-9]+)", line)
                if m:
                   return float(m.group(2)) / 1000.0
@@ -32,26 +30,12 @@ class DS1820:
 
 
    def read (self):
-      sum = 0.0
-      no_reads = 0
-      for i in range(3):
-         value = self.__read_sensor()
-         print "DS1820, try", i, ":", value
-         if value:
-            sum += value
-            no_reads += 1
-
-      if no_reads == 0:
-          return None
-      else:
-          sum = sum/no_reads
-          print "DS1820, sum", ":", sum
-
-          if self.__qvalue is not None:
-              value = "%.1f" % (sum)
-              self.__qvalue.value = value
-
-          return sum
+       value = self.__read_sensor()
+       if value is not None:
+           if self.__qvalue is not None:
+               value = "%.1f" % (sum)
+               self.__qvalue.value = value
+       return value
 
 ### eof ###
 
