7ba5ec9b11ca7470dc48ce5ea73e63c412b87ec3
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2016-11-05 20:39:23 +0100

    added additional url for doku

--- libs/sensors/BMP280.py
+++ libs/sensors/BMP280.py
@@ -3,9 +3,10 @@
 # BMP280.py                                                                    #
 # (c) https://github.com/thomaspfeiffer-git 2016                               #
 ################################################################################
-"""control temperature and air pressure sensor BMP280."""
+"""control temperature, humidiy, and air pressure sensor BMP280."""
 
 # https://github.com/cudacode/Python-BMP280/blob/master/BMP280.py
+# better: https://github.com/bastienwirtz/Adafruit_Python_BMP/blob/master/Adafruit_BMP/BMP280.py
 
 # eof #
 
