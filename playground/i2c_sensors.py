9f69a505eab874d8da778bc030bf0c4f54f4e9a0
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2016-11-05 21:13:55 +0100

    preparing for BME280

--- playground/i2c_sensors.py
+++ playground/i2c_sensors.py
@@ -13,10 +13,12 @@ from time import sleep, strftime
 sys.path.append('../libs')
 sys.path.append('../libs/sensors')
 
-import BMP180
-import MCP9808
+import BME280    # air pressure, temperature, humidity
+import BMP180    # air pressure, temperature
+import MCP9808   # temperatur
 
 bmp180  = BMP180.BMP180()
+bme280  = BME280.BME280()
 
 
 MCP9808_1_ADDR = 0x18
@@ -30,6 +32,9 @@ while True:
      print("Sensor     | Größe | Messwert | Einheit |")
      print("BMP180     | Druck | {:>8.2f} | hPa     |".format(bmp180.read_pressure()/100.0))
      print("BMP180     | Temp  | {:>8.2f} | °C      |".format(bmp180.read_temperature()))
+#     print("BME280     | Druck | {:>8.2f} | hPa     |".format(bme280.read_pressure()/100.0))
+#     print("BME280     | Temp  | {:>8.2f} | °C      |".format(bme280.read_temperature()))
+#     print("BME280     | Humi  | {:>8.2f} | % rF    |".format(bme280.read_humidity()))
      print("MCP9808 #1 | Temp  | {:>8.2f} | °C      |".format(mcp9808_1.read_temperature()))
      print("MCP9808 #2 | Temp  | {:>8.2f} | °C      |".format(mcp9808_2.read_temperature()))
      print("")
