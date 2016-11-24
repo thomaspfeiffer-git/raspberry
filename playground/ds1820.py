1c1f2af08bcea743f6815e25ef6789a66792f119
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2016-11-24 20:46:56 +0100

    initial checkin

---
+++ playground/ds1820.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import sys
+
+sys.path.append('../libs')
+sys.path.append('../libs/sensors')
+
+from DS1820 import DS1820
+
+ds1820 = DS1820("/sys/bus/w1/devices/28-000006b50d05/w1_slave")
+
+print ds1820.read()
+
+
