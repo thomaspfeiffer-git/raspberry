68f5e1cdad4d5f38ea0c565b91cb7dfaa07a6e2e
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-01-13 23:35:40 +0100

    initial checkin

--- libs/actors/PCA9685.py
+++ libs/actors/PCA9685.py
@@ -18,29 +18,39 @@ PCA9685_ADDRESS = 0x40
 
 
 class PCA9685 (I2C):
+    # Registers:
+    MODE1     = 0x00
+    MODE2     = 0x01
     PRE_SCALE = 0xFE
+
+    # Bits:
+    SLEEP     = 0x10
+
     _first = True
 
     def __init__ (self, address=PCA9685_ADDRESS, lock=None):
         super().__init__(lock)
+
         self._address = address
         if PCA9685._first:   # reset on first init
             self.all_reset()
             PCA9685._first = False
 
+    def __read (self, reg):
+        return I2C._bus.read_byte_data(self.__address, reg)
 
-    def sleep(self):
-        """Send the controller to sleep"""
-        self.write(Registers.MODE_1, self.mode_1 | (1 << Mode1.SLEEP))
-
-    def wake(self):
-        """Wake up the controller"""
-        self.write(Registers.MODE_1, self.mode_1 & (255 - (1 << Mode1.SLEEP)))
-
-    def write(self, reg, value):
+    def __write (self, reg, value):
         """Write raw byte value to the specified register"""
         with I2C._lock:
             I2C._bus.write_byte_data(self.__address, reg, value)
+
+    def __sleep (self):
+        """Send the controller to sleep"""
+        self.__write(MODE1, self.__read(MODE1) | (1 << SLEEP))
+
+    def __wake (self):
+        """Wake up the controller"""
+        self.__write(MODE1, self.__read(MODE1) & (255 - (1 << SLEEP)))
 
     def all_reset (self):
         pass
@@ -51,5 +61,5 @@ class PCA9685 (I2C):
     def set_pwm (self, channel, on, off):
         pass
 
-
+# eof #
 
