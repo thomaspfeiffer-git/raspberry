d9af7b78c5ef2b1d46843eef2ac1866939dd227c
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-01-21 21:51:56 +0100

    added wardrobe

--- weather/weather_graph.sh
+++ weather/weather_graph.sh
@@ -4,6 +4,7 @@ RRDPATH=/schild/weather/
 RRD=$RRDPATH/weather.rrd
 RRD_K=$RRDPATH/weather_kidsroom.rrd
 RRD_KB=$RRDPATH/weather_kollerberg.rrd
+RRD_WR=$RRDPATH/wardrobe.rrd
 
 
 PNG_TEMP_D=$RRDPATH/weather_temp_d.png
@@ -26,11 +27,15 @@ PNG_CPU_W=$RRDPATH/cpu_temp_w.png
 PNG_CPU_M=$RRDPATH/cpu_temp_m.png
 PNG_CPU_Y=$RRDPATH/cpu_temp_y.png
 
-PNG_HEATING_D=$RRDPATH/heating_d.png
-PNG_HEATING_W=$RRDPATH/heating_w.png
-PNG_HEATING_M=$RRDPATH/heating_m.png
-PNG_HEATING_Y=$RRDPATH/heating_y.png
+PNG_WARDROBE_D=$RRDPATH/wardrobe_d.png
+PNG_WARDROBE_W=$RRDPATH/wardrobe_w.png
+PNG_WARDROBE_M=$RRDPATH/wardrobe_m.png
+PNG_WARDROBE_Y=$RRDPATH/wardrobe_y.png
 
+PNG_WR_LIGHTNESS_D=$RRDPATH/wr_lightness_d.png
+PNG_WR_LIGHTNESS_W=$RRDPATH/wr_lightness_w.png
+PNG_WR_LIGHTNESS_M=$RRDPATH/wr_lightness_m.png
+PNG_WR_LIGHTNESS_Y=$RRDPATH/wr_lightness_y.png
 
 
 WIDTH=1024
@@ -58,6 +63,7 @@ printTemp ()
     DEF:temp_indoor=$RRD:temp_indoor:AVERAGE                 \
     DEF:temp_window=$RRD:temp_4:AVERAGE                      \
     DEF:kidsroom_temp1=$RRD_K:kidsroom_temp1:AVERAGE         \
+    DEF:wr_temp1=$RRD_WR:wr_temp1:AVERAGE                    \
     DEF:kb_i_t1=$RRD_KB:kb_i_t1:AVERAGE                      \
     DEF:kb_a_t1=$RRD_KB:kb_a_t1:AVERAGE                      \
     DEF:kb_k_t1=$RRD_KB:kb_k_t1:AVERAGE                      \
@@ -86,6 +92,11 @@ printTemp ()
     GPRINT:kidsroom_temp1:AVERAGE:"Mittelwert\: %5.2lf °C"   \
     GPRINT:kidsroom_temp1:MAX:"Max\: %5.2lf °C"              \
     GPRINT:kidsroom_temp1:MIN:"Min\: %5.2lf °C\n"            \
+    LINE1:wr_temp1#ffcc00:"Temperatur Kleiderkasten      "   \
+    GPRINT:wr_temp1:LAST:"Aktuell\: %5.2lf °C"               \
+    GPRINT:wr_temp1:AVERAGE:"Mittelwert\: %5.2lf °C"         \
+    GPRINT:wr_temp1:MAX:"Max\: %5.2lf °C"                    \
+    GPRINT:wr_temp1:MIN:"Min\: %5.2lf °C\n"                  \
     LINE1:kb_i_t1#40FF00:"Temperatur Kollerberg innen   "    \
     GPRINT:kb_i_t1:LAST:"Aktuell\: %5.2lf °C"                \
     GPRINT:kb_i_t1:AVERAGE:"Mittelwert\: %5.2lf °C"          \
@@ -100,7 +111,7 @@ printTemp ()
     GPRINT:kb_k_t1:LAST:"Aktuell\: %5.2lf °C"                \
     GPRINT:kb_k_t1:AVERAGE:"Mittelwert\: %5.2lf °C"          \
     GPRINT:kb_k_t1:MAX:"Max\: %5.2lf °C"                     \
-    GPRINT:kb_k_t1:MIN:"Min\: %5.2lf °C\n"
+    GPRINT:kb_k_t1:MIN:"Min\: %5.2lf °C\n"      
  }
 
 
@@ -113,17 +124,18 @@ printTemp ()
 printCPUTemp ()
   {
     rrdtool graph $2                                                     \
-    --title "Temperatur Raspberry Pi [°C]"                               \
+    --title "Temperatur Pis [°C]"                                         \
     --end now --start end-$1                                             \
     -w $WIDTH -h $HEIGHT -a PNG                                          \
     --watermark "$WATERMARK"                                             \
     --right-axis 1:0                                                     \
     DEF:temp_cpu=$RRD:temp_cpu:AVERAGE                                   \
     DEF:kidsroom_tempcpu=$RRD_K:kidsroom_tempcpu:AVERAGE                 \
+    DEF:wr_tempcpu=$RRD_WR:wr_tempcpu:AVERAGE                            \
     DEF:kb_i_tcpu=$RRD_KB:kb_i_tcpu:AVERAGE                              \
     DEF:kb_a_tcpu=$RRD_KB:kb_a_tcpu:AVERAGE                              \
     DEF:kb_k_tcpu=$RRD_KB:kb_k_tcpu:AVERAGE                              \
-    LINE1:temp_cpu#FF0000:"Temperatur Raspberry Pi Wohnzimmer       "    \
+    LINE1:temp_cpu#FF0000:"Temperatur NanoPi NEO Air Wohnzimmer     "    \
     GPRINT:temp_cpu:LAST:"\t Aktuell\: %5.2lf °C"                        \
     GPRINT:temp_cpu:AVERAGE:"Mittelwert\: %5.2lf °C"                     \
     GPRINT:temp_cpu:MAX:"Max\: %5.2lf °C"                                \
@@ -133,6 +145,11 @@ printCPUTemp ()
     GPRINT:kidsroom_tempcpu:AVERAGE:"Mittelwert\: %5.2lf °C"             \
     GPRINT:kidsroom_tempcpu:MAX:"Max\: %5.2lf °C"                        \
     GPRINT:kidsroom_tempcpu:MIN:"Min\: %5.2lf °C\n"                      \
+    LINE1:wr_tempcpu#ffcc00:"Temperatur Raspberry Pi Kleiderkasten    " \
+    GPRINT:wr_tempcpu:LAST:"\t Aktuell\: %5.2lf °C"                \
+    GPRINT:wr_tempcpu:AVERAGE:"Mittelwert\: %5.2lf °C"             \
+    GPRINT:wr_tempcpu:MAX:"Max\: %5.2lf °C"                        \
+    GPRINT:wr_tempcpu:MIN:"Min\: %5.2lf °C\n"                      \
     LINE1:kb_i_tcpu#40FF00:"Temperatur Raspberry Pi Kollerberg innen "   \
     GPRINT:kb_i_tcpu:LAST:"\t Aktuell\: %5.2lf °C"                       \
     GPRINT:kb_i_tcpu:AVERAGE:"Mittelwert\: %5.2lf °C"                    \
@@ -169,6 +186,7 @@ printHumidity ()
     DEF:humi_outdoor=$RRD:humi_outdoor:AVERAGE                 \
     DEF:humi_indoor=$RRD:humi_indoor:AVERAGE                   \
     DEF:kidsroom_humi=$RRD_K:kidsroom_humi:AVERAGE             \
+    DEF:wr_humi=$RRD_WR:wr_humi:AVERAGE                        \
     DEF:kb_i_humi=$RRD_KB:kb_i_humi:AVERAGE                    \
     DEF:kb_a_humi=$RRD_KB:kb_a_humi:AVERAGE                    \
     DEF:kb_k_humi=$RRD_KB:kb_k_humi:AVERAGE                    \
@@ -187,6 +205,11 @@ printHumidity ()
     GPRINT:kidsroom_humi:AVERAGE:"Mittelwert\: %5.2lf %%"      \
     GPRINT:kidsroom_humi:MAX:"Max\: %5.2lf %%"                 \
     GPRINT:kidsroom_humi:MIN:"Min\: %5.2lf %%\n"               \
+    LINE1:wr_humi#ffcc00:"Luftfeuchtigkeit Kleiderkasten    "  \
+    GPRINT:wr_humi:LAST:"\t Aktuell\: %5.2lf %%"               \
+    GPRINT:wr_humi:AVERAGE:"Mittelwert\: %5.2lf %%"            \
+    GPRINT:wr_humi:MAX:"Max\: %5.2lf %%"                       \
+    GPRINT:wr_humi:MIN:"Min\: %5.2lf %%\n"                     \
     LINE1:kb_i_humi#40FF00:"Luftfeuchtigkeit Kollerberg innen " \
     GPRINT:kb_i_humi:LAST:"\t Aktuell\: %5.2lf %%"             \
     GPRINT:kb_i_humi:AVERAGE:"Mittelwert\: %5.2lf %%"          \
@@ -239,6 +262,57 @@ printAirPressure ()
 
 
 
+
+#######################################################################
+# printWardrobe()                                                     #
+# Parameter:                                                          #
+# $1 Time Range                                                       #
+# $2 Filename                                                         #
+#######################################################################
+printWardrobe()
+  {
+    rrdtool graph $2                                     \
+    --title "Kleiderkasten"                              \
+    --end now --start end-$1                             \
+    -w $WIDTH -h $(($HEIGHT/2)) -a PNG                   \
+    --watermark "$WATERMARK"                             \
+    --alt-y-grid                                         \
+    --right-axis 1:0                                     \
+    DEF:wr_open1=$RRD_WR:wr_open1:AVERAGE                \
+    DEF:wr_open2=$RRD_WR:wr_open2:AVERAGE                \
+    DEF:wr_open3=$RRD_WR:wr_open3:AVERAGE                \
+    DEF:wr_open4=$RRD_WR:wr_open4:AVERAGE                \
+    AREA:wr_open1#ffcc00:"Kleiderkasten offen  "         \
+    GPRINT:wr_open1:LAST:"\t Aktuell\: %5.0lf"           \
+    GPRINT:wr_open1:AVERAGE:"Mittelwert\: %5.2lf\n"      \
+    STACK:wr_open3#ffff00:"Lade offen           "        \
+    GPRINT:wr_open3:LAST:"\t Aktuell\: %5.0lf"           \
+    GPRINT:wr_open3:AVERAGE:"Mittelwert\: %5.2lf\n"
+ }
+
+
+
+printLightness()
+  {
+    rrdtool graph $2                                        \
+    --title "Helligkeit Kleiderkasten [lux]"                \
+    --end now --start end-$1                                \
+    -w $WIDTH -h $HEIGHT -a PNG                             \
+    --alt-autoscale                                         \
+    --alt-y-grid                                            \
+    --rigid                                                 \
+    --watermark "$WATERMARK"                                \
+    --right-axis 1:0                                        \
+    --right-axis-format "%4.0lf"                            \
+    DEF:wr_lightness=$RRD_WR:wr_lightness:AVERAGE           \
+    LINE1:wr_lightness#ffcc00:"Helligkeit Kleiderkasten   " \
+    GPRINT:wr_lightness:LAST:"\t Aktuell\: %5.2lf lux"      \
+    GPRINT:wr_lightness:AVERAGE:"Mittelwert\: %5.2lf lux"   \
+    GPRINT:wr_lightness:MAX:"Max\: %5.2lf lux"              \
+    GPRINT:wr_lightness:MIN:"Min\: %5.2lf lux\n"              
+ }
+
+
 printTemp "36h", "$PNG_TEMP_D"
 printTemp "7d",  "$PNG_TEMP_W"
 printTemp "30d", "$PNG_TEMP_M"
@@ -259,4 +333,15 @@ printAirPressure "7d",  "$PNG_PRES_W"
 printAirPressure "30d", "$PNG_PRES_M"
 printAirPressure "365d", "$PNG_PRES_Y"
 
+printWardrobe "36h", "$PNG_WARDROBE_D"
+printWardrobe "7d", "$PNG_WARDROBE_W"
+printWardrobe "30d", "$PNG_WARDROBE_M"
+printWardrobe "365d", "$PNG_WARDROBE_Y"
 
+printLightness "36h", "$PNG_WR_LIGHTNESS_D"
+printLightness "7d", "$PNG_WR_LIGHTNESS_W"
+printLightness "30d", "$PNG_WR_LIGHTNESS_M"
+printLightness "365d", "$PNG_WR_LIGHTNESS_Y"
+
+# eof #
+ 
