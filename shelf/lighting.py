b2ebfd8d0d435eb9ed79ce27c4a03af456b0309e
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-02-03 22:59:25 +0100

    some optimization for the rainbow pattern

--- shelf/lighting.py
+++ shelf/lighting.py
@@ -15,6 +15,15 @@
 
 # sudo PYTHONPATH=".:build/lib.linux-armv7l-2.7" python tp/strandtest.py &
 
+# >>> from flask import Markup
+# >>> Markup('<strong>Hello %s!</strong>') % '<blink>hacker</blink>'
+# Markup(u'<strong>Hello &lt;blink&gt;hacker&lt;/blink&gt;!</strong>')
+# >>> Markup.escape('<blink>hacker</blink>')
+# Markup(u'&lt;blink&gt;hacker&lt;/blink&gt;')
+# >>> Markup('<em>Marked up</em> &raquo; HTML').striptags()
+# u'Marked up \xbb HTML'
+
+
 
 
 ### setup ###
@@ -36,7 +45,7 @@ import sys
 import threading
 from time import sleep
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 
@@ -110,14 +119,15 @@ class Control_Strip (threading.Thread):
             raise ValueError("'value' has to be in 0 .. 255")
         self._strip.set_brightness(value)
 
-    def set_pattern (self, pattern):
-        self.__pattern = pattern
+    def set_pattern (self, method, **kwargs):
+        self.__pattern = method
+        self.__kwargs  = kwargs
         self.__flags[Flags.pattern_changed] = True
 
     def run (self):
         self.__flags[Flags.pattern_changed] = False
         while self.__flags[Flags.running]:
-            self.__pattern(self._strip, self.__flags)
+            self.__pattern(self._strip, self.__flags, self.__kwargs)
             self.__flags[Flags.pattern_changed] = False
        
     def stop (self):
@@ -130,40 +140,40 @@ def static_pattern_wait (flags):
     while flags[Flags.running] and not flags[Flags.pattern_changed]:
         sleep(0.05) 
 
-def pattern_red (strip, flags):
+def pattern_red (strip, flags, kwargs):
     strip.set_color(0xFF0000)
     static_pattern_wait(flags)
 
-def pattern_green (strip, flags):
+def pattern_green (strip, flags, kwargs):
     strip.set_color(0x00FF00)
     static_pattern_wait(flags)
 
-def pattern_blue (strip, flags):
+def pattern_blue (strip, flags, kwargs):
     strip.set_color(0x0000FF)
     static_pattern_wait(flags)
 
-def pattern_yellow (strip, flags):
+def pattern_yellow (strip, flags, kwargs):
     strip.set_color(0xFFFF00)
     static_pattern_wait(flags)
 
-def pattern_fuchsia (strip, flags):
+def pattern_fuchsia (strip, flags, kwargs):
     strip.set_color(0xFF00FF)
     static_pattern_wait(flags)
 
-def pattern_aqua (strip, flags):
+def pattern_aqua (strip, flags, kwargs):
     strip.set_color(0x00FFFF)
     static_pattern_wait(flags)
 
-def pattern_white (strip, flags):
+def pattern_white (strip, flags, kwargs):
     strip.set_color(0xFFFFFF)
     static_pattern_wait(flags)
 
-def pattern_black (strip, flags):
+def pattern_black (strip, flags, kwargs):
     strip.set_color(0x000000)
     static_pattern_wait(flags)
 
 
-def pattern_rainbow (strip, flags):
+def pattern_rainbow (strip, flags, kwargs):
     def wheel (pos):
         if pos < 85:
             return Color(pos * 3, 255 - pos * 3, 0)
@@ -174,6 +184,8 @@ def pattern_rainbow (strip, flags):
             pos -= 170
             return Color(0, pos * 3, 255 - pos * 3)
 
+    delay = int(kwargs['delay']) # TODO: constant instant of magic string
+    
     while flags[Flags.running] and not flags[Flags.pattern_changed]:
         for j in range(256):
             color = wheel(j)
@@ -182,11 +194,11 @@ def pattern_rainbow (strip, flags):
             strip.show()
 
             if not flags[Flags.running] or flags[Flags.pattern_changed]:
-                break
+               break
 
-            for s in range(500):    # interruptible sleep  
-                while flags[Flags.running] and not flags[Flags.pattern_changed]:
-                    sleep(0.01)
+            for s in range(delay):    # interruptible sleep  
+                if flags[Flags.running] and not flags[Flags.pattern_changed]:
+                    sleep(0.001)
 
 
 ############################################################################
@@ -203,36 +215,36 @@ def light_on ():
 
 @app.route('/off')
 def light_off ():
-    c.set_pattern(pattern_black)
+    c.set_pattern(method=pattern_black)
 
 @app.route('/color/red')
 def set_color_red ():
-    c.set_pattern(pattern_red)
+    c.set_pattern(method=pattern_red)
     return "color set"
 
 @app.route('/color/green')
 def set_color_green ():
-    c.set_pattern(pattern_green)
+    c.set_pattern(method=pattern_green)
     return "color set"
 
 @app.route('/color/blue')
 def set_color_blue ():
-    c.set_pattern(pattern_blue)
+    c.set_pattern(method=pattern_blue)
     return "color set"
 
 @app.route('/color/yellow')
 def set_color_yellow ():
-    c.set_pattern(pattern_yellow)
+    c.set_pattern(method=pattern_yellow)
     return "color set"
 
 @app.route('/color/fuchsia')
 def set_color_fuchsia ():
-    c.set_pattern(pattern_fuchsia)
+    c.set_pattern(method=pattern_fuchsia)
     return "color set"
 
 @app.route('/color/aqua')
 def set_color_aqua ():
-    c.set_pattern(pattern_aqua)
+    c.set_pattern(method=pattern_aqua)
     return "color set"
 
 @app.route('/colorrgb/<string:color>')
@@ -242,8 +254,9 @@ def set_color_rgb (color):
 
 @app.route('/rainbow')
 def rainbow ():
-    c.set_pattern(pattern_rainbow)
-    return "rainbow"
+    delay = request.args.get('delay', '5000')  # delay in milliseconds
+    c.set_pattern(method=pattern_rainbow, delay=delay)
+    return "rainbow, delay: {}".format(delay)
 
 
 @app.route('/lightness')
