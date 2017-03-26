bfbae456d691a1dee673a0e4cf92444bd8c8572a
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-03-26 20:40:32 +0200

    initial checkin

---
+++ homeassistant/Queueserver/QueueServer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+###############################################################################
+# QueueServer.py                                                              #
+# (c) https://github.com/thomaspfeiffer-git 2016, 2017                        #
+###############################################################################
+"""Server for messages of sensor values based on SensorQueueServer"""
+
+import sys
+
+sys.path.append('../libs')
+from SensorQueue import SensorQueueServer
+from Shutdown import Shutdown
+
+
+def shutdown_application ():
+    """called on shutdown; stops all threads"""
+    sys.exit(0)
+
+
+if __name__ == '__main__':
+    shutdown = Shutdown(shutdown_func=shutdown_application)
+    SensorServer = SensorQueueServer()
+    SensorServer.start()
+
+### eof ###
+
