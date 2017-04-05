0155a17fa363a5cd2d36f3b89b295e630301a9db
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-04-05 18:05:22 +0200

    continued implementation

--- homeassistant/Brightness/Brightness.py
+++ homeassistant/Brightness/Brightness.py
@@ -11,6 +11,7 @@
 # TODO 
 
 
+from datetime import datetime
 import subprocess
 import sys
 import threading
@@ -26,38 +27,98 @@ from sensors.TSL2561 import TSL2561
 from Shutdown import Shutdown
 
 
+CONTROLBRIGHTNESS = '/sys/class/backlight/rpi_backlight/brightness'
+
+
 ##############################################################################
-# Control ####################################################################
-class Control (threading.Thread):
+# Sensor #####################################################################
+class Sensor (threading.Thread):
+    """todo """
+
     MIN = 15
     MAX = 255
-    sensor = TSL2561()
 
     def __init__ (self):
         threading.Thread.__init__(self)
+        self.sensor    = TSL2561()
+        self.__lux     = 0
         self.__running = False
+
+    @property
+    def lux (self):
+        return self.__lux
+
+    def run (self):
+        self.__running = True
+        while self.__running:
+            v = self.sensor.lux() * 2
+            if v < self.MIN: v = self.MIN
+            if v > self.MAX: v = self.MAX
+            self.__lux = v
+            time.sleep(1)
+
+    def stop (self):
+        self.__running = False
+
+
+##############################################################################
+# Control ####################################################################
+class Control (threading.Thread):
+    """todo """
+
+    DELAYTOLIGHTOFF = 15
+
+    def __init__ (self):
+        threading.Thread.__init__(self)
+        self.timestamp  = datetime.now()
+        self.__switchedon = False
+        self.__running = False
+
+    @staticmethod
+    def set_brightness ():
+        lastvalue = 0
+
+        def set_value (brightness):
+            nonlocal lastvalue
+
+            if brightness != lastvalue:
+                lastvalue = brightness
+
+                command = "sudo bash -c \"echo \\\"{}\\\" > {}\""
+                command = command.format(brightness, CONTROLBRIGHTNESS)
+
+                # print("command:", command)
+                subprocess.call(command, shell=True)
+
+        return set_value
+
+    def switch_on (self):
+        self.__switchedon = True
+        self.timestamp = datetime.timestamp(seconds=DELAYTOLIGHTOFF)
+
+    def switched_on (self):
+        if datetime.now() > self.timestamp:
+            self.__switchedon = False
+        return self.__switchedon
 
     def run (self):
         self.__running = True
         measurements = Measurements(maxlen=20)
+        set_brightness = self.set_brightness()
 
         while self.__running:
-            lux = int(sensor.lux()) * 2
-            if lux < self.MIN: lux = self.MIN
-            if lux > self.MAX: lux = self.MAX
-            measurements.append(lux)
-            avg = int(measurements.avg())
+            measurements.append(sensor.lux)
+                    # full brightness between 6 am and 10 pm or
+                    # if manually switched on
+            if 6 <= datetime.now().hour < 18 or self.switched_on(): 
+                set_brightness(Sensor.MAX)   
+            else:
+                set_brightness(int(measurements.avg()))
+            time.sleep(0.02)
 
-            # TODO: call only on change of value etc
-            command = "sudo bash -c \"echo \\\"{}\\\" > /sys/class/backlight/rpi_backlight/brightness\"".format(avg)
-
-            # print("command:", command)
-            subprocess.call(command, shell=True)
-
-            time.sleep(0.1)
+        set_brightness(Sensor.MAX) # set max brightness on exit
 
     def stop (self):
-        # TODO set brightness to self.MAX on exit
         self.__running = False
 
 
@@ -68,6 +129,8 @@ def shutdown_application ():
     print("in shutdown_application()")
     control.stop()
     control.join()
+    sensor.stop()
+    sensor.join()
     sys.exit(0)
 
 
@@ -75,6 +138,9 @@ def shutdown_application ():
 # Main ########################################################################
 if __name__ == '__main__':
     shutdown = Shutdown(shutdown_func=shutdown_application)
+    sensor = Sensor()
+    sensor.start()
+
     control = Control()
     control.start()
 
