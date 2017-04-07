fa9285451d504578603bd75e913a6d9af04dea9a
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-04-07 17:21:05 +0200

    added some more applications

--- homeassistant/start_applications.sh
+++ homeassistant/start_applications.sh
@@ -5,7 +5,21 @@ echo "Starting QueueServer.py ..."
 cd Queueserver/
 nohup ./QueueServer.py &
 echo "QueueServer.py started."
+cd ..
 
 
+echo "Starting Brightness.py ..."
+cd Brightness/
+nohup ./Brightness.py &
+echo "Brightness.py started."
+cd ..
 
+
+echo "Starting Weatherstation.py ..."
+cd Weather/
+nohup ./Weatherstation.py &
+echo "Weatherstation.py started."
+cd ..
+
+# eof #
 
