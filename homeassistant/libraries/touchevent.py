0072c76386b83e9d17240ae2301437f2d6e92610
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-04-22 21:55:34 +0200

    initial checkin

---
+++ homeassistant/libraries/touchevent.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+############################################################################
+# touchevent.py                                                            #
+# (c) https://github.com/thomaspfeiffer-git/raspberry, 2017                #
+############################################################################
+"""
+"""
+
+import json
+import sys
+from urllib.error import HTTPError, URLError 
+from urllib.request import urlopen
+
+sys.path.append('../../libs')
+from Logging import Log
+
+from config import CONFIG
+
+class Touchevent (object):
+    @staticmethod
+    def event ():
+        """return True if the event shall be handled by the caller,
+           otherwise False."""
+        try:
+            with urlopen(CONFIG.URL_BRIGHTNESS_CONTROL) as response:
+                data = json.loads(response.read().decode("utf-8"))
+        except (HTTPError, URLError):
+            Log("Error: {0[0]} {0[1]}".format(sys.exc_info()))
+        else:
+            return data['FullBrightness']
+        return True
+
+# eof #
+
