5db1cae639abf55f89277c81bbb1650d68b04d6b
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-04-23 21:11:51 +0200

    some refactorings and implementation of class CONFIG

--- homeassistant/MessageOfTheDay/MessageOfTheDay.py
+++ homeassistant/MessageOfTheDay/MessageOfTheDay.py
@@ -28,6 +28,12 @@ sys.path.append('../../libs')
 from Shutdown import Shutdown
 from Logging import Log
 
+from config import CONFIG
+
+
+# message_of_the_day = "Frohe Ostern!"
+message_of_the_day = None
+
 
 ###############################################################################
 # MotDApp #####################################################################
@@ -36,17 +42,20 @@ class MotDApp (tk.Frame):
         super().__init__(master)
 
         self.master = master
-        self.grid()
+        self.frame  = tk.Frame(self.master, bg=CONFIG.COLORS.BACKGROUND,
+                               width=CONFIG.COORDINATES.WIDTH, 
+                               height=CONFIG.COORDINATES.HEIGHT)
+        self.frame.pack_propagate(0)
+        self.frame.pack()
 
-        self.font  = Font(family="Arial", size=20)
-        self.frame = tk.Frame(self)    # TODO: configfile
-        self.frame.config(width=410, height=480, background="LightSkyBlue")
-        self.frame.grid_propagate(0)
-        self.frame.grid()
-#        self.text = tk.Label(self.frame, text="Frohe Ostern!", font=self.font,
-#                             anchor="center", justify="center",
-#                             foreground="black", background="yellow")
-#        self.text.grid(sticky="we")
+        self.font  = Font(family=CONFIG.FONTS.FAMILY, size=CONFIG.FONTS.SIZE_NORMAL)
+
+        if message_of_the_day is not None:
+            self.text = tk.Label(self.frame, text=message_of_the_day, 
+                                 font=self.font,
+                                 foreground=CONFIG.COLORS.FONT, 
+                                 background=CONFIG.COLORS.BACKGROUND)
+            self.text.pack()
 
         # self.drawPicture(self.frame, "ei01.jpg", 0.55)
         # self.drawPicture(self.frame, "calimero.png", 0.55)
@@ -57,10 +66,10 @@ class MotDApp (tk.Frame):
         picture = PIL.Image.open(picture)
         w, h = map(lambda x: int(x*zoom), picture.size)
         picture = picture.resize((w, h), PIL.Image.ANTIALIAS)
+        self.image = PIL.ImageTk.PhotoImage(picture)
 
-        self.image = PIL.ImageTk.PhotoImage(picture)  # TODO: width=<configfile>
-        self.pic = tk.Label(frame, image=self.image, width=410, justify="center", anchor="center", background="LightSkyBlue")
-        self.pic.grid(sticky="we") 
+        self.pic = tk.Label(frame, image=self.image)
+        self.pic.pack()
 
 
 ###############################################################################
@@ -68,17 +77,18 @@ class MotDApp (tk.Frame):
 class MotD (object):
     def __init__ (self):
         self.root = tk.Tk()
-
         self.root.overrideredirect(1)
         self.root.config(cursor='none')
         self.root.resizable(width=False, height=False)
 
-        self.root.width  = 410   # TODO: config file
-        self.root.height = 480
+        self.root.width  = CONFIG.COORDINATES.WIDTH
+        self.root.height = CONFIG.COORDINATES.HEIGHT
         self.root.borderwidth = 10
-
-        self.root.geometry("410x480+280+0")   # TODO: config file
-        self.root.config(bg="LightSkyBlue")
+        self.root.geometry("{}x{}+{}+{}".format(self.root.width, 
+                                                self.root.height,
+                                                CONFIG.COORDINATES.XPOS, 
+                                                CONFIG.COORDINATES.YPOS))
+        self.root.config(bg=CONFIG.COLORS.BACKGROUND)
         self.app = MotDApp(master=self.root)
         
     def poll (self):
@@ -87,14 +97,14 @@ class MotD (object):
 
     def run (self):
         """start polling and run application"""
-        elf.root.pollid = elf.root.after(50, self.poll)
+        self.root.pollid = self.root.after(50, self.poll)
         self.app.mainloop()
 
     def stop (self):
         """stops application, called on shutdown"""
         self.root.after_cancel(self.root.pollid)
         self.root.destroy()
-        self.root.quit() # TODO: check usage of destroy() and quit()
+        self.root.quit()
 
 
 ###############################################################################
@@ -103,7 +113,7 @@ def shutdown_application ():
     """called on shutdown; stops all threads"""
     Log("shutdown_application()")
 
-    timer.stop()
+    motd.stop()
     sys.exit(0)
 
 
---
+++ homeassistant/MessageOfTheDay/config.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+###############################################################################
+# Config.py                                                                   #
+# (c) https://github.com/thomaspfeiffer-git 2017                              #
+###############################################################################
+"""lots of config stuff
+   1) display properties
+   2) fonts
+   3) colors
+   4) files
+   5) coordinates
+   6) misc stuff
+"""
+
+import configparser
+import os
+
+
+CONFIGFILE = "../config.ini"    
+cfg = configparser.ConfigParser()
+cfg.read(CONFIGFILE)
+
+
+class CONFIG:
+    """various config stuff"""
+
+    class FONTS:
+        """definitions of fonts and font sizes"""
+        FAMILY = "Arial"
+        SIZE_NORMAL = int(20)
+
+    class COLORS:
+        """definitions of colors"""
+        # BACKGROUND = "LightSkyBlue"
+        BACKGROUND = "White"
+        FONT = "Black"
+
+    class COORDINATES:
+        WIDTH  = int(cfg['MessageOfTheDay']['width'])
+        HEIGHT = int(cfg['MessageOfTheDay']['height'])
+        XPOS   = int(cfg['MessageOfTheDay']['xpos'])
+        YPOS   = int(cfg['MessageOfTheDay']['ypos'])
+
+    URL_BRIGHTNESS_CONTROL = cfg['Brightness']['URL']
+
+# eof #
+
