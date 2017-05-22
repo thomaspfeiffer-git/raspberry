ef9ec9b8c2cca38fb51193421570387fa264bf2f
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-05-22 20:40:14 +0200

    initial checkin

---
+++ playground/convert_ping.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+from datetime import datetime
+import sys
+
+sys.stdin.readline() # ignore first line
+for line in sys.stdin:
+    line = line.split(" ")
+    line[0] = "{}: ".format(datetime.fromtimestamp(float(line[0].strip('[').rstrip(']'))))
+    print(" ".join(line), end="")
+
+
