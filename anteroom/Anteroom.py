7b8f89cd02289d5dafe31e03dd6e74f089a3fd98
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-10-01 15:14:05 +0200

    continued implementation

--- anteroom/Anteroom.py
+++ anteroom/Anteroom.py
@@ -7,14 +7,25 @@
 """control lighting of our anteroom"""
 
 ### usage ###
-# TODO
-# sudo Anteroom.py
+# sudo bash
+# export FLASK_APP=Anteroom.py
+# flask run --host=0.0.0.0 >Anteroom.log 2>&1 &
+
 
 ### setup ###
-# TODO
+# http://flask.pocoo.org/docs/0.12/
+#
+# http://jinja.pocoo.org/docs/2.9/
+# sudo pip3 install Jinja2
+#
+# http://werkzeug.pocoo.org/docs/0.11/
+# sudo pip3 install Werkzeug
+#
+# http://flask.pocoo.org/docs/0.12/
+# sudo pip3 install Flask
 
 
-from flask import Flask
+from flask import Flask, request
 import sys
 import time
 
@@ -30,22 +41,27 @@ app = Flask(__name__)
 # PWM #########################################################################
 class PWM (PCA9685):
     def __init__ (self, channel):
-        # super().__init__(address=PCA9685_BASE_ADDRESS)
-        super(PWM,self).__init__(address=PCA9685_BASE_ADDRESS)
+        super().__init__(address=PCA9685_BASE_ADDRESS)
         self.__channel = channel
 
     def set_pwm (self, on):
-        # super().set_pwm(self.__channel, self.MAX-int(on), self.MAX)
-        super(PWM,self).set_pwm(self.__channel, self.MAX-int(on), self.MAX)
+        super().set_pwm(self.__channel, self.MAX-int(on), self.MAX)
 
 
 ############################################################################
 # Flask stuff ##############################################################
 @app.route('/relais')
-    relais = int(request.args.get('status', 'off'))  
+def Relais ():
+    relais = request.args.get('status', 'off')
+    print("Request: relais={}".format(relais))
     # TODO: validate param
 
-
+    if relais == 'on':
+        pwm.set_pwm(PWM.MIN)
+        # pwm.set_pwm(PWM.MAX)
+    else:
+        pwm.set_pwm(PWM.MIN)
+    return "ok"    
 
 
 
