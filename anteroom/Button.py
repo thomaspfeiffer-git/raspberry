f8dc1b1402961000e7bb9b796ad87d5c068312ad
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-10-28 06:54:46 +0200

    initial checkon

---
+++ anteroom/Button.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python -u
+# -*- coding: utf-8 -*-
+############################################################################
+# Button.py                                                                #
+# (c) https://github.com/thomaspfeiffer-git/raspberry, 2017                #
+############################################################################
+"""control lighting of our anteroom:
+   check button and toggle anteroom's light on and off"""
+
+### usage ###
+# sudo Button.py
+
+import RPi.GPIO as io
+import socket
+import sys
+import time
+import urllib
+
+sys.path.append("../libs/")
+from Logging import Log
+
+
+pin_ir = 8
+io.setmode(io.BOARD)
+io.setup(pin_ir, io.IN) 
+
+url = "http://localhost:5000/toggle" # TODO config file
+
+
+###############################################################################
+def CallToggle ():
+    try:
+        response = urllib.urlopen(url)
+        data = response.read().decode("utf-8")
+        Log("toggled!")
+    except (IOError):
+        Log("Error: {0[0]} {0[1]}".format(sys.exc_info()))
+        raise ValueError
+    except socket.timeout:
+        Log("socket.timeout: {0[0]} {0[1]}".format(sys.exc_info()))
+        raise ValueError
+
+
+###############################################################################
+## main ######################################################################
+last = None
+while True:
+    act = io.input(pin_ir)   # TODO: debounce if necessary
+    if act != last:
+        CallToggle()
+        last = act
+
+    time.sleep(0.05)
+
+# eof #
+
