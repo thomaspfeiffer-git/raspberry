2b691dcc691b3b3053c3aeacef8311a90156cb0c
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-11-04 21:11:35 +0100

    increased runtime stability in SendRelaisStatus()

--- anteroom/Relais.py
+++ anteroom/Relais.py
@@ -30,15 +30,16 @@ url = "http://localhost:5000/relais?status={}" # TODO config file
 def SendRelaisStatus (status):
     url_ = url.format(status)
 
+    Log("SendRelaisStatus: {}".format(status))
     try:
         response = urllib.urlopen(url_)
         data = response.read().decode("utf-8")
     except (IOError):
         Log("Error: {0[0]} {0[1]}".format(sys.exc_info()))
-        raise ValueError
+        # raise ValueError
     except socket.timeout:
         Log("socket.timeout: {0[0]} {0[1]}".format(sys.exc_info()))
-        raise ValueError
+        # raise ValueError
 
 
 ###############################################################################
