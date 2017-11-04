d48ced16d57891d68b61648751dde57d0383df6a
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-11-04 09:07:35 +0100

    working call to mjpg-streamer

--- felix/take_picture.sh
+++ felix/take_picture.sh
@@ -3,10 +3,8 @@
 
 export LIB....
 
-# -r 2592x1944
-
-mjpg_streamer -i input_uvc.so -d /dev/video0 -r 1280x960 -f 1 -q 95 \
-              -o output_file.so -d 15 -c ./blink.sh
+./mjpg_streamer -i "input_uvc.so -y 1 -f 1 -r 2592x1936 -q 95 -n" \
+              -o "output_file.so -d 15000"
 
 
 
