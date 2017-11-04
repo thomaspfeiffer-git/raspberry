8964aa0b6d8b33e4e82b53c97bd651c54ded74a4
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-11-04 21:13:07 +0100

    initial checkin

--- felix/Buttons.py
+++ felix/Buttons.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python -u
 # -*- coding: utf-8 -*-
-############################################################################
-# Buttons.py                                                               #
-# (c) https://github.com/thomaspfeiffer-git/raspberry, 2017                #
-############################################################################
-""" """
+###############################################################################
+# Buttons.py                                                                  #
+# (c) https://github.com/thomaspfeiffer-git/raspberry, 2017                   #
+###############################################################################
+"""controls the buttons:
+   - button 1: toggle camera on/off and shutdown
+   - button 2: using battery or main power supply (switch)"""
    
 
 ### usage ###
 # nohup sudo ./Buttons.py > buttons.log 2>&1 &
+
 
 import RPi.GPIO as io
 import socket
@@ -24,28 +27,26 @@ pin = 21    # TODO: config file
 io.setmode(io.BOARD)
 io.setup(pin, io.IN) 
 
-url = "http://localhost:5000/XXXXXXXXXXXXXXXXXXXXX" # TODO config file
-url_reset = "reset"
+url = "http://localhost:5000/{}" # TODO config file
+url_shutdown = "shutdown"
 url_toggle = "toggle"
 time_delay_to_reset = 3.0 # TODO: config file
 
 
 ###############################################################################
-def CallToggle (param):
+def CallFelixControl (param):
+    Log("CallFelixControl: {}".format(param))
     try:
         response = urllib.urlopen(url.format(param))
         data = response.read().decode("utf-8")
-        Log("{}".format(param))
-    except (IOError):
+    except IOError:
         Log("Error: {0[0]} {0[1]}".format(sys.exc_info()))
-        raise ValueError
     except socket.timeout:
-        LogZZ("socket.timeout: {0[0]} {0[1]}".format(sys.exc_info()))
-        raise ValueError
+        Log("socket.timeout: {0[0]} {0[1]}".format(sys.exc_info()))
 
 
 ###############################################################################
-## main ######################################################################
+## main #######################################################################
 time_pressed = None
 time_released = None
 last = None
@@ -58,10 +59,10 @@ while True:
                 time_pressed = time.time()
             if act == 1:
                 time_released = time.time()
-                if time_released - time_pressed > time_delay_to_reset
-                    Log("CallReset()")
+                if time_released - time_pressed > time_delay_to_reset:
+                    CallFelixControl(url_shutdown)
                 else:
-                    Log("CallToggle()")
+                    CallFelixControl(url_toggle)
                 time_pressed = None
                 time_released = None
         last = act
