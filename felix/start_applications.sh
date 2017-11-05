2958bc5f359b6fdcc3e3818a935c7021148b3686
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-11-05 12:28:26 +0100

    initial checkin

---
+++ felix/start_applications.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# starts all needed applications 
+
+
+LogDir=./Log/
+PicDir=./Pictures/
+
+if [ ! -d "$LogDir" ]; then
+   mkdir "$LogDir"
+fi
+if [ ! -d "$PicDir" ]; then
+   mkdir "$PicDir"
+fi
+
+
+nohup sudo ./Buttons.py > "$LogDir/buttons.log" 2>&1 &
+nohup ./Felix.py > "$LogDir/felix.log" 2>&1 &
+
