abf0d9431918a89c141ae4077580e7767b554a7f
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-11-08 06:18:29 +0100

    comppatibility python 2.x

--- pilix/config.py
+++ pilix/config.py
@@ -5,33 +5,49 @@
 ###############################################################################
 """lots of config stuff"""
 
-import configparser
+# configparser was named ConfigParser in Python 2.x
+try:
+    import configparser as cfgparser
+except Exception:
+    import ConfigParser as cfgparser 
+
 import os
 
+
 CONFIGFILE = "pilix.ini"    
-cfg = configparser.ConfigParser()
+cfg = cfgparser.ConfigParser()
+
 cfg.read(CONFIGFILE)
 
 class CONFIG:
     class PIN:
-         LED_Status  = int(cfg['Pilix']['pinLED_Status'])
-         LED_Picture = int(cfg['Pilix']['pinLED_Picture'])
+         # LED_Status  = int(cfg['Pilix']['pinLED_Status'])
+         # LED_Picture = int(cfg['Pilix']['pinLED_Picture'])
+         LED_Status  = int(cfg.get('Pilix', 'pinLED_Status'))
+         LED_Picture = int(cfg.get('Pilix', 'pinLED_Picture'))
 
-         BTN_Control = int(cfg['Pilix']['pinBTN_Control'])
-         BTN_Battery = int(cfg['Pilix']['pinBTN_Battery'])
+         # BTN_Control = int(cfg['Pilix']['pinBTN_Control'])
+         # BTN_Battery = int(cfg['Pilix']['pinBTN_Battery'])
+         BTN_Control = int(cfg.get('Pilix', 'pinBTN_Control'))
+         BTN_Battery = int(cfg.get('Pilix', 'pinBTN_Battery'))
 
     class Camera:
-         Intervall = int(cfg['Camera']['Intervall'])
+         # Intervall = int(cfg['Camera']['Intervall'])
+         Intervall = int(cfg.get('Camera', 'Intervall'))
 
     class API:
-         url = cfg['Pilix']['urlAPI']
+         # url = cfg['Pilix']['urlAPI']
+         url = cfg.get('Pilix', 'urlAPI')
 
     class File:
-         csv = cfg['Pilix']['CSV']
-         log = cfg['Pilix']['Logfile']
+         # csv = cfg['Pilix']['CSV']
+         # log = cfg['Pilix']['Logfile']
+         csv = cfg.get('Pilix', 'CSV')
+         log = cfg.get('Pilix', 'Logfile')
 
     class APP:
-         delayToShutdown = int(cfg['Pilix']['delayToShutdown'])
+         # delayToShutdown = int(cfg['Pilix']['delayToShutdown'])
+         delayToShutdown = int(cfg.get('Pilix', 'delayToShutdown'))
 
 # eof #
 
