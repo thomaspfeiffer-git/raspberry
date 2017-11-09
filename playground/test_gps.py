b2fb4bda6bd3b6ab2008dab5da8f6d63a0f94386
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-11-09 20:33:00 +0100

    migrated to python3

--- playground/test_gps.py
+++ playground/test_gps.py
@@ -9,6 +9,7 @@
 
 # you might need to install:
 # sudo apt-get install gpsd gpsd-clients python-gps -y
+# sudo -H pip3 install gps3
 
 # howto start and test:
 """
@@ -19,31 +20,25 @@ sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
 cgps -s
 """
 
-import gps
 import pprint
 
-# Listen on port 2947 (gpsd) of localhost
-session = gps.gps("localhost", "2947")
-session.stream(gps.WATCH_ENABLE | gps.WATCH_NEWSTYLE)
+from gps3 import gps3
+gps_socket = gps3.GPSDSocket()
+data_stream = gps3.DataStream()
+gps_socket.connect()
+gps_socket.watch()
 
-while True:
-    try:
-        report = session.next()
-        # Wait for a 'TPV' report and display the current time
-        # To see all report data, uncomment the line below
-        # print report
-        if report['class'] == 'TPV':
-        #     if hasattr(report, 'time'):
-        #         print report.time
-            pprint.pprint(report)
-    except KeyError:
-        pass
-    except KeyboardInterrupt:
-        quit()
-    except StopIteration:
-        session = None
-        print "GPSD has terminated"
 
+for new_data in gps_socket:
+    if new_data:
+        data_stream.unpack(new_data)
+ 
+        print('Time:      ', data_stream.TPV['time'])
+        print('Longitude: ', data_stream.TPV['lon'])
+        print('Latitude:  ', data_stream.TPV['lat'])
+        print('Altitude:  ', data_stream.TPV['alt'])
+        print('Climb:     ', data_stream.TPV['climb'])
+        print('Speed:     ', data_stream.TPV['speed'])
 
 # eof #
 
