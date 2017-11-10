a180f64d6d3b0cce3461051e2fafae09c46d9778
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-11-10 19:43:15 +0100

    added output to csv file

--- playground/test_gps.py
+++ playground/test_gps.py
@@ -20,6 +20,7 @@ sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
 cgps -s
 """
 
+import csv
 import pprint
 
 from gps3 import gps3
@@ -29,21 +30,43 @@ gps_socket.connect()
 gps_socket.watch()
 
 
+
+V_GPS_Time   = "GPS Time"
+V_GPS_Lon    = "GPS Longitude"
+V_GPS_Lat    = "GPS Latitude"
+V_GPS_Alt    = "GPS Altitude"
+V_GPS_Climb  = "GPS Climb"
+V_GPS_Speed  = "GPS Speed"
+V_GPS_Track  = "GPS Track"
+V_GPS_ErrLon = "GPS Err Longitude"
+V_GPS_ErrLat = "GPS Err Latitude"
+V_GPS_ErrAlt = "GPS Err Altitude"
+
+fieldnames = [V_GPS_Time, V_GPS_Lon, V_GPS_Lat, V_GPS_Alt, V_GPS_Climb,
+              V_GPS_Speed, V_GPS_Track, V_GPS_ErrLon, V_GPS_ErrLat, V_GPS_ErrAlt]
+
+with open('test_gps.csv', 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+    writer.writeheader()
+
+
 for new_data in gps_socket:
     if new_data:
         data_stream.unpack(new_data)
+        gps_data = {V_GPS_Time: data_stream.TPV['time'],
+                    V_GPS_Lon: data_stream.TPV['lon'],
+                    V_GPS_Lat: data_stream.TPV['lat'],
+                    V_GPS_Alt: data_stream.TPV['alt'],
+                    V_GPS_Climb: data_stream.TPV['alt'],
+                    V_GPS_Speed: data_stream.TPV['speed'],
+                    V_GPS_Track: data_stream.TPV['track'],
+                    V_GPS_ErrLon: data_stream.TPV['epx'],
+                    V_GPS_ErrLat: data_stream.TPV['epy'],
+                    V_GPS_ErrAlt: data_stream.TPV['epv']}
+        with open('test_gps.csv', 'a', newline='') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames, delimiter=';')
+            writer.writerow(gps_data)
  
-        print('Time:      ', data_stream.TPV['time'])
-        print('Longitude: ', data_stream.TPV['lon'])
-        print('Latitude:  ', data_stream.TPV['lat'])
-        print('Altitude:  ', data_stream.TPV['alt'])
-        print('Climb:     ', data_stream.TPV['climb'])
-        print('Speed:     ', data_stream.TPV['speed'])
-        print("Track:     ", data_stream.TPV['track'])
-        print("Long Err:  ", data_stream.TPV['epx'])
-        print("Lat Err:   ", data_stream.TPV['epy'])
-        print("Alt Err:   ", data_stream.TPV['epv'])
-        print("")
 
 # eof #
 
