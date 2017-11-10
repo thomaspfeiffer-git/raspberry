7e2146b5f8a7ae8015c83272c91829554c1fbd9c
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-11-10 20:44:24 +0100

    preparing for gps

--- pilix/Pilix.py
+++ pilix/Pilix.py
@@ -73,6 +73,16 @@ V_RunningOnBattery   = "running on battery"
 V_TemperatureCPU     = "Temperature CPU"
 V_Timestamp          = "Timestamp"
 V_Time               = "Time"
+V_GPS_Time           = "GPS Time"
+V_GPS_Lon            = "GPS Longitude"
+V_GPS_Lat            = "GPS Latitude"
+V_GPS_Alt            = "GPS Altitude"
+V_GPS_Climb          = "GPS Climb"
+V_GPS_Speed          = "GPS Speed"
+V_GPS_Track          = "GPS Track"
+V_GPS_ErrLon         = "GPS Err Longitude"
+V_GPS_ErrLat         = "GPS Err Latitude"
+V_GPS_ErrAlt         = "GPS Err Altitude"
 
 
 app = Flask(__name__)
@@ -201,7 +211,7 @@ class Display (object):
         self.draw_line("Temp: {} °C".format(data[V_TemperatureBox]))
         self.draw_line("Press: {} hPa".format(data[V_Pressure] / 100.0))
         self.draw_line("Voltage: {:.2f} V".format(data[V_Voltage]))
-        self.draw_line("Temp CPU: {} °C".format(data[V_TemperatureCPU]))
+        self.draw_line("X: {} Y: {}".format(data[V_GPS_Lon], data[V_GPS_Lat]))
         self.draw_line("Time: {}".format(data[V_Time]))
 
         self.display.image(self.image)
@@ -217,7 +227,9 @@ class Display (object):
 class CSV (object):
     fieldnames = [V_Time, V_TemperatureBox, V_TemperatureOutside, 
                   V_Pressure, V_Voltage, V_RunningOnBattery, V_TemperatureCPU, 
-                  V_Timestamp]
+                  V_Timestamp, V_GPS_Time, V_GPS_Lon, V_GPS_Lat, V_GPS_Alt, 
+                  V_GPS_Climb, V_GPS_Speed, V_GPS_Track, V_GPS_ErrLon, 
+                  V_GPS_ErrLat, V_GPS_ErrAlt]
 
     def __init__ (self):
         with open(CONFIG.File.csv, 'w', newline='') as csvfile:
@@ -243,10 +255,25 @@ class Control (threading.Thread):
         self.running_on_battery = False
         self._running = True
 
+
+    def get_gps_data (self):
+        return {V_GPS_Time: "17:17:17",
+                V_GPS_Lon: "18.18",
+                V_GPS_Lat: "48.48",
+                V_GPS_Alt: "222",
+                V_GPS_Climb: "10",
+                V_GPS_Speed: "100",
+                V_GPS_Track: "47",
+                V_GPS_ErrLon: "3",
+                V_GPS_ErrLat: "4",
+                V_GPS_ErrAlt: "5"}
+
     def run (self):
         while self._running:
             data = self.sensors.read()
             data[V_RunningOnBattery] = self.running_on_battery
+            data.appand(self.get_gps_data())
+
             self.display.print(data)
             self.csv.write(data)
             for _ in range(15):  # sleep for 15 x 2 = 30 seconds
