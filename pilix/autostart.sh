53f30279410c96686bb03c0b9b773ee723d2473a
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-12-18 21:28:50 +0100

    improved version

--- pilix/autostart.sh
+++ pilix/autostart.sh
@@ -4,7 +4,7 @@
 # shall be called in /etc/rc.local
 
 
-# switching wifi off 3 mins after startup (safe battery)
+# switching wifi off one minute after startup (safe battery)
 # starting internet access (GSM/GPRS using SIM800L)
 (sleep 60 ; sudo ifconfig wlan0 down ; sleep 10 ; sudo pon fona ) &
 
