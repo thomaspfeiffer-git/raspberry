f705a36b6e6ac045e8d309d998024d5a2ec7438d
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2017-12-30 21:11:09 +0100

    tweaking

--- playground/lora/lora_rec_display.py
+++ playground/lora/lora_rec_display.py
@@ -310,7 +310,7 @@ class RF95:
         # default mode
         self.set_mode_idle()
 
-        self.set_modem_config(TP_2)
+        self.set_modem_config(TP_3)
         self.set_preamble_length(8)
         
         return True
