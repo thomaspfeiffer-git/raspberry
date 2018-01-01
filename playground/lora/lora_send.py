db7358170f40804952f510262c1ee01cf58f4712
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2018-01-01 17:21:53 +0100

    tweaking

--- playground/lora/lora_send.py
+++ playground/lora/lora_send.py
@@ -244,6 +244,11 @@ TP_4 = (0x78, 0xB4, 0x00)
 #   Signalbandwidth: 62.5 kHz
 TP_5 = (0x68, 0xB4, 0x00)
 
+#   Chips/Symbol: 4096
+#   Codingrate: 4 (4/8)
+#   Signalbandwidth: 500 kHz
+TP_6 = (0x98, 0xC4, 0x00)
+
 # set_modem_mode
 
 
@@ -312,7 +317,7 @@ class RF95:
         # default mode
         self.set_mode_idle()
 
-        self.set_modem_config(TP_1)
+        self.set_modem_config(TP_6)
         self.set_preamble_length(8)
         
         return True
