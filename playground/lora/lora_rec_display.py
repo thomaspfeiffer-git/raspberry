b073264e1a3c51feac89831ed397e00b17630dc7
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2018-01-02 19:29:30 +0100

    started cleanup and refactoring

--- playground/lora/lora_rec_display.py
+++ playground/lora/lora_rec_display.py
@@ -18,266 +18,28 @@
 # along with this program.  
 # If not, see <http://www.gnu.org/licenses/>.
 
-import time
-import sys
-import spidev
 import RPi.GPIO as GPIO
-
+import spidev
+import sys
+import time
 
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
 sys.path.append('../../libs')
+from Logging import Log
+from Shutdown import Shutdown
 from actuators.SSD1306 import SSD1306
+from RFM9x_constants import *
 
 
-
-FXOSC=32000000
-FSTEP = FXOSC / 524288
-
-# Register names (LoRa Mode, from table 85)
-REG_00_FIFO=0x00
-REG_01_OP_MODE=0x01
-REG_02_RESERVED=0x02
-REG_03_RESERVED=0x03
-REG_04_RESERVED=0x04
-REG_05_RESERVED=0x05
-REG_06_FRF_MSB=0x06
-REG_07_FRF_MID=0x07
-REG_08_FRF_LSB=0x08
-REG_09_PA_CONFIG=0x09
-REG_0A_PA_RAMP=0x0a
-REG_0B_OCP=0x0b
-REG_0C_LNA=0x0c
-REG_0D_FIFO_ADDR_PTR=0x0d
-REG_0E_FIFO_TX_BASE_ADDR=0x0e
-REG_0F_FIFO_RX_BASE_ADDR=0x0f
-REG_10_FIFO_RX_CURRENT_ADDR=0x10
-REG_11_IRQ_FLAGS_MASK=0x11
-REG_12_IRQ_FLAGS=0x12
-REG_13_RX_NB_BYTES=0x13
-REG_14_RX_HEADER_CNT_VALUE_MSB=0x14
-REG_15_RX_HEADER_CNT_VALUE_LSB=0x15
-REG_16_RX_PACKET_CNT_VALUE_MSB=0x16
-REG_17_RX_PACKET_CNT_VALUE_LSB=0x17
-REG_18_MODEM_STAT=0x18
-REG_19_PKT_SNR_VALUE=0x19
-REG_1A_PKT_RSSI_VALUE=0x1a
-REG_1B_RSSI_VALUE=0x1b
-REG_1C_HOP_CHANNEL=0x1c
-REG_1D_MODEM_CONFIG1=0x1d
-REG_1E_MODEM_CONFIG2=0x1e
-REG_1F_SYMB_TIMEOUT_LSB=0x1f
-REG_20_PREAMBLE_MSB=0x20
-REG_21_PREAMBLE_LSB=0x21
-REG_22_PAYLOAD_LENGTH=0x22
-REG_23_MAX_PAYLOAD_LENGTH=0x23
-REG_24_HOP_PERIOD=0x24
-REG_25_FIFO_RX_BYTE_ADDR=0x25
-REG_26_MODEM_CONFIG3=0x26
-REG_28_FREQ_ERROR=0x28
-REG_31_DETECT_OPT=0x31
-REG_37_DETECTION_THRESHOLD=0x37
-
-REG_40_DIO_MAPPING1=0x40
-REG_41_DIO_MAPPING2=0x41
-REG_42_VERSION=0x42
-
-REG_4B_TCXO=0x4b
-REG_4D_PA_DAC=0x4d
-REG_5B_FORMER_TEMP=0x5b
-REG_61_AGC_REF=0x61
-REG_62_AGC_THRESH1=0x62
-REG_63_AGC_THRESH2=0x63
-REG_64_AGC_THRESH3=0x64
-
-# REG_01_OP_MODE                             0x01
-LONG_RANGE_MODE=0x80
-ACCESS_SHARED_REG=0x40
-MODE=0x07
-MODE_SLEEP=0x00
-MODE_STDBY=0x01
-MODE_FSTX=0x02
-MODE_TX=0x03
-MODE_FSRX=0x04
-MODE_RXCONTINUOUS=0x05
-MODE_RXSINGLE=0x06
-MODE_CAD=0x07
-
-# REG_09_PA_CONFIG                           0x09
-PA_SELECT=0x80
-MAX_POWER=0x70
-OUTPUT_POWER=0x0f
-
-# REG_0A_PA_RAMP                             0x0a
-LOW_PN_TX_PLL_OFF=0x10
-PA_RAMP=0x0f
-PA_RAMP_3_4MS=0x00
-PA_RAMP_2MS=0x01
-PA_RAMP_1MS=0x02
-PA_RAMP_500US=0x03
-PA_RAMP_250US=0x0
-PA_RAMP_125US=0x05
-PA_RAMP_100US=0x06
-PA_RAMP_62US=0x07
-PA_RAMP_50US=0x08
-PA_RAMP_40US=0x09
-PA_RAMP_31US=0x0a
-PA_RAMP_25US=0x0b
-PA_RAMP_20US=0x0c
-PA_RAMP_15US=0x0d
-PA_RAMP_12US=0x0e
-PA_RAMP_10US=0x0f
-
-# REG_0B_OCP                                 0x0b
-OCP_ON=0x20
-OCP_TRIM=0x1f
-
-# REG_0C_LNA                                 0x0c
-LNA_GAIN=0xe0
-LNA_BOOST=0x03
-LNA_BOOST_DEFAULT=0x00
-LNA_BOOST_150PC=0x11
-LNA_BOOST_MAX=0x23  # Added by TP; TODO ---> bring to production
-                    # taken from https://github.com/PiInTheSky/lora-gateway/blob/master/gateway.c#L118
-
-# REG_11_IRQ_FLAGS_MASK                      0x11
-RX_TIMEOUT_MASK=0x80
-RX_DONE_MASK=0x40
-PAYLOAD_CRC_ERROR_MASK=0x20
-VALID_HEADER_MASK=0x10
-TX_DONE_MASK=0x08
-CAD_DONE_MASK=0x04
-FHSS_CHANGE_CHANNEL_MASK=0x02
-CAD_DETECTED_MASK=0x01
-
-# REG_12_IRQ_FLAGS                           0x12
-RX_TIMEOUT=0x80
-RX_DONE=0x40
-PAYLOAD_CRC_ERROR=0x20
-VALID_HEADER=0x10
-TX_DONE=0x08
-CAD_DONE=0x04
-FHSS_CHANGE_CHANNEL=0x02
-CAD_DETECTED=0x01
-
-# REG_18_MODEM_STAT                          0x18
-RX_CODING_RATE=0xe0
-MODEM_STATUS_CLEAR=0x10
-MODEM_STATUS_HEADER_INFO_VALID=0x08
-MODEM_STATUS_RX_ONGOING=0x04
-MODEM_STATUS_SIGNAL_SYNCHRONIZED=0x02
-MODEM_STATUS_SIGNAL_DETECTED=0x01
-
-# REG_1C_HOP_CHANNEL                         0x1c
-PLL_TIMEOUT=0x80
-RX_PAYLOAD_CRC_IS_ON=0x40
-FHSS_PRESENT_CHANNEL=0x3f
-
-# REG_1D_MODEM_CONFIG1                       0x1d
-BW_7K8HZ=0x00
-BW_10K4HZ=0x10
-BW_15K6HZ=0x20
-BW_20K8HZ=0x30
-BW_31K25HZ=0x40
-BW_41K7HZ=0x50
-BW_62K5HZ=0x60
-BW_125KHZ=0x70
-BW_250KHZ=0x80
-BW_500KHZ=0x90
-
-CODING_RATE_4_5=0x02
-CODING_RATE_4_6=0x04
-CODING_RATE_4_7=0x06
-CODING_RATE_4_8=0x08
-
-IMPLICIT_HEADER_MODE_ON=0x00
-IMPLICIT_HEADER_MODE_OFF=0x01
-
-# REG_1E_MODEM_CONFIG2                       0x1e
-SPREADING_FACTOR_64CPS=0x60
-SPREADING_FACTOR_128CPS=0x70
-SPREADING_FACTOR_256CPS=0x80
-SPREADING_FACTOR_512CPS=0x90
-SPREADING_FACTOR_1024CPS=0xa0
-SPREADING_FACTOR_2048CPS=0xb0
-SPREADING_FACTOR_4096CPS=0xc0
-TX_CONTINUOUS_MODE_ON=0x08
-TX_CONTINUOUS_MODE_OFF=0x00
-RX_PAYLOAD_CRC_ON=0x02
-RX_PAYLOAD_CRC_OFF=0x00
-SYM_TIMEOUT_MSB=0x03
-
-# REG_26_MODEM_CONFIG3
-AGC_AUTO_ON=0x04
-AGC_AUTO_OFF=0x00
-
-# REG_4D_PA_DAC                              0x4d
-PA_DAC_DISABLE=0x04
-PA_DAC_ENABLE=0x07
-
-MAX_MESSAGE_LEN=255
-
-# default params
-Bw125Cr45Sf128 = (0x72,   0x74,    0x00)
-Bw500Cr45Sf128 = (0x92,   0x74,    0x00)
-Bw31_25Cr48Sf512 = (0x48,   0x94,    0x00)
-Bw125Cr48Sf4096 = (0x78,   0xc4,    0x00)
-
-
-# Bw125Cr45Sf128 : Bw = 125 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on. Default medium range.
-# Bw500Cr45Sf128 : Bw = 500 kHz, Cr = 4/5, Sf = 128chips/symbol, CRC on. Fast+short range.
-# Bw31_25Cr48Sf512 : Bw = 31.25 kHz, Cr = 4/8, Sf = 512chips/symbol, CRC on. Slow+long range.
-# Bw125Cr48Sf4096 : Bw = 125 kHz, Cr = 4/8, Sf = 4096chips/symbol, CRC on. Slow+long range.
-
-
-TP_0 = Bw125Cr45Sf128
-
-#   Chips/Symbol: 2048
-#   Codingrate: 4 (4/8)
-#   Signalbandwidth: 20.8 kHz
-TP_1 = (0x38, 0xB4, 0x04|0x08)
-
-#   Chips/Symbol: 1024
-#   Codingrate: 4 (4/8)
-#   Signalbandwidth: 20.8 kHz
-TP_2 = (0x38, 0xA4, 0x04|0x08)
-
-#   Chips/Symbol: 1024
-#   Codingrate: 4 (4/8)
-#   Signalbandwidth: 62.5 kHz
-TP_3 = (0x68, 0xA4, 0x04|0x08)
-
-#   Chips/Symbol: 4096
-#   Codingrate: 4 (4/8)
-#   Signalbandwidth: 62.5 kHz
-TP_4 = (0x68, 0xC4, 0x04|0x08)
-
-#   Chips/Symbol: 4096
-#   Codingrate: 4 (4/8)
-#   Signalbandwidth: 41.7 kHz
-TP_5 = (0x58, 0xC4, 0x04|0x08)
-
-# set_modem_mode
-
-
-# SPI
-SPI_WRITE_MASK=0x80
-
-# Modes
-RADIO_MODE_INITIALISING=0
-RADIO_MODE_SLEEP=1
-RADIO_MODE_IDLE=2
-RADIO_MODE_TX=3
-RADIO_MODE_RX=4
-RADIO_MODE_CAD=5
-
-class RF95:
-    def __init__(self, cs=0, int_pin=25, reset_pin=None):
-        # init class
+class RF95 (object):
+    def __init__ (self, config, frequency, int_pin, reset_pin, cs=0):
         self.spi = spidev.SpiDev()
-        self.cs = cs
+        self.spi_cs = cs
+        self.config = config
+        self.frequency = frequency
         self.int_pin = int_pin
         self.reset_pin = reset_pin
         self.mode = RADIO_MODE_INITIALISING
@@ -288,22 +50,20 @@ class RF95:
         self.tx_good = 0    # tx packets sent
         self.rx_good = 0    # rx packets recv
         self.rx_buf_valid = False 
-        self.frequency    = None
     
 
-    def init(self):
-        # open SPI and initialize RF95
-        self.spi.open(0,self.cs)
-        # self.spi.max_speed_hz = 488000
+    def init (self):
+        """open SPI and initialize RF95"""
+        self.spi.open(0, self.spi_cs)
         self.spi.max_speed_hz = 244000
         self.spi.close()
 
         # set interrupt pin
-        # GPIO.setmode(GPIO.BCM)   # old: BCM
         GPIO.setmode(GPIO.BOARD)
         GPIO.setwarnings(False)
         GPIO.setup(self.int_pin, GPIO.IN)
-        GPIO.add_event_detect(self.int_pin, GPIO.RISING, callback=self.handle_interrupt)
+        GPIO.add_event_detect(self.int_pin, GPIO.RISING, 
+                              callback=self.handle_interrupt)
         
         # set reset pin
         if self.reset_pin != None:
@@ -315,7 +75,7 @@ class RF95:
         # set sleep mode and LoRa mode
         self.spi_write(REG_01_OP_MODE, MODE_SLEEP | LONG_RANGE_MODE)
         
-        time.sleep(0.01)        
+        time.sleep(0.05)        
         # check if we are set
         if self.spi_read(REG_01_OP_MODE) != (MODE_SLEEP | LONG_RANGE_MODE):
             return False
@@ -327,8 +87,9 @@ class RF95:
         # default mode
         self.set_mode_idle()
 
-        self.set_modem_config(TP_5)
+        self.set_modem_config(self.config)
         self.set_preamble_length(8)
+        self.set_frequency(self.frequency)
         
         return True
 
@@ -392,28 +153,27 @@ class RF95:
         self.spi_write(REG_12_IRQ_FLAGS, 0xff) # Clear all IRQ flags
 
 
-
     def spi_write(self, reg, data):
-        self.spi.open(0,self.cs)
+        self.spi.open(0,self.spi_cs)
         # transfer one byte
         self.spi.xfer2([reg | SPI_WRITE_MASK, data])
         self.spi.close()
 
     def spi_read(self, reg):
-        self.spi.open(0,self.cs)
+        self.spi.open(0,self.spi_cs)
         data = self.spi.xfer2([reg & ~SPI_WRITE_MASK, 0])
         self.spi.close()
         return data[1]
 
     def spi_write_data(self, reg, data):
-        self.spi.open(0, self.cs)
+        self.spi.open(0, self.spi_cs)
         # transfer byte list
         self.spi.xfer2([reg | SPI_WRITE_MASK] + data)
         self.spi.close()
 
     def spi_read_data(self, reg, length):
         data = []
-        self.spi.open(0, self.cs)
+        self.spi.open(0, self.spi_cs)
         # start address + amount of bytes to read
         data = self.spi.xfer2([reg & ~SPI_WRITE_MASK] + [0]*length)
         self.spi.close()
@@ -452,7 +212,6 @@ class RF95:
             self.mode = RADIO_MODE_TX   
         return True
 
-
     def set_tx_power(self, power):
         if power>23:
             power=23
@@ -476,24 +235,6 @@ class RF95:
         self.spi_write(REG_26_MODEM_CONFIG3, config[2])
         # self.spi_write(REG_0C_LNA, LNA_BOOST_MAX) # TODO only in receiver mode!
         
-    # set custom mode
-    def set_modem_config_custom(self, \
-        bandwidth = BW_125KHZ, \
-        coding_rate = CODING_RATE_4_5, \
-        imp_header = IMPLICIT_HEADER_MODE_OFF, \
-        spreading_factor = SPREADING_FACTOR_128CPS, \
-        crc = RX_PAYLOAD_CRC_ON, \
-        continuous_tx = TX_CONTINUOUS_MODE_OFF, \
-        timeout = SYM_TIMEOUT_MSB, \
-        agc_auto = AGC_AUTO_OFF):
-
-        self.spi_write(REG_1D_MODEM_CONFIG1, \
-            bandwidth | coding_rate | implicit_header)
-        self.spi_write(REG_1E_MODEM_CONFIG2, \
-            spreading_factor | continuous_tx | crc | timeout)
-        self.spi_write(REG_26_MODEM_CONFIG3, \
-            agc_auto)
-
     def set_preamble_length(self, length):
         self.spi_write(REG_20_PREAMBLE_MSB, length >> 8)
         self.spi_write(REG_21_PREAMBLE_LSB, length & 0xff)
@@ -505,7 +246,7 @@ class RF95:
     
         self.wait_packet_sent() 
         self.set_mode_idle()
-        # beggining of FIFO
+        # beginning of FIFO
         #self.spi_write(REG_0E_FIFO_TX_BASE_ADDR, 0)
         self.spi_write(REG_0D_FIFO_ADDR_PTR, 0)
 
@@ -513,7 +254,7 @@ class RF95:
         self.spi_write_data(REG_00_FIFO, data)
         self.spi_write(REG_22_PAYLOAD_LENGTH, len(data))
 
-                # put radio in TX mode
+        # put radio in TX mode
         self.set_mode_tx()
         return True
 
@@ -562,38 +303,29 @@ class RF95:
         if self.int_pin:
             GPIO.cleanup(self.int_pin)
 
-# Example, send two strings and (uncomment to) receive and print a reply
+
+###############################################################################
+# Exit ########################################################################
+def shutdown_application ():
+    """cleanup stuff"""
+    rf95.set_mode_idle()
+    rf95.cleanup()
+
+    sys.exit(0)
+
+
+###############################################################################
+# Main ########################################################################
 if __name__ == "__main__":
-    rf95 = RF95(0, 31, 32) 
+    shutdown = Shutdown(shutdown_func=shutdown_application)
+
+    rf95 = RF95(config=TP_5, frequency=433500000, int_pin=31, reset_pin=32) 
     if not rf95.init():
-        print("RF95 not found")
+        Log("Error: RF95 not found")
         rf95.cleanup()
-        quit(1)
+        sys.exit(1)
     else:
-        print("RF95 LoRa mode ok")
-
-
-# Hints on distance tuning:
-# http://wiki.dragino.com/index.php?title=LoRa_Questions#Check_the_Modem_Setting_in_Software
-
-
-        # set frequency and power
-    rf95.set_frequency(433500000)
-
-
-    # Sender ##############################################
-    """
-    count = 0
-    while True:
-        payload = "ID: {}; {}".format(count, time.strftime("%H%M%S"))
-
-        print("Sending Data ({})".format(payload))
-        rf95.send(rf95.str_to_data(payload))
-        rf95.wait_packet_sent()
-        print("Sent!")
-        count += 1
-        time.sleep(30);
-    """
+        Log("RF95 LoRa mode ok")
 
     disp = SSD1306()
     disp.begin()
@@ -611,8 +343,6 @@ if __name__ == "__main__":
 
 
     # Receiver ###########################################
-
-    # now wait for reply
     while True:
         while not rf95.available():
             pass
@@ -635,7 +365,5 @@ if __name__ == "__main__":
         disp.image(image)
         disp.display()
 
-
-    rf95.set_mode_idle()
-    rf95.cleanup()
+# eof #
 
