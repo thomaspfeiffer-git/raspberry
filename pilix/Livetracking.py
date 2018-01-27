f922050b60e9e77bb1a051a15dc49448f26850d3
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2018-01-27 13:08:27 +0100

    some optimizations

--- pilix/Livetracking.py
+++ pilix/Livetracking.py
@@ -179,7 +179,10 @@ class Pilix_RFM96W (RFM9x):
             Log("RFM96W LoRa mode ok!")
 
         if sender:    
-            self.set_tx_power(CONFIG.Livetracking.LoRa_TX_Power)
+            # self.set_tx_power(CONFIG.Livetracking.LoRa_TX_Power)
+            self.set_tx_power(PA_MAX_BOOST)
+        else:    
+            self.set_lna(LNA_BOOST_MAX)
 
 
 ###############################################################################
