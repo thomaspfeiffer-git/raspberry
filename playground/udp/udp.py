8316493b56b968324cad8cb211bcd83394fc90f4
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2018-03-14 06:03:31 +0100

    continued implementation

--- playground/udp/udp.py
+++ playground/udp/udp.py
@@ -9,26 +9,59 @@ Basic data transfer using UDP.
 """
 
 
+import argparse
+import socket
 import sys
+import time
 
-sys.path.append("../libs/")
+sys.path.append("../../libs/")
 from Logging import Log
 from Shutdown import Shutdown
+
+
+
+###############################################################################
+# CONFIG ######################################################################
+class CONFIG (object):
+    IP_ADDRESS_SERVER = "127.0.0.1"
+    UDP_PORT = 20519
+    MAX_PACKET_SIZE = 256
 
 
 ###############################################################################
 # Sender ######################################################################
 class Sender (object):
-    pass
+    def __init__ (self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self._running = True
 
+    def run (self):
+        while self._running:
+            datagram = "Daten via UDP: {}".format(time.strftime("%Y%m%d %X")).encode('utf-8')
+            sent = self.socket.sendto(datagram, 
+                                      (CONFIG.IP_ADDRESS_SERVER, CONFIG.UDP_PORT))
+            Log("Sent bytes (UDP): {}; data: {}".format(sent,datagram))
+            time.sleep(5)
 
+    def stop (self):
+        self._running = False
 
 
 ###############################################################################
 # Receiver ####################################################################
 class Receiver (object):
-    pass
+    def __init__ (self):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind(("127.0.0.1", CONFIG.UDP_PORT))
+        self._running = True
 
+    def run (self):
+        while self._running:
+            datagram = self.socket.recv(CONFIG.MAX_PACKET_SIZE).decode('utf-8')
+            Log("Received: {}".format(datagram))
+
+    def stop (self):
+        self._running = False
 
 
 ###############################################################################
@@ -36,6 +69,7 @@ class Receiver (object):
 def shutdown_application ():
     """cleanup stuff"""
     Log("Stopping application")
+    udp.stop()
     Log("Application stopped")
     sys.exit(0)
 
@@ -45,8 +79,18 @@ def shutdown_application ():
 if __name__ == "__main__":
     shutdown_application = Shutdown(shutdown_func=shutdown_application)
 
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--send", help="send data", action="store_true")
+    group.add_argument("--receive", help="receive data", action="store_true")
+    args = parser.parse_args()
 
+    if args.send:
+        udp = Sender()
+    if args.receive:
+        udp = Receiver()
 
+    udp.run()    
 
 # eof #
 
