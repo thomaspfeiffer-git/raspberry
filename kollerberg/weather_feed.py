39ca4567918f297a14ca20bf0d6dbe69e02a9d9e
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2018-03-17 10:48:53 +0100

    added threads

--- kollerberg/weather_feed.py
+++ kollerberg/weather_feed.py
@@ -16,6 +16,7 @@ rrd database and to the sensor value queue.
 import socket
 import sys
 import threading
+import time
 
 sys.path.append('../libs')
 
@@ -27,22 +28,49 @@ from Shutdown import Shutdown
 ###############################################################################
 ###############################################################################
 class UDP_Receiver (threading.Thread):
-    pass
+    def __init__ (self):
+        threading.Thread.__init__(self)
+        self._running = True
 
+    def run (self):
+        while self._running:
+            Log("in UDP_Receiver.run()")
+            time.sleep(10) # TODO: interruptible sleep
+
+    def stop (self):
+        self._running = False
 
 
 ###############################################################################
 ###############################################################################
 class ToQueue (threading.Thread):
-    pass
+    def __init__ (self):
+        threading.Thread.__init__(self)
+        self._running = True
 
+    def run (self):
+        while self._running:
+            Log("in ToQueue.run()")
+            time.sleep(10) # TODO: interruptible sleep
+
+    def stop (self):
+        self._running = False
 
 
 ###############################################################################
 ###############################################################################
 class ToRRD (threading.Thread):
-    pass
+    def __init__ (self):
+        threading.Thread.__init__(self)
+        self._running = True
 
+    def run (self):
+        while self._running:
+            Log("in ToRRD.run()")
+            time.sleep(10) # TODO: interruptible sleep
+
+    def stop (self):
+        self._running = False
 
 
 ###############################################################################
@@ -50,6 +78,12 @@ class ToRRD (threading.Thread):
 def shutdown_application ():
     """cleanup stuff"""
     Log("Stopping application")
+    to_rrd.stop()
+    to_rrd.join()
+    to_queue.stop()
+    to_queue.join()
+    udp.stop()
+    udp.join()
     Log("Application stopped")
     sys.exit(0)
 
@@ -59,7 +93,16 @@ def shutdown_application ():
 if __name__ == '__main__':
     shutdown_application = Shutdown(shutdown_func=shutdown_application)
 
+    udp = UDP_Receiver()
+    to_queue = ToQueue()
+    to_rrd = ToRRD()
 
+    udp.start()
+    to_queue.start()
+    to_rrd.start()
+
+    while True:
+        time.sleep(120)
 
 # eof #
 
