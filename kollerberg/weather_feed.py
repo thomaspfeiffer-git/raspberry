f23d1974c23fe0d6ff93638845d45aff8add5381
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2018-03-17 11:08:43 +0100

    implemented udp receiver

--- kollerberg/weather_feed.py
+++ kollerberg/weather_feed.py
@@ -13,6 +13,7 @@ rrd database and to the sensor value queue.
 # TODO
 
 
+import configparser as cfgparser
 import socket
 import sys
 import threading
@@ -24,18 +25,45 @@ from Commons import Digest
 from Logging import Log
 from Shutdown import Shutdown
 
+CREDENTIALS = "/home/pi/configs/weather_feed.cred"
+
 
 ###############################################################################
-###############################################################################
+# UDP_Receiver ################################################################
 class UDP_Receiver (threading.Thread):
     def __init__ (self):
         threading.Thread.__init__(self)
+
+        cred = cfgparser.ConfigParser()
+        cred.read(CREDENTIALS)
+
+        self.SECRET = cred['UDP']['SECRET']
+        self.UDP_PORT = int(cred['UDP']['UDP_PORT'])
+        self.MAX_PACKET_SIZE = int(cred['UDP']['MAX_PACKET_SIZE'])
+
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.socket.bind((self.my_ip(), self.UDP_PORT))
+        self.digest = Digest(self.SECRET)
+
         self._running = True
+
+    @staticmethod
+    def my_ip ():
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        try:
+            # doesn't even have to be reachable
+            s.connect(('10.255.255.255', 1))
+            IP = s.getsockname()[0]
+        except:
+            IP = '127.0.0.1'
+        finally:
+            s.close()
+            return IP
 
     def run (self):
         while self._running:
-            Log("in UDP_Receiver.run()")
-            time.sleep(10) # TODO: interruptible sleep
+            datagram = self.socket.recv(self.MAX_PACKET_SIZE).decode('utf-8')
+            Log("Received: {}".format(datagram))
 
     def stop (self):
         self._running = False
@@ -91,7 +119,7 @@ def shutdown_application ():
 ###############################################################################
 # main ########################################################################
 if __name__ == '__main__':
-    shutdown_application = Shutdown(shutdown_func=shutdown_application)
+    shutdown_app = Shutdown(shutdown_func=shutdown_application)
 
     udp = UDP_Receiver()
     to_queue = ToQueue()
