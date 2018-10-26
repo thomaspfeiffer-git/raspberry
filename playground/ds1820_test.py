0d7814d592f5e718db6b613d254c5836fa1ba95e
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2018-10-26 22:29:29 +0200

    added logging and loop

--- playground/ds1820_test.py
+++ playground/ds1820_test.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python3
 
 import sys
+import time
 
 sys.path.append('../libs')
 sys.path.append('../libs/sensors')
 
+from Logging import Log
 from DS1820 import DS1820
 
-ds1820 = DS1820("/sys/bus/w1/devices/28-000006b50d05/w1_slave")
+ds1820 = DS1820("/sys/bus/w1/devices/28-000008561957/w1_slave")
 
-print(ds1820.read_temperature())
+while True:
+    Log(ds1820.read_temperature())
+    time.sleep(10)
 
 
