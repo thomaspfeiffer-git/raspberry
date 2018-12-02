959f2ee896186847d67cbe91130a56302947dadd
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2018-12-02 15:21:20 +0100

    continue implementation

--- seti/Monitor.py
+++ seti/Monitor.py
@@ -35,6 +35,20 @@ from Shutdown import Shutdown
 from sensors.CPU import CPU
 
 
+V_Timestamp = "Timestamp"
+V_CPU_Temp = "CPU Temperature"
+V_CPU_AvgLoad = "CPU AvgLoad"
+V_CPU_Usage_Core0 = "CPU Usage Core 0"
+V_CPU_Usage_Core1 = "CPU Usage Core 1"
+V_CPU_Usage_Core2 = "CPU Usage Core 2"
+V_CPU_Usage_Core3 = "CPU Usage Core 3"
+V_CPU_Frequency = "CPU Frequency"
+V_Temp_Room = "Temperature Room"
+V_Temp_Airflow = "Temperature Airflow"
+V_Humidity = "Humidity"
+
+
+
 ###############################################################################
 ## Shutdown stuff #############################################################
 def shutdown_application ():
@@ -51,12 +65,20 @@ if __name__ == "__main__":
     cpu = CPU()
 
     while True:
-        Log(cpu.read_temperature())
-        Log(psutil.cpu_percent(percpu=True))
-        Log(psutil.cpu_freq())
-        Log(psutil.sensors_temperatures())
-        Log(os.getloadavg()[0])
-        print("")
+        cpu_usage = psutil.cpu_percent(percpu=True)
+        data = { V_Timestamp: time.strftime("%Y%m%d %H:%M:%S"),
+                 V_CPU_Temp: cpu.read_temperature(),
+                 V_CPU_AvgLoad: os.getloadavg()[0],
+                 V_CPU_Frequency: psutil.cpu_freq()[0],
+                 V_CPU_Usage_Core0: cpu_usage[0],
+                 V_CPU_Usage_Core1: cpu_usage[1],
+                 V_CPU_Usage_Core2: cpu_usage[2],
+                 V_CPU_Usage_Core3: cpu_usage[3],
+                 V_Temp_Room: "n/a",
+                 V_Temp_Airflow: "n/a",
+                 V_Humidity: "n/a" }
+
+        print(data)
         time.sleep(10)
 
 
