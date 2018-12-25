75fd3167cc499c75b553b93778e82bbc9136181a
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2018-12-25 06:57:56 +0100

    some minor polishing

--- airparticulates/Airparticulates.py
+++ airparticulates/Airparticulates.py
@@ -119,8 +119,7 @@ class ToUDP (StoreData):
         payload = "{},{}:{}".format("particulates_{}".format(self.id),self.rrd_template,self.rrd_data)
         datagram = "{},{}".format(payload,self.digest(payload)).encode('utf-8')
         try:
-            sent = self.socket.sendto(datagram, 
-                                      (self.IP_ADDRESS_SERVER, self.UDP_PORT))
+            sent = self.socket.sendto(datagram, (self.IP_ADDRESS_SERVER, self.UDP_PORT))
             Log("Sent bytes: {}; data: {}".format(sent,datagram))
         except:
             Log("Cannot send data: {0[0]} {0[1]}".format(sys.exc_info()))
