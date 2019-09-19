3cecf4a880e79d1224ea74037d0d278e9ad6c57f
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2019-09-19 19:16:54 +0200

    implementation of gpio

--- pool/Fan.py
+++ pool/Fan.py
@@ -17,9 +17,34 @@ from Logging import Log
 
 
 ###############################################################################
+# gpio ########################################################################
+class gpio (object):
+    def __init__ (self, linux_gpio):
+        # TODO: Check hardware - nanopi neo only
+        self.pin = "{}".format(linux_gpio)
+
+#        o = open("/sys/class/gpio/export", "w"); o.write(self.pin); o.close()
+#        time.sleep(0.5)
+#        o = open("/sys/class/gpio/gpio{}/direction".format(self.pin), "w"); o.write("out"); o.close()
+
+    def on (self):
+#        o = open("/sys/class/gpio/gpio{}/value".format(self.pin), "w"); o.write("1"); o.close()
+        pass
+
+    def off (self):
+#        o = open("/sys/class/gpio/gpio{}/value".format(self.pin), "w"); o.write("0"); o.close()
+        pass
+
+    def close (self):
+        self.off()
+#        o = open("/sys/class/gpio/unexport", "w"); o.write(self.pin); o.close()
+
+
+###############################################################################
 # Fan #########################################################################
-class Fan (object):
+class Fan (gpio):
     def __init__ (self, pin, delay=0):
+        super().__init__(linux_gpio=pin)
         self.pin = pin
         self.delay = delay
 
@@ -28,21 +53,25 @@ class Fan (object):
 
     def __on (self):
         time.sleep(self.delay)
+        super().on()
         Log("Fan {} switched on.".format(self.pin))
 
     def __off (self):
         time.sleep(self.delay)
+        super().off()
         Log("Fan {} switched off.".format(self.pin))
 
     def on (self):
         if self.__thread_on:
             self.__thread_on.join()
+            self.__thread_on = None
         self.__thread_on = threading.Thread(target=self.__on)
         self.__thread_on.start()
 
     def off (self):
         if self.__thread_off:
             self.__thread_off.join()
+            self.__thread_off = None
         self.__thread_off = threading.Thread(target=self.__off)
         self.__thread_off.start()
 
--- pool/Ventilation.py
+++ pool/Ventilation.py
@@ -13,9 +13,13 @@ Controls ventilation of the control room of our swimming pool.
 ### TODO
 
 
+### Packages you might need to install ###
+# sudo pip3 install Pillow
+# sudo pip3 install schedule
+
+
 import sys
 import time
-
 
 
 sys.path.append("../libs/")
@@ -23,13 +27,34 @@ from Logging import Log
 from Shutdown import Shutdown
 
 from Display import Display
+from Fan import Fan
 from Sensors import Sensors, Sensordata
 from UDP import UDP_Sender
 
 
-# Fans (AirIn, AirOut):
-# https://www.amazon.de/s?k=l%C3%BCfter+5v+60mm&__mk_de_DE=%C3%85M%C3%85%C5%BD%C3%95%C3%91&ref=nb_sb_noss
+###############################################################################
+# Control #####################################################################
+class Control (object):
+    def __init__ (self):
+        self.fan_in = Fan(65)
+        self.fan_out = Fan(66)
+        self.fan_box = Fan(67)
 
+    def ventilation_on (self):
+        pass
+        # foreach fan: on
+
+    def ventilation_off (self):
+        pass
+        # foreach fan: off
+
+    def run (self):
+        pass
+
+    def stop (self):
+        self.fan_in.close()   # TODO: immediate close necessary on shutdown
+        self.fan_out.close()
+        self.fan_box.close()
 
 
 ###############################################################################
@@ -37,6 +62,7 @@ from UDP import UDP_Sender
 def shutdown_application ():
     """cleanup stuff"""
     Log("Stopping application")
+    control.stop()
     udp_sender.stop()
     udp_sender.join()
     sensors.stop()
@@ -51,16 +77,16 @@ if __name__ == "__main__":
     shutdown_application = Shutdown(shutdown_func=shutdown_application)
     data = Sensordata()
 
-    display = Display(data)
+#    display = Display(data)
 
-    udp_sender = UDP_Sender(data)
-    udp_sender.start()
+#    udp_sender = UDP_Sender(data)
+#    udp_sender.start()
 
-    sensors = Sensors(data,update_display=display.print)
-    sensors.start()
+#    sensors = Sensors(data,update_display=display.print)
+#    sensors.start()
 
-    while True:
-        time.sleep(0.5)
+    control = Control()
+    control.run()
 
 # eof #
 
