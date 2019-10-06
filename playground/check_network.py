4cc2db129841fd1cc7c63808bcf14e0d98f412b3
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2019-10-06 20:40:29 +0200

    initial checkin

---
+++ playground/check_network.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3 -u
+# -*- coding: utf-8 -*-
+##############################################################################
+# check_network.py                                                           #
+# (c) https://github.com/thomaspfeiffer-git/raspberry, 2019                  #
+##############################################################################
+
+
+import subprocess
+import sys
+import time
+
+sys.path.append('../libs')
+
+from Logging import Log
+from Shutdown import Shutdown
+
+
+
+###############################################################################
+###############################################################################
+def ping ():
+    IP = "128.0.3.45"
+    try:
+        subprocess.check_call(["ping", "-c", "1", "-w", "1", IP], 
+                              stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+
+
+###############################################################################
+# Shutdown stuff ##############################################################
+def shutdown_application ():
+    """cleanup stuff"""
+    Log("Stopping application")
+    Log("Application stopped")
+    sys.exit(0)
+
+
+###############################################################################
+## main #######################################################################
+if __name__ == "__main__":
+    shutdown_application = Shutdown(shutdown_func=shutdown_application)
+
+    print("ping: {}".format(ping()))
+
+
+# eof #
+
