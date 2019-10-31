2788ee5f348e7101d169f3b9c0fe6f9982713973
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2019-10-31 21:27:00 +0100

    migrated to python3

--- anteroom/Button.py
+++ anteroom/Button.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -u
+#!/usr/bin/python3 -u
 # -*- coding: utf-8 -*-
 ############################################################################
 # Button.py                                                                #
@@ -10,27 +10,23 @@
 ### usage ###
 # sudo Button.py
 
-import RPi.GPIO as io
 import socket
 import sys
 import time
-import urllib
+import urllib.request
 
 sys.path.append("../libs/")
+from gpio import gpio as io
 from Logging import Log
-
-
-pin_ir = 10
-io.setmode(io.BOARD)
-io.setup(pin_ir, io.IN) 
+from Shutdown import Shutdown
 
 url = "http://localhost:5000/toggle?button=1" # TODO config file
 
-
 ###############################################################################
+# CallToggle ##################################################################
 def CallToggle ():
     try:
-        response = urllib.urlopen(url)
+        response = urllib.request.urlopen(url)
         data = response.read().decode("utf-8")
         Log("toggled!")
     except (IOError):
@@ -38,18 +34,30 @@ def CallToggle ():
     except socket.timeout:
         Log("socket.timeout: {0[0]} {0[1]}".format(sys.exc_info()))
 
+###############################################################################
+# Shutdown stuff ##############################################################
+def shutdown_application ():
+    """cleanup stuff"""
+    Log("Stopping application")
+    btn.close()
+    Log("Application stopped")
+    sys.exit(0)
 
 ###############################################################################
 ## main ######################################################################
-last = None
-while True:
-    act = io.input(pin_ir)   # TODO: debounce if necessary
-    if act != last:
-        if last == 1 and act == 0:  # falling edge
-            CallToggle()
-        last = act
+if __name__ == "__main__":
+    shutdown_application = Shutdown(shutdown_func=shutdown_application)
 
-    time.sleep(0.05)
+    btn = io(199, io.IN)   # Pin 10
+    last = None
+    while True:
+        act = int(btn.read())   # TODO: debounce if necessary
+        if act != last:
+            if last == 1 and act == 0:  # falling edge
+                CallToggle()
+            last = act
+
+        time.sleep(0.05)
 
 # eof #
 
