097125c29940495c9a3b4d8298011e8874cd47d9
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2020-01-04 19:13:17 +0100

    initial checkin

---
+++ weather/anteroom.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3 -u
+# -*- coding: utf-8 -*-
+###############################################################################
+# anteroom.py                                                                 #
+# Monitors temperature and humidity in our anteroom.                          #
+# (c) https://github.com/thomaspfeiffer-git 2020                              #
+###############################################################################
+""" Collect temperature and humidity in our anteroom (mainly with HTU21DF). """
+
+# start with:
+# nohup ./  TODO TODO
+
+import rrdtool
+import sys
+import time
+
+sys.path.append('../libs')
+
+from sensors.HTU21DF import HTU21DF
+from sensors.CPU import CPU
+
+from Logging import Log
+from Shutdown import Shutdown
+
+
+# Misc for rrdtool
+DATAFILE      = "/schild/weather/anteroom.rrd"  # TODO: check
+DS_TEMP       = "temp" 
+DS_HUMI       = "humi"
+DS_TEMPCPU    = "temp_cpu"
+
+
+###############################################################################
+# Main ########################################################################
+def main():
+    """main part"""
+    htu21df = HTU21DF()
+    cpu = CPU()
+
+    rrd_template = DS_TEMP       + ":" + \
+                   DS_HUMI       + ":" + \
+                   DS_TEMPCPU
+
+    while True:
+        temperature = htu21df.read_temperature()
+        humidity    = htu21df.read_humidity()
+        cpu_temp    = cpu.read_temperature()
+     
+        rrd_data = "N:" + \
+                   ":".join(f"{d:.2f}" for d in [temperature, \
+                                                 humidity,   \
+                                                 cpu_temp])
+                                                          
+        Log(rrd_template)
+        Log(rrd_data)
+        # rrdtool.update(DATAFILE, "--template", rrd_template, rrd_data) 
+   
+        time.sleep(50)
+
+
+###############################################################################
+# Exit ########################################################################
+def shutdown_application ():
+    """cleanup stuff"""
+    sys.exit(0)
+
+
+###############################################################################
+###############################################################################
+if __name__ == '__main__':
+    shutdown = Shutdown(shutdown_func=shutdown_application)
+
+    main()
+
+# eof #
+
