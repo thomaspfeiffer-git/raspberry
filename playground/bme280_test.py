532c248860b96bcc0760e6b1a97586b54056999e
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2020-01-04 18:53:55 +0100

    some polishing

--- playground/bme280_test.py
+++ playground/bme280_test.py
@@ -6,7 +6,6 @@
 ###############################################################################
 """Tests sensor BME280 (air pressure, humidity, temperature."""
 
-
 import sys
 import time
 
@@ -14,16 +13,16 @@ sys.path.append('../libs')
 
 from sensors.BME280 import BME280    # air pressure, temperature, humidity
 
-bme280  = BME280()
+bme280 = BME280()
 
 while True:
-     bme280_pressure     = bme280.read_pressure()/100.0
-     bme280_temperature  = bme280.read_temperature()
-     bme280_humidity     = bme280.read_humidity()
+     bme280_pressure    = bme280.read_pressure()/100.0
+     bme280_temperature = bme280.read_temperature()
+     bme280_humidity    = bme280.read_humidity()
 
-     print("BME280     | Druck | {:>8.2f} | hPa     |".format(bme280_pressure))
-     print("BME280     | Humi  | {:>8.2f} | % rF    |".format(bme280_humidity))
-     print("BME280     | Temp  | {:>8.2f} | C       |".format(bme280_temperature))
+     print(f"BME280 | Press | {bme280_pressure:>8.2f} | hPa  |")
+     print(f"BME280 | Humi  | {bme280_humidity:>8.2f} | % rF |")
+     print(f"BME280 | Temp  | {bme280_temperature:>8.2f} | °C   |")
 
      time.sleep(1)
 
