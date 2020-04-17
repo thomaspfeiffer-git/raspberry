4fcd942b0671b5b54ed6d395f069cbdcc4a369c2
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2020-04-17 21:24:33 +0200

    --

---
+++ airparticulates/airparticulates_udp.py
@@ -0,0 +1,184 @@
+20200417 19:44:22: Received: particulates_2,2_pm25:2_pm10:N:4.3:7.8,Udxw3yAGa51CXlhVPAlcH78KxdWQu+fvGWxsOfKC4LM=
+20200417 19:44:47: Received: particulates_1,1_pm25:1_pm10:N:5.1:7.2,0GGWE+pk7xdRdseB79+s1tvEBTTcuRE7I8r8S76W+aA=
+20200417 19:44:57: rrd updated: N:5.1:7.2:4.3:7.8
+20200417 19:45:47: rrd updated: N:5.1:7.2:4.3:7.8
+20200417 19:46:37: rrd updated: N:5.1:7.2:4.3:7.8
+20200417 19:47:27: rrd updated: N:5.1:7.2:4.3:7.8
+20200417 19:47:32: Received: particulates_2,2_pm25:2_pm10:N:4.3:7.8,Udxw3yAGa51CXlhVPAlcH78KxdWQu+fvGWxsOfKC4LM=
+20200417 19:47:57: Received: particulates_1,1_pm25:1_pm10:N:5.1:7.2,0GGWE+pk7xdRdseB79+s1tvEBTTcuRE7I8r8S76W+aA=
+20200417 19:48:17: rrd updated: N:5.1:7.2:4.3:7.8
+20200417 19:49:07: rrd updated: N:5.1:7.2:4.3:7.8
+20200417 19:49:58: rrd updated: N:5.1:7.2:4.3:7.8
+20200417 19:50:43: Received: particulates_2,2_pm25:2_pm10:N:5.0:8.0,6rQ4orGsaTJP+5l+TIUEr+/hKnnfqpmlh0Mt/7scnMg=
+20200417 19:50:48: rrd updated: N:5.1:7.2:5.0:8.0
+20200417 19:51:08: Received: particulates_1,1_pm25:1_pm10:N:5.1:7.2,0GGWE+pk7xdRdseB79+s1tvEBTTcuRE7I8r8S76W+aA=
+20200417 19:51:38: rrd updated: N:5.1:7.2:5.0:8.0
+20200417 19:52:28: rrd updated: N:5.1:7.2:5.0:8.0
+20200417 19:53:18: rrd updated: N:5.1:7.2:5.0:8.0
+20200417 19:53:53: Received: particulates_2,2_pm25:2_pm10:N:5.0:8.0,6rQ4orGsaTJP+5l+TIUEr+/hKnnfqpmlh0Mt/7scnMg=
+20200417 19:54:08: Received: particulates_1,1_pm25:1_pm10:N:5.6:7.6,kSqqX+ep9b4/9/gfSFeWXyC0NmPqWcFi1+o8H/PclXQ=
+20200417 19:54:08: rrd updated: N:5.6:7.6:5.0:8.0
+20200417 19:54:58: rrd updated: N:5.6:7.6:5.0:8.0
+20200417 19:55:48: rrd updated: N:5.6:7.6:5.0:8.0
+20200417 19:56:38: rrd updated: N:5.6:7.6:5.0:8.0
+20200417 19:57:03: Received: particulates_2,2_pm25:2_pm10:N:5.0:8.0,6rQ4orGsaTJP+5l+TIUEr+/hKnnfqpmlh0Mt/7scnMg=
+20200417 19:57:28: rrd updated: N:5.6:7.6:5.0:8.0
+20200417 19:57:49: Received: particulates_1,1_pm25:1_pm10:N:5.7:10.8,gPBlFyYljoMk1WY5KNnyGsXHK9bDPdi63qpy2pjy41g=
+20200417 19:58:19: rrd updated: N:5.7:10.8:5.0:8.0
+20200417 19:59:09: rrd updated: N:5.7:10.8:5.0:8.0
+20200417 19:59:59: rrd updated: N:5.7:10.8:5.0:8.0
+20200417 20:00:14: Received: particulates_2,2_pm25:2_pm10:N:5.0:10.1,SqOnDlRbhJiaokZyN44i6RFFUTnlXu1aAQ+0TzmR12Y=
+20200417 20:00:49: rrd updated: N:5.7:10.8:5.0:10.1
+20200417 20:00:59: Received: particulates_1,1_pm25:1_pm10:N:5.7:10.8,gPBlFyYljoMk1WY5KNnyGsXHK9bDPdi63qpy2pjy41g=
+20200417 20:01:39: rrd updated: N:5.7:10.8:5.0:10.1
+20200417 20:02:29: rrd updated: N:5.7:10.8:5.0:10.1
+20200417 20:03:19: rrd updated: N:5.7:10.8:5.0:10.1
+20200417 20:03:24: Received: particulates_2,2_pm25:2_pm10:N:5.0:10.1,SqOnDlRbhJiaokZyN44i6RFFUTnlXu1aAQ+0TzmR12Y=
+20200417 20:04:09: Received: particulates_1,1_pm25:1_pm10:N:5.7:10.8,gPBlFyYljoMk1WY5KNnyGsXHK9bDPdi63qpy2pjy41g=
+20200417 20:04:09: rrd updated: N:5.7:10.8:5.0:10.1
+20200417 20:04:59: rrd updated: N:5.7:10.8:5.0:10.1
+20200417 20:05:50: rrd updated: N:5.7:10.8:5.0:10.1
+20200417 20:06:35: Received: particulates_2,2_pm25:2_pm10:N:5.0:10.1,SqOnDlRbhJiaokZyN44i6RFFUTnlXu1aAQ+0TzmR12Y=
+20200417 20:06:40: rrd updated: N:5.7:10.8:5.0:10.1
+20200417 20:07:20: Received: particulates_1,1_pm25:1_pm10:N:6.0:7.8,AFv1GwwBJo3kP4we99kPLmj61BgnYi162Oh/Yhvw9MY=
+20200417 20:07:30: rrd updated: N:6.0:7.8:5.0:10.1
+20200417 20:08:20: rrd updated: N:6.0:7.8:5.0:10.1
+20200417 20:09:10: rrd updated: N:6.0:7.8:5.0:10.1
+20200417 20:09:45: Received: particulates_2,2_pm25:2_pm10:N:5.0:10.1,SqOnDlRbhJiaokZyN44i6RFFUTnlXu1aAQ+0TzmR12Y=
+20200417 20:10:00: rrd updated: N:6.0:7.8:5.0:10.1
+20200417 20:10:30: Received: particulates_1,1_pm25:1_pm10:N:6.0:7.8,AFv1GwwBJo3kP4we99kPLmj61BgnYi162Oh/Yhvw9MY=
+20200417 20:10:50: rrd updated: N:6.0:7.8:5.0:10.1
+20200417 20:11:40: rrd updated: N:6.0:7.8:5.0:10.1
+20200417 20:12:30: rrd updated: N:6.0:7.8:5.0:10.1
+20200417 20:12:55: Received: particulates_2,2_pm25:2_pm10:N:6.1:9.9,MSWDv0TwQcyl/ErNTsrn5OhttBlDtNAcBRiDvAJTuxc=
+20200417 20:13:20: rrd updated: N:6.0:7.8:6.1:9.9
+20200417 20:13:41: Received: particulates_1,1_pm25:1_pm10:N:6.0:7.8,AFv1GwwBJo3kP4we99kPLmj61BgnYi162Oh/Yhvw9MY=
+20200417 20:14:11: rrd updated: N:6.0:7.8:6.1:9.9
+20200417 20:15:01: rrd updated: N:6.0:7.8:6.1:9.9
+20200417 20:15:51: rrd updated: N:6.0:7.8:6.1:9.9
+20200417 20:16:06: Received: particulates_2,2_pm25:2_pm10:N:6.1:9.9,MSWDv0TwQcyl/ErNTsrn5OhttBlDtNAcBRiDvAJTuxc=
+20200417 20:16:41: rrd updated: N:6.0:7.8:6.1:9.9
+20200417 20:16:51: Received: particulates_1,1_pm25:1_pm10:N:5.7:8.9,2kt3kqANjeFZFxE0i+OEGMkOk5FFVF4Yt+wX8cGIZVE=
+20200417 20:17:31: rrd updated: N:5.7:8.9:6.1:9.9
+20200417 20:18:21: rrd updated: N:5.7:8.9:6.1:9.9
+20200417 20:19:11: rrd updated: N:5.7:8.9:6.1:9.9
+20200417 20:19:16: Received: particulates_2,2_pm25:2_pm10:N:6.1:9.9,MSWDv0TwQcyl/ErNTsrn5OhttBlDtNAcBRiDvAJTuxc=
+20200417 20:20:01: Received: particulates_1,1_pm25:1_pm10:N:5.7:8.9,2kt3kqANjeFZFxE0i+OEGMkOk5FFVF4Yt+wX8cGIZVE=
+20200417 20:20:01: rrd updated: N:5.7:8.9:6.1:9.9
+20200417 20:20:51: rrd updated: N:5.7:8.9:6.1:9.9
+20200417 20:21:42: rrd updated: N:5.7:8.9:6.1:9.9
+20200417 20:22:27: Received: particulates_2,2_pm25:2_pm10:N:4.9:10.3,Lue8IzVViUBDYVFxXC7TrL0dndNYQ0ReTs/3e1IzHUU=
+20200417 20:22:32: rrd updated: N:5.7:8.9:4.9:10.3
+20200417 20:23:12: Received: particulates_1,1_pm25:1_pm10:N:5.7:8.9,2kt3kqANjeFZFxE0i+OEGMkOk5FFVF4Yt+wX8cGIZVE=
+20200417 20:23:22: rrd updated: N:5.7:8.9:4.9:10.3
+20200417 20:24:12: rrd updated: N:5.7:8.9:4.9:10.3
+20200417 20:25:02: rrd updated: N:5.7:8.9:4.9:10.3
+20200417 20:25:42: Received: particulates_2,2_pm25:2_pm10:N:4.9:10.3,Lue8IzVViUBDYVFxXC7TrL0dndNYQ0ReTs/3e1IzHUU=
+20200417 20:25:52: rrd updated: N:5.7:8.9:4.9:10.3
+20200417 20:26:22: Received: particulates_1,1_pm25:1_pm10:N:5.7:8.9,2kt3kqANjeFZFxE0i+OEGMkOk5FFVF4Yt+wX8cGIZVE=
+20200417 20:26:42: rrd updated: N:5.7:8.9:4.9:10.3
+20200417 20:27:32: rrd updated: N:5.7:8.9:4.9:10.3
+20200417 20:28:22: rrd updated: N:5.7:8.9:4.9:10.3
+20200417 20:28:52: Received: particulates_2,2_pm25:2_pm10:N:4.9:10.3,Lue8IzVViUBDYVFxXC7TrL0dndNYQ0ReTs/3e1IzHUU=
+20200417 20:29:12: rrd updated: N:5.7:8.9:4.9:10.3
+20200417 20:29:32: Received: particulates_1,1_pm25:1_pm10:N:5.4:8.7,u8Siv9XeuJTfJW53PaqiD0emt8FhOZEdwZeQ8lPDcQU=
+20200417 20:30:03: rrd updated: N:5.4:8.7:4.9:10.3
+20200417 20:30:53: rrd updated: N:5.4:8.7:4.9:10.3
+20200417 20:31:43: rrd updated: N:5.4:8.7:4.9:10.3
+20200417 20:32:03: Received: particulates_2,2_pm25:2_pm10:N:4.1:4.9,JYp8EvnfAwSJCUhS/uyi4UMK+NntZm8ovoV5g1urFIU=
+20200417 20:32:33: rrd updated: N:5.4:8.7:4.1:4.9
+20200417 20:32:43: Received: particulates_1,1_pm25:1_pm10:N:5.4:8.7,u8Siv9XeuJTfJW53PaqiD0emt8FhOZEdwZeQ8lPDcQU=
+20200417 20:33:23: rrd updated: N:5.4:8.7:4.1:4.9
+20200417 20:34:13: rrd updated: N:5.4:8.7:4.1:4.9
+20200417 20:35:03: rrd updated: N:5.4:8.7:4.1:4.9
+20200417 20:35:13: Received: particulates_2,2_pm25:2_pm10:N:4.1:4.9,JYp8EvnfAwSJCUhS/uyi4UMK+NntZm8ovoV5g1urFIU=
+20200417 20:35:53: Received: particulates_1,1_pm25:1_pm10:N:5.4:8.7,u8Siv9XeuJTfJW53PaqiD0emt8FhOZEdwZeQ8lPDcQU=
+20200417 20:35:53: rrd updated: N:5.4:8.7:4.1:4.9
+20200417 20:36:43: rrd updated: N:5.4:8.7:4.1:4.9
+20200417 20:37:33: rrd updated: N:5.4:8.7:4.1:4.9
+20200417 20:38:24: rrd updated: N:5.4:8.7:4.1:4.9
+20200417 20:38:24: Received: particulates_2,2_pm25:2_pm10:N:4.1:4.9,JYp8EvnfAwSJCUhS/uyi4UMK+NntZm8ovoV5g1urFIU=
+20200417 20:39:04: Received: particulates_1,1_pm25:1_pm10:N:5.7:11.2,S3qC3FyqtVKuMqOhBjPet5Z9G4LFgROPXaX0tohdw+I=
+20200417 20:39:14: rrd updated: N:5.7:11.2:4.1:4.9
+20200417 20:40:04: rrd updated: N:5.7:11.2:4.1:4.9
+20200417 20:40:54: rrd updated: N:5.7:11.2:4.1:4.9
+20200417 20:41:34: Received: particulates_2,2_pm25:2_pm10:N:11.2:13.6,vN5+wpjrccF7DhjwgONQN81+YliigVi+ZDwn4I7lPw4=
+20200417 20:41:44: rrd updated: N:5.7:11.2:11.2:13.6
+20200417 20:42:14: Received: particulates_1,1_pm25:1_pm10:N:5.7:11.2,S3qC3FyqtVKuMqOhBjPet5Z9G4LFgROPXaX0tohdw+I=
+20200417 20:42:34: rrd updated: N:5.7:11.2:11.2:13.6
+20200417 20:43:24: rrd updated: N:5.7:11.2:11.2:13.6
+20200417 20:44:14: rrd updated: N:5.7:11.2:11.2:13.6
+20200417 20:44:44: Received: particulates_2,2_pm25:2_pm10:N:11.2:13.6,vN5+wpjrccF7DhjwgONQN81+YliigVi+ZDwn4I7lPw4=
+20200417 20:45:04: rrd updated: N:5.7:11.2:11.2:13.6
+20200417 20:45:24: Received: particulates_1,1_pm25:1_pm10:N:5.7:11.2,S3qC3FyqtVKuMqOhBjPet5Z9G4LFgROPXaX0tohdw+I=
+20200417 20:45:55: rrd updated: N:5.7:11.2:11.2:13.6
+20200417 20:46:45: rrd updated: N:5.7:11.2:11.2:13.6
+20200417 20:47:35: rrd updated: N:5.7:11.2:11.2:13.6
+20200417 20:47:55: Received: particulates_2,2_pm25:2_pm10:N:11.2:13.6,vN5+wpjrccF7DhjwgONQN81+YliigVi+ZDwn4I7lPw4=
+20200417 20:48:25: rrd updated: N:5.7:11.2:11.2:13.6
+20200417 20:48:35: Received: particulates_1,1_pm25:1_pm10:N:6.0:10.0,Ejmvp/XxQZ0S4l51Nga3IgeK9gtbnwWe8LhBe7wGsO8=
+20200417 20:49:15: rrd updated: N:6.0:10.0:11.2:13.6
+20200417 20:50:05: rrd updated: N:6.0:10.0:11.2:13.6
+20200417 20:50:55: rrd updated: N:6.0:10.0:11.2:13.6
+20200417 20:51:05: Received: particulates_2,2_pm25:2_pm10:N:11.2:13.6,vN5+wpjrccF7DhjwgONQN81+YliigVi+ZDwn4I7lPw4=
+20200417 20:51:45: Received: particulates_1,1_pm25:1_pm10:N:6.0:10.0,Ejmvp/XxQZ0S4l51Nga3IgeK9gtbnwWe8LhBe7wGsO8=
+20200417 20:51:45: rrd updated: N:6.0:10.0:11.2:13.6
+20200417 20:52:35: rrd updated: N:6.0:10.0:11.2:13.6
+20200417 20:53:25: rrd updated: N:6.0:10.0:11.2:13.6
+20200417 20:54:16: rrd updated: N:6.0:10.0:11.2:13.6
+20200417 20:54:16: Received: particulates_2,2_pm25:2_pm10:N:8.4:10.3,xDNFGcYy7sFb5eM/pB/2pJp2yOpCkmtxGAZyZs+PSmM=
+20200417 20:54:56: Received: particulates_1,1_pm25:1_pm10:N:6.0:10.0,Ejmvp/XxQZ0S4l51Nga3IgeK9gtbnwWe8LhBe7wGsO8=
+20200417 20:55:06: rrd updated: N:6.0:10.0:8.4:10.3
+20200417 20:55:56: rrd updated: N:6.0:10.0:8.4:10.3
+20200417 20:56:46: rrd updated: N:6.0:10.0:8.4:10.3
+20200417 20:57:26: Received: particulates_2,2_pm25:2_pm10:N:8.4:10.3,xDNFGcYy7sFb5eM/pB/2pJp2yOpCkmtxGAZyZs+PSmM=
+20200417 20:57:36: rrd updated: N:6.0:10.0:8.4:10.3
+20200417 20:58:06: Received: particulates_1,1_pm25:1_pm10:N:5.4:8.6,dZOV431RVkJ2KJXm1B2CFbQRfcCNPbTc4WyLjOm25D4=
+20200417 20:58:26: rrd updated: N:5.4:8.6:8.4:10.3
+20200417 20:59:16: rrd updated: N:5.4:8.6:8.4:10.3
+20200417 21:00:06: rrd updated: N:5.4:8.6:8.4:10.3
+20200417 21:00:36: Received: particulates_2,2_pm25:2_pm10:N:8.4:10.3,xDNFGcYy7sFb5eM/pB/2pJp2yOpCkmtxGAZyZs+PSmM=
+20200417 21:00:56: rrd updated: N:5.4:8.6:8.4:10.3
+20200417 21:01:16: Received: particulates_1,1_pm25:1_pm10:N:5.4:8.6,dZOV431RVkJ2KJXm1B2CFbQRfcCNPbTc4WyLjOm25D4=
+20200417 21:01:47: rrd updated: N:5.4:8.6:8.4:10.3
+20200417 21:02:37: rrd updated: N:5.4:8.6:8.4:10.3
+20200417 21:03:27: rrd updated: N:5.4:8.6:8.4:10.3
+20200417 21:03:47: Received: particulates_2,2_pm25:2_pm10:N:4.6:6.5,uQZNjaTNUCFKUwtUFGgE24mnU/5yq87pFRZFQ+dIjqk=
+20200417 21:04:17: rrd updated: N:5.4:8.6:4.6:6.5
+20200417 21:04:27: Received: particulates_1,1_pm25:1_pm10:N:5.4:8.6,dZOV431RVkJ2KJXm1B2CFbQRfcCNPbTc4WyLjOm25D4=
+20200417 21:05:07: rrd updated: N:5.4:8.6:4.6:6.5
+20200417 21:05:57: rrd updated: N:5.4:8.6:4.6:6.5
+20200417 21:06:47: rrd updated: N:5.4:8.6:4.6:6.5
+20200417 21:06:57: Received: particulates_2,2_pm25:2_pm10:N:4.6:6.5,uQZNjaTNUCFKUwtUFGgE24mnU/5yq87pFRZFQ+dIjqk=
+20200417 21:07:37: Received: particulates_1,1_pm25:1_pm10:N:5.4:8.6,dZOV431RVkJ2KJXm1B2CFbQRfcCNPbTc4WyLjOm25D4=
+20200417 21:07:37: rrd updated: N:5.4:8.6:4.6:6.5
+20200417 21:08:27: rrd updated: N:5.4:8.6:4.6:6.5
+20200417 21:09:17: rrd updated: N:5.4:8.6:4.6:6.5
+20200417 21:10:08: rrd updated: N:5.4:8.6:4.6:6.5
+20200417 21:10:08: Received: particulates_2,2_pm25:2_pm10:N:4.6:6.5,uQZNjaTNUCFKUwtUFGgE24mnU/5yq87pFRZFQ+dIjqk=
+20200417 21:10:48: Received: particulates_1,1_pm25:1_pm10:N:6.2:10.0,EfAE7Xi3Umgp1/xli7OSStFxCkXDuypPHreHS3iiIIE=
+20200417 21:10:58: rrd updated: N:6.2:10.0:4.6:6.5
+20200417 21:11:48: rrd updated: N:6.2:10.0:4.6:6.5
+20200417 21:12:38: rrd updated: N:6.2:10.0:4.6:6.5
+20200417 21:13:18: Received: particulates_2,2_pm25:2_pm10:N:5.8:9.1,l+NxX/X6c+sr3E52u4CA/BYCIOMqiU7TeYZCIk5v9DU=
+20200417 21:13:28: rrd updated: N:6.2:10.0:5.8:9.1
+20200417 21:13:58: Received: particulates_1,1_pm25:1_pm10:N:6.2:10.0,EfAE7Xi3Umgp1/xli7OSStFxCkXDuypPHreHS3iiIIE=
+20200417 21:14:18: rrd updated: N:6.2:10.0:5.8:9.1
+20200417 21:15:08: rrd updated: N:6.2:10.0:5.8:9.1
+20200417 21:15:58: rrd updated: N:6.2:10.0:5.8:9.1
+20200417 21:16:28: Received: particulates_2,2_pm25:2_pm10:N:5.8:9.1,l+NxX/X6c+sr3E52u4CA/BYCIOMqiU7TeYZCIk5v9DU=
+20200417 21:16:48: rrd updated: N:6.2:10.0:5.8:9.1
+20200417 21:17:08: Received: particulates_1,1_pm25:1_pm10:N:6.2:10.0,EfAE7Xi3Umgp1/xli7OSStFxCkXDuypPHreHS3iiIIE=
+20200417 21:17:39: rrd updated: N:6.2:10.0:5.8:9.1
+20200417 21:18:29: rrd updated: N:6.2:10.0:5.8:9.1
+20200417 21:19:19: rrd updated: N:6.2:10.0:5.8:9.1
+20200417 21:19:39: Received: particulates_2,2_pm25:2_pm10:N:5.8:9.1,l+NxX/X6c+sr3E52u4CA/BYCIOMqiU7TeYZCIk5v9DU=
+20200417 21:20:09: rrd updated: N:6.2:10.0:5.8:9.1
+20200417 21:20:19: Received: particulates_1,1_pm25:1_pm10:N:5.8:8.7,Xc0GZxt89j/+c0V/jQT7puJcodSI4UDvkK9CC6aezKw=
+20200417 21:20:59: rrd updated: N:5.8:8.7:5.8:9.1
+20200417 21:21:49: rrd updated: N:5.8:8.7:5.8:9.1
+20200417 21:22:39: rrd updated: N:5.8:8.7:5.8:9.1
+20200417 21:22:49: Received: particulates_2,2_pm25:2_pm10:N:5.8:9.1,l+NxX/X6c+sr3E52u4CA/BYCIOMqiU7TeYZCIk5v9DU=
+20200417 21:23:29: rrd updated: N:5.8:8.7:5.8:9.1
+20200417 21:23:29: Received: particulates_1,1_pm25:1_pm10:N:5.8:8.7,Xc0GZxt89j/+c0V/jQT7puJcodSI4UDvkK9CC6aezKw=
+20200417 21:24:19: rrd updated: N:5.8:8.7:5.8:9.1
