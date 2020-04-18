c02a38f3824fd47827478e124de1a4667cabcb82
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2020-04-18 08:34:37 +0200

    refactoring and polishing

--- weather/Serverroom.py
+++ weather/Serverroom.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3 -u
 # -*- coding: utf-8 -*-
 ###############################################################################
-# serverroom.py                                                               #
+# Serverroom.py                                                               #
 # Monitors temperature and humidity in our server room                        #
 # (c) https://github.com/thomaspfeiffer-git 2020                              #
 ###############################################################################
 """ Collect temperature and humidity in our server room (mainly with HTU21DF). """
 
-# start with:
-# nohup ./serverroom.py 2>&1 >serverroom.log &
+"""
+##### start with:
+### read data from sensor and send to udp server
+nohup ./Serverroom.py --sensor 2>&1 >serverroom.log &
 
-import rrdtool  # TODO remove after migration
+### receive data via udp and store in rrd database
+nohup ./Serverroom.py --receiver 2>&1 >serverroom_udp.log &
+"""
+
+import argparse
+import os
+import rrdtool
 import sys
 import time
 
@@ -21,27 +29,24 @@ from sensors.CPU import CPU
 
 from Logging import Log
 from Shutdown import Shutdown
-from Serverroom_UDP import UDP_Sender
+import UDP
 
 
 # Misc for rrdtool
-DATAFILE      = "/schild/weather/serverroom.rrd"   # TODO remove after migration
+CREDENTIALS = os.path.expanduser("~/credentials/serverroom.cred")
+RRDFILE = os.path.expanduser("~/rrd/databases/serverroom.rrd")
 DS_TEMP       = "temp"
 DS_HUMI       = "humi"
 DS_TEMPCPU    = "tempcpu"
 
 
 ###############################################################################
-# Main ########################################################################
-def main():
-    """main part"""
+# Sensor ######################################################################
+def Sensor ():
+    """reads data from sensor"""
     htu21df = HTU21DF()
     cpu = CPU()
-    udp = UDP_Sender()
-
-    rrd_template = DS_TEMP + ":" + \
-                   DS_HUMI + ":" + \
-                   DS_TEMPCPU
+    udp = UDP.Sender(CREDENTIALS)
 
     while True:
         temperature = htu21df.read_temperature()
@@ -52,14 +57,25 @@ def main():
                    ":".join(f"{d:.2f}" for d in [temperature, \
                                                  humidity,   \
                                                  cpu_temp])
-        # Log(rrd_data)
         udp.send(rrd_data)
-        try:   # TODO remove after migration
-            rrdtool.update(DATAFILE, "--template", rrd_template, rrd_data)
+        time.sleep(50)
+
+
+###############################################################################
+# Receiver ####################################################################
+def Receiver ():
+    rrd_template = DS_TEMP + ":" + \
+                   DS_HUMI + ":" + \
+                   DS_TEMPCPU
+    udp = UDP.Receiver(CREDENTIALS)
+
+    while True:
+        data = udp.receive()
+        Log(f"RRD Data received: {data}")
+        try:
+            rrdtool.update(RRDFILE, "--template", rrd_template, data)
         except rrdtool.OperationalError:
             Log("Cannot update rrd database: {0[0]} {0[1]}".format(sys.exc_info()))
-
-        time.sleep(50)
 
 
 ###############################################################################
@@ -70,11 +86,20 @@ def shutdown_application ():
 
 
 ###############################################################################
-###############################################################################
+# main ########################################################################
 if __name__ == '__main__':
     shutdown = Shutdown(shutdown_func=shutdown_application)
 
-    main()
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--sensor", help="read data from sensor and send to udp server", action="store_true")
+    group.add_argument("--receiver", help="receive data via udp and store in rrd database", action="store_true")
+    args = parser.parse_args()
+
+    if args.sensor:
+        Sensor()
+    if args.receiver:
+        Receiver()
 
 # eof #
 
