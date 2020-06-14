b275f26e763ce8c5d91184125694d90bab04b8fe
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2020-06-14 20:45:43 +0200

    initial checkin

--- pool/Circulation.py
+++ pool/Circulation.py
@@ -1,1 +1,48 @@
-...
+#!/usr/bin/python3 -u
+# -*- coding: utf-8 -*-
+###############################################################################
+# Circulation.py                                                              #
+# (c) https://github.com/thomaspfeiffer-git/raspberry, 2020                   #
+###############################################################################
+
+"""
+"""
+
+### Usage ###
+# nohup ./Circulation.py 2>&1 > circulation.log &
+
+
+import sys
+
+
+sys.path.append("../libs/")
+from Logging import Log
+from Shutdown import Shutdown
+
+
+
+
+
+###############################################################################
+# Shutdown stuff ##############################################################
+def shutdown_application ():
+    """cleanup stuff"""
+    Log("Stopping application")
+
+
+    Log("Application stopped")
+    sys.exit(0)
+
+
+###############################################################################
+## main #######################################################################
+if __name__ == "__main__":
+    shutdown_application = Shutdown(shutdown_func=shutdown_application)
+
+
+
+
+
+
+# eof #
+
