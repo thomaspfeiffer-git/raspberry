e0359a3c3db3be0439a9efff5fb2da67c1530d57
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2020-06-15 12:23:08 +0200

    some prototyping

--- pool/Circulation.py
+++ pool/Circulation.py
@@ -12,6 +12,7 @@
 # nohup ./Circulation.py 2>&1 > circulation.log &
 
 
+from datetime import datetime, timedelta
 from flask import Flask, render_template
 import sys
 
@@ -25,10 +26,52 @@ app = Flask(__name__)
 
 
 ###############################################################################
+# IO ##########################################################################
+class IO (object):
+    pass
+
+
+###############################################################################
+# Timer #######################################################################
+class Timer (object):
+    def __init__ (self):
+        self.on_until = datetime(1970, 1, 1)
+
+    def on (self, time_=30):
+        self.on_until = datetime.now() + timedelta(seconds=time_*60)
+
+    def off (self):
+        self.on_until = datetime(1970, 1, 1)
+
+    @property
+    def status (self):
+        if datetime.now() < self.on_until:
+            return self.on_until
+        else:
+            return 0
+
+
+###############################################################################
+# Control #####################################################################
+class Control (object):
+    def __init__ (self):
+        self._running = False
+
+    def run (self):
+        # TODO
+        self._running = True
+
+        while self._running:
+            time.sleep(0.1)
+
+    def stop (self):
+        self._running = False
+
+
+###############################################################################
 # Flask stuff #################################################################
 @app.route('/')
-def Control ():
-    from datetime import datetime
+def HTTP_Control ():
     now = datetime.now()
     return render_template('control_circulation.html', now=now)
 
@@ -49,8 +92,16 @@ def shutdown_application ():
 if __name__ == "__main__":
     shutdown_application = Shutdown(shutdown_func=shutdown_application)
 
+    timer = Timer()
+
+    Log(f"Timerstatus: {timer.status}")
+    timer.on()
+    Log(f"Timerstatus: {timer.status}")
+    timer.off()
+    Log(f"Timerstatus: {timer.status}")
+
     # app.run(host="0.0.0.0", port=80)
-    app.run(host="0.0.0.0")
+#     app.run(host="0.0.0.0")
 
 # eof #
 
