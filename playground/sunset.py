b4d727114e80da3c6bd967f1fda8a173ba449399
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2020-11-27 21:43:26 +0100

    initial checkin

---
+++ playground/sunset.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3 -u
+# -*- coding: utf-8 -*-
+###############################################################################
+#                                                                             #
+# (c) https://github.com/thomaspfeiffer-git/raspberry, 2017                   #
+###############################################################################
+
+
+from urllib.error import HTTPError, URLError
+from urllib.request import urlopen
+
+
+
+
+
+
+
+
+
+
+# eof #
+
