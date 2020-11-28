db88d68b4b6a88a2e9df99799d0fa82371457a51
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2020-11-28 08:12:24 +0100

    continued implementation

--- playground/sunset.py
+++ playground/sunset.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 ###############################################################################
 #                                                                             #
-# (c) https://github.com/thomaspfeiffer-git/raspberry, 2017                   #
+# (c) https://github.com/thomaspfeiffer-git/raspberry, 2020                   #
 ###############################################################################
 
 from attrdict import AttrDict
@@ -15,23 +15,19 @@ url = "https://api.sunrise-sunset.org/json?lat=48.2&lng=15.63333&formatted=0"
 
 with urlopen(url) as response:
     data = AttrDict(json.loads(response.read().decode("utf-8")))
+# TODO: exception handling
 
 
-sunrise = datetime.strptime(data['results']['sunrise'], '%Y-%m-%dT%H:%M:%S%z')
-sunset  = datetime.strptime(data['results']['sunset'],  '%Y-%m-%dT%H:%M:%S%z')
+local_tz = datetime.now(timezone(timedelta(0))).astimezone().tzinfo
+sunrise = datetime.strptime(data['results']['sunrise'], '%Y-%m-%dT%H:%M:%S%z').astimezone(local_tz)
+sunset  = datetime.strptime(data['results']['sunset'],  '%Y-%m-%dT%H:%M:%S%z').astimezone(local_tz)
 
-print(f"sunrise: {sunrise}")
-print(f"sunset:  {sunset}")
-
-
-LOCAL_TIMEZONE = datetime.now(timezone(timedelta(0))).astimezone().tzinfo
-print(LOCAL_TIMEZONE)
-
-sunrise = sunrise.astimezone(LOCAL_TIMEZONE)
-sunset  = sunset.astimezone(LOCAL_TIMEZONE)
+delta = timedelta(hours=1)
+on = sunrise-delta <= datetime.now(tz=local_tz) <= sunset+delta
 
 print(f"sunrise: {sunrise}")
 print(f"sunset:  {sunset}")
+print(f"on: {on}")
 
 # eof #
 
