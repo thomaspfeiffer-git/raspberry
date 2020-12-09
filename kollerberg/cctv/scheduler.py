0b0566292bb4a1c393a41af11626dc08de82d567
Thomas Pfeiffer <thomas.pfeiffer@smtp.at>
2020-12-09 16:28:31 +0100

    added default for return value

--- kollerberg/cctv/scheduler.py
+++ kollerberg/cctv/scheduler.py
@@ -12,12 +12,25 @@ import sys
 from urllib.error import HTTPError, URLError
 from urllib.request import urlopen
 
+sys.path.append("../../libs/")
+from Logging import Log
+
 url = "https://api.sunrise-sunset.org/json?lat=48.2&lng=15.63333&formatted=0"
 
-with urlopen(url) as response:
-    data = AttrDict(json.loads(response.read().decode("utf-8")))
-# TODO: exception handling
+if len(sys.argv) == 2:
+    defaultvalue = int(str(sys.argv)[1])
+else:
+    defaultvalue = 1
 
+try:
+    with urlopen(url) as response:
+        data = AttrDict(json.loads(response.read().decode("utf-8")))
+except (HTTPError, URLError, ConnectionResetError):
+    Log("Error: {0[0]} {0[1]}".format(sys.exc_info()))
+    sys.exit(defaultvalue)
+except socket.timeout:
+    Log("socket.timeout: {0[0]} {0[1]}".format(sys.exc_info()))
+    sys.exit(defaultvalue)
 
 local_tz = datetime.now(timezone(timedelta(0))).astimezone().tzinfo
 sunrise = datetime.strptime(data['results']['sunrise'], '%Y-%m-%dT%H:%M:%S%z').astimezone(local_tz)
