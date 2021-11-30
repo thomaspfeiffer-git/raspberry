7f13402c60dc0add2d307d91bb9e0ddcbee6ee05
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2021-11-30 23:50:13 +0100

    continued implementation

--- buzzer/Buzzer.py
+++ buzzer/Buzzer.py
@@ -25,6 +25,7 @@ from tkinter.font import Font
 
 import os
 import sys
+import threading
 import time
 
 sys.path.append('../libs')
@@ -32,18 +33,57 @@ from Shutdown import Shutdown
 from Logging import Log
 
 
+###############################################################################
+###############################################################################
+class Counter (threading.Thread):
+    """ """
+    def __init__ (self):
+        threading.Thread.__init__(self)  # todo: improve?
+        self.counter = 0
+        self._running = False
 
+    def init_values (self, master):
+        self.counter_tk = tk.StringVar(master)  # todo: improve? only one variable?
+
+    def run (self):
+        self._running = True
+        while self._running:
+            self.counter += 1
+            self.counter_tk.set(self.counter)
+            time.sleep(1)
+
+    def stop (self):
+        self._running = False
+
+
+###############################################################################
+###############################################################################
 class ScreenApp (tk.Frame):
     def __init__ (self, master=None):
         super().__init__(master)
 
         self.master = master
+
+        self.font = Font(family="Arial", size=400, weight="bold")
+
         self.screen = tk.Frame(self.master)
-        self.screen.config(bg="green", width=500, height=500)
+        self.screen.config(bg="white", width=self.master.width,
+                                       height=self.master.height)
+
+        counter.init_values(self)
+        counter.start()
+        self.counter = counter.counter_tk
+
+        self.text = tk.Label(self.screen, textvariable=self.counter,
+                             foreground="red", background="white",
+                             font=self.font)
+        self.text.place(relx=.5, rely=.5, anchor="center")
+
         self.screen.grid()
 
 
-
+###############################################################################
+###############################################################################
 class Screen (object):
     """  """
     def __init__ (self):
@@ -52,8 +92,8 @@ class Screen (object):
         self.root.config(cursor='none')
         self.root.resizable(width=False, height=False)
 
-        self.root.width  = 2000
-        self.root.height = 1000
+        self.root.width  = self.root.winfo_screenwidth()
+        self.root.height = self.root.winfo_screenheight()
         self.root.borderwidth = 10
         self.root.geometry("{}x{}+{}+{}".format(self.root.width,
                                                 self.root.height, 0, 0))
@@ -76,14 +116,14 @@ class Screen (object):
         self.root.quit() # TODO: check usage of destroy() and quit()
 
 
-
-
-
 ###############################################################################
 # shutdown_application ########################################################
 def shutdown_application ():
     """called on shutdown; stops all threads"""
     Log("Shutdown.")
+
+    counter.stop()
+    counter.join()
 
     screen.stop()
     sys.exit(0)
@@ -100,9 +140,10 @@ if __name__ == '__main__':
 
     shutdown = Shutdown(shutdown_func=shutdown_application)
 
+    counter = Counter()
+
     screen = Screen()
     screen.run()
-
 
 # eof #
 
