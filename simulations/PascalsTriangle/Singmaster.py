d37f1c93ba95e2fbaa0063938e8ffe4f20bfe83a
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2022-10-01 20:42:42 +0200

    testing with number of iterations

--- simulations/PascalsTriangle/Singmaster.py
+++ simulations/PascalsTriangle/Singmaster.py
@@ -11,7 +11,8 @@ https://en.wikipedia.org/wiki/Singmaster%27s_conjecture
 
 from collections import defaultdict
 
-iterations = 1000
+iterations = 10000
+max_appaerances = 50
 
 def pascal (line):
     next_line = []
@@ -32,14 +33,15 @@ if __name__ == '__main__':
     for i in range(iterations):
         line = pascal(line)
         for number in line:
-            count[number] += 1
+            if count[number] < max_appaerances:
+                count[number] += 1
 
     for number in count.keys():
         appearances[count[number]].append(number)
 
     for i in appearances:
-        if len(appearances[i]) > 20:
-            print(f"{i}: [{len(appearances[i])} items]")
+        if len(appearances[i]) >= max_appaerances:
+            print(f"{i}: [more than {max_appaerances} items]")
         else:
             appearances[i].sort()
             print(f"{i}: {appearances[i]}")
