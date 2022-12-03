282ccd1254507ad16f898c4cbf7889f7ac102724
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2022-12-03 15:17:55 +0100

    sensor 'airout' broken; using fake data now

--- pool/Sensors.py
+++ pool/Sensors.py
@@ -136,7 +136,7 @@ class Sensors (threading.Thread):
         self.ds1820_water = DS1820("/sys/bus/w1/devices/28-030c979462a8/w1_slave")
         self.bme280_box = BME280()
         self.sht31_airin = SHT31(addr=SHT31_BASEADDR)
-        self.sht31_airout = SHT31(addr=SHT31_SECONDARYADDR)
+        # self.sht31_airout = SHT31(addr=SHT31_SECONDARYADDR)
         self.pcf8591 = PCF8591()
 
         self.__data = data
@@ -150,8 +150,10 @@ class Sensors (threading.Thread):
             self.__data.box_humidity = self.bme280_box.read_humidity()
             self.__data.airin_temp = self.sht31_airin.read_temperature()
             self.__data.airin_humidity = self.sht31_airin.read_humidity()
-            self.__data.airout_temp = self.sht31_airout.read_temperature()
-            self.__data.airout_humidity = self.sht31_airout.read_humidity()
+            # self.__data.airout_temp = self.sht31_airout.read_temperature()
+            # self.__data.airout_humidity = self.sht31_airout.read_humidity()
+            self.__data.airout_temp = self.__data.airin_temp
+            self.__data.airout_humidity = 98.88
             self.__data.engine_circulation = self.pcf8591.read(channel=0)
             self.__data.engine_countercurrent = self.pcf8591.read(channel=1)
             self.__data.outdoor_temp = self.ds1820_outdoor.read_temperature()
