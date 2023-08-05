53a939f149c8d2bcc935c69d669ae7b1d6080f4d
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2023-08-05 06:43:25 +0200

    finished implementation of prototype

--- playground/modbus_sdm630.py
+++ playground/modbus_sdm630.py
@@ -7,6 +7,9 @@
 
 """
 prototyping usage of SDM630 via modbus
+
+Sourcecode taken from
+https://jojokorpi.ddns.net/wordpress/index.php/2021/06/01/sdm630-modbus-zaehler-auslesen/
 """
 
 
@@ -18,13 +21,41 @@ sudo pip3 install -U minimalmodbus
 
 """
 
+import time
 import minimalmodbus
 
-instrument = minimalmodbus.Instrument('/dev/ttyUSB1', 1)
+sdm630 = minimalmodbus.Instrument('/dev/ttyUSB0', 1)
+sdm630.serial.baudrate = 9600
+
+input_register = {
+    "Spannung_L1": {
+        "port": 0, "digits": 2, "Unit": "V"},
+    "Spannung_L2": {
+        "port": 2, "digits": 2, "Unit": "V"},
+    "Spannung_L3": {
+        "port": 4, "digits": 2, "Unit": "V"},
+    "Strom_L1": {
+        "port": 6, "digits": 2, "Unit": "A"},
+    "Strom_L2": {
+        "port": 8, "digits": 2, "Unit": "A"},
+    "Strom_L3": {
+        "port": 10, "digits": 2, "Unit": "A"},
+    "aktueller_Gesamtstrom": {
+        "port": 48, "digits": 2, "Unit": "A"},
+    "aktuelle_Gesamtwirkleistung": {
+        "port": 52, "digits": 2, "Unit": "W"}
+}
 
 
+for key in input_register:
+    measurement = key
+    value = sdm630.read_float(functioncode=4,
+                              registeraddress=input_register[key]["port"],
+                              number_of_registers=input_register[key]["digits"])
+    unit = input_register[key]["Unit"]
 
-
+    print(f"{measurement}: {value:.2f} {unit}")
+    time.sleep(0.5)
 
 # eof #
 
