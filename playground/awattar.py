c8984b3c3951835cb7fc86210d1a736f99d5576d
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2023-08-10 22:00:04 +0200

    implemented scheduler

--- playground/awattar.py
+++ playground/awattar.py
@@ -11,18 +11,33 @@ https://www.awattar.at/services/api/
 """
 
 
+"""
+sudo pip3 install schedule
+
+"""
+
 from datetime import datetime
 import json
+from schedule import every, repeat, run_pending
+import time
 from urllib.request import urlopen
 
 
 url = "https://api.awattar.at/v1/marketdata"
-data_json = json.loads(urlopen(url).read())
 
-for hour in range(3):
-    timestamp = datetime.fromtimestamp(int(data_json['data'][hour]['start_timestamp']/1000))
-    value = data_json['data'][hour]['marketprice'] / 10.0
-    print(f"Timestamp: {timestamp}; Value: {value:.2f}")
+
+@repeat(every().hour.at(":01"))
+def update_data():
+    data_json = json.loads(urlopen(url).read())
+    for hour in range(3):
+        timestamp = datetime.fromtimestamp(int(data_json['data'][hour]['start_timestamp']/1000))
+        value = data_json['data'][hour]['marketprice'] / 10.0
+        print(f"Timestamp: {timestamp}; Value: {value:.2f}")
+
+update_data()
+while True:
+    run_pending()
+    time.sleep(1)
 
 # eof #
 
