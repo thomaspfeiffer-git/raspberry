d44e7db9db0d0bacde9e2b8d601ed8a65f348951
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2023-08-11 15:59:25 +0200

    some code improvements

--- playground/awattar.py
+++ playground/awattar.py
@@ -23,32 +23,48 @@ import time
 from urllib.request import urlopen
 
 
-url = "https://api.awattar.at/v1/marketdata"
 
 
-@repeat(every().hour.at(":01"))
-def update_data():
-    data = json.loads(urlopen(url).read())['data']
-    lowest_price = data[0]
-    for hour in data:
-        hour['start_timestamp'] = datetime.fromtimestamp(int(hour['start_timestamp']/1000))
-        hour['end_timestamp'] = datetime.fromtimestamp(int(hour['end_timestamp']/1000))
-        hour['marketprice'] /= 10.0
-        hour['unit'] = "ct/kWh"
+###############################################################################
+###############################################################################
+class Awattar (object):
+    url = "https://api.awattar.at/v1/marketdata"
 
-        if hour['marketprice'] < lowest_price['marketprice']:
-            lowest_price = hour
+    def __init__ (self):
+        self.data = { 'hourly ratings': None,
+                      'lowest price': None }
 
-    print(data)
-    print(f"lowest price: {lowest_price}")
+    @repeat(every().hour.at(":01"))
+    def update_data (self):
+        data = json.loads(urlopen(self.url).read())['data']
+        lowest_price = data[0]
+        for hour in data:
+            hour['start_timestamp'] = datetime.fromtimestamp(int(hour['start_timestamp']/1000))
+            hour['end_timestamp'] = datetime.fromtimestamp(int(hour['end_timestamp']/1000))
+            hour['marketprice'] /= 10.0
+            hour['unit'] = "ct/kWh"
+
+            if hour['marketprice'] < lowest_price['marketprice']:
+                lowest_price = hour
+
+        self.data['hourly ratings'] = data
+        self.data['lowest price'] = lowest_price
 
 
-update_data()
+###############################################################################
+## main #######################################################################
+if __name__ == "__main__":
+    awattar = Awattar()
+    awattar.update_data()
 
-"""
-while True:
-    run_pending()
-    time.sleep(1)
-"""
+    import pprint
+    pprint.pprint(awattar.data)
+
+    """
+    while True:
+        run_pending()
+        time.sleep(1)
+    """
+
 # eof #
 
