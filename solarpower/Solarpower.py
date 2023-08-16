9e16e0383efbb296e41dc19e03db898112db7d46
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2023-08-16 11:15:02 +0200

    finished implementation of csv logging

--- solarpower/Solarpower.py
+++ solarpower/Solarpower.py
@@ -195,24 +195,25 @@ class Receiver (object):
 class CSV (object):
     def __init__ (self):
         self.fieldnames = ["Timestamp", Main_Meter.field_P, Solar_Meter.field_P]
+        self.today = 0
         self.new_file()
 
     def new_file (self):
-        self.filename = f"solarpower_{datetime.now().strftime('%Y%m%d')}.csv"
-        if not os.path.isfile(self.filename):
-            with open(self.filename, 'w', newline='') as file:
-                writer = csv.DictWriter(file, fieldnames=self.fieldnames)
-                writer.writeheader()
+        if self.today != datetime.now().day:   # new day? --> start with new file
+            self.today = datetime.now().day
+            self.filename = f"csv/solarpower_{datetime.now().strftime('%Y%m%d')}.csv"
 
-    def get_item_from_rrd (self, rrd_template, rrd_data, item):
+            if not os.path.isfile(self.filename):
+                with open(self.filename, 'w', newline='') as file:
+                     writer = csv.DictWriter(file, fieldnames=self.fieldnames)
+                     writer.writeheader()
+
+    @staticmethod
+    def get_item_from_rrd (rrd_template, rrd_data, item):
         return { item: rrd_data.split(':')[rrd_template.split(':').index(item)+1] }
 
     def write (self, rrd_template, rrd_data):
-        """ python 3.9
-        csv_data = { "Timestamp": datetime.now().strftime("%Y%m%d %H:%M:%S") } |         \
-                   self.get_item_from_rrd(rrd_template, rrd_data, Main_Meter.field_P) |  \
-                   self.get_item_from_rrd(rrd_template, rrd_data, Solar_Meter.field_P)
-        """
+        self.new_file()
         csv_data = { "Timestamp": datetime.now().strftime("%Y%m%d %H:%M:%S") }
         csv_data.update(self.get_item_from_rrd(rrd_template, rrd_data, Main_Meter.field_P))
         csv_data.update(self.get_item_from_rrd(rrd_template, rrd_data, Solar_Meter.field_P))
