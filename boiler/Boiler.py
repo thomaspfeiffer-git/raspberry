9b7e28056e7b66b12715182d39de9d894c9a38a0
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2024-07-13 14:43:12 +0200

    implemented scheduling

--- boiler/Boiler.py
+++ boiler/Boiler.py
@@ -19,6 +19,7 @@
 import argparse
 from datetime import datetime, timedelta
 from gpiozero import Button, LED
+import schedule
 import sys
 import time
 
@@ -62,20 +63,20 @@ class Relais (object):
 
     def on (self):
         self.__relais.on()
-        MyLogging("Switched boiler on.")
+        Log("Switched boiler on.")
 
     def off (self):
         self.__relais.off()
-        MyLogging("Switched boiler off.")
+        Log("Switched boiler off.")
 
 
 ###############################################################################
 # Shutdown stuff ##############################################################
 def shutdown_application ():
     """cleanup stuff"""
-    MyLogging.Log("Stopping application")
+    Log("Stopping application.")
     boiler.off()
-    MyLogging("Application stopped")
+    Log("Application stopped.")
     sys.exit(0)
 
 
@@ -83,27 +84,41 @@ def shutdown_application ():
 ## main #######################################################################
 if __name__ == "__main__":
     shutdown_application = Shutdown(shutdown_func=shutdown_application)
-    MyLogging = MyLogging()
+    Log = MyLogging()
     boiler = Relais(CONFIG.Boiler.pin)
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--duration", "-d", nargs=1, type=int, default=[118],
-                        help="Duration in minutes the boiler shall heat.")
     parser.add_argument("--start", "-s", nargs=1, type=int,
                         help="Time of day (hour) the boiler shall be switched on.")
+    parser.add_argument("--duration", "-d", nargs=1, type=int, default=[118],
+                        help="Duration in minutes the boiler shall heat.")
     args = parser.parse_args()
-    MyLogging(args)
 
-
-    """
+    now = datetime.now()
     if args.start == None:
-        boiler.on()
-        #schedule boiler.off()
-    """
-    boiler.on()
-    time.sleep(5)
-    boiler.off()
+        start_time = now + timedelta(minutes=1)
+    else:
+        start_time = now.replace(hour=args.start[0], minute=1)
+    if start_time < now:
+        raise ValueError("Start time must be in the future.")
 
+    stop_time = start_time + timedelta(minutes=args.duration[0])
+    exit_time = stop_time + timedelta(minutes=1)
+
+    start_time = f"{start_time.hour}:{start_time.minute:02d}"
+    Log(f"Set start time to {start_time}.")
+    stop_time = f"{stop_time.hour}:{stop_time.minute:02d}"
+    Log(f"Set stop time to {stop_time}.")
+    exit_time = f"{exit_time.hour}:{exit_time.minute:02d}"
+    Log(f"Set exit time to {exit_time}.")
+
+    schedule.every().day.at(start_time).do(boiler.on)
+    schedule.every().day.at(stop_time).do(boiler.off)
+    schedule.every().day.at(exit_time).do(lambda: shutdown_application.shutdown(0,0))
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
 
 # eof #
 
