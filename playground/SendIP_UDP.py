d86dcf35094783ef6315ea6d0bfc125e840dcaf6
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2024-08-27 21:15:47 +0200

    initial checkin

---
+++ playground/SendIP_UDP.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3 -u
+# -*- coding: utf-8 -*-
+###############################################################################
+# TODO                                                                        #
+# (c) https://github.com/thomaspfeiffer-git 2024                              #
+###############################################################################
+
+"""
+"""
+
+"""
+###### Usage ######
+### Sensor
+nohup ./SendIP_UDP.py --sensor 2>&1 > sendip_udp.log &
+
+### Receiver
+nohup ./SendIP_UDP.py --receiver 2>&1 > sendip_udp.log &
+"""
+
+
+import argparse
+import os
+import sys
+import time
+
+sys.path.append("../libs/")
+from Logging import Log
+from Shutdown import Shutdown
+import UDP
+
+
+CREDENTIALS = os.path.expanduser("SendIP_UDP.cred")
+
+UPDATE_INTERVAL_SEND_DATA_UDP = 10            # interval for sending data to external server
+
+
+
+###############################################################################
+# Receiver ####################################################################
+class Receiver (object):
+    def __init__ (self):
+        self.udp = UDP.Receiver(CREDENTIALS)
+
+    def start (self):
+        while True:
+            payload = self.udp.receive()
+            Log(f"{payload}")
+
+
+###############################################################################
+# Shutdown stuff ##############################################################
+def shutdown_application ():
+    """cleanup stuff"""
+    Log("Stopping application")
+    Log("Application stopped")
+    sys.exit(0)
+
+
+###############################################################################
+## main #######################################################################
+if __name__ == "__main__":
+    shutdown_application = Shutdown(shutdown_func=shutdown_application)
+
+    parser = argparse.ArgumentParser()
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument("--sender", help="sends IP address to udp server", action="store_true")
+    group.add_argument("--receiver", help="receives IP address from sender", action="store_true")
+    args = parser.parse_args()
+
+    if args.receiver:
+        pass
+
+    if args.sender:
+        pass
+
+# eof #
+
