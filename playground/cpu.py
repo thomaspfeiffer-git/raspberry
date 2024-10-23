39f3e5dde4ddc78a010bac3be0a1a04590c2bc9a
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2024-10-23 20:30:53 +0200

    testing platform.platform()

--- playground/cpu.py
+++ playground/cpu.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+import platform
 import sys
 sys.path.append('../libs')
 
 from sensors.CPU import CPU
+
+
+print(platform.platform())
 
 cpu = CPU()
 print(cpu.read_temperature())
