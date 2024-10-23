db51352590410c847f982ed92f7fa826a2422664
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2024-10-23 21:21:46 +0200

    adding Board by adafruit_platformdetect.Detector

--- playground/cpu.py
+++ playground/cpu.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-import platform
+
+####
+# sudo pip3 install --break-system-packages Adafruit-PlatformDetect
+
+from adafruit_platformdetect import Detector
 import sys
 sys.path.append('../libs')
 
 from sensors.CPU import CPU
+
+detector = Detector()
+print(f"Board: {detector.board.id}")
 
 cpu = CPU()
 print(cpu.read_temperature())
