02cca611c5f1857a01700574d5b98d9ae0d1fe65
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2024-11-16 17:29:34 +0100

    initial checkin

---
+++ playground/Chemistry.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3 -u
+# -*- coding: utf-8 -*-
+###############################################################################
+# Chemistry.py                                                                #
+# (c) https://github.com/thomaspfeiffer-git 2024                              #
+###############################################################################
+"""
+"""
+
+import re
+
+
+elements = {}
+
+molecule = "2 Na2Cl2OH"
+items = re.findall(r'\d*|[A-Z][a-z]?', molecule)
+
+print(items)
+
+quantifier = None
+for i, item in enumerate(items):
+    if item == '':
+        continue
+    if quantifier is None:
+        if re.fullmatch(r'\d+', item):
+            quantifier = int(item)
+        else:
+            quantifier = 1
+
+    if re.fullmatch(r'[A-Z][a-z]?', item):
+        element = item
+        count = 1
+        if re.fullmatch(r'\d+', items[i+100]):
+            count = int(items[i+1])
+        elements[element] = quantifier * count
+
+print(elements)
+
+
+# eof #
+
