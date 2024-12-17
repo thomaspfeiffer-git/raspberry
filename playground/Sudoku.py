c63f394987df60c03cc61150c49b90e6b1037ed0
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2024-12-17 13:26:43 +0100

    finished implementation

--- playground/Sudoku.py
+++ playground/Sudoku.py
@@ -9,6 +9,7 @@
 https://mathe-gut-erklaert.de/pdfs/000_Sudoku.pdf
 """
 
+"""
 board = [0, 0, 0,  0, 0, 0,  0, 0, 8,   #  0 ..  8
          0, 2, 0,  0, 5, 0,  7, 6, 0,   #  9 .. 17
          0, 6, 0,  0, 0, 0,  0, 0, 3,   # 18 .. 26
@@ -20,6 +21,18 @@ board = [0, 0, 0,  0, 0, 0,  0, 0, 8,   #  0 ..  8
          0, 0, 0,  6, 0, 0,  0, 0, 0,   # 54 .. 62
          8, 0, 0,  0, 0, 0,  0, 0, 0,   # 63 .. 71
          1, 0, 0,  2, 7, 0,  0, 4, 0]   # 72 .. 80
+"""
+
+board = [3, 0, 6, 5, 0, 8, 4, 0, 0,
+         5, 2, 0, 0, 0, 0, 0, 0, 0,
+         0, 8, 7, 0, 0, 0, 0, 3, 1,
+         0, 0, 3, 0, 1, 0, 0, 8, 0,
+         9, 0, 0, 8, 6, 3, 0, 0, 5,
+         0, 5, 0, 0, 9, 0, 6, 0, 0,
+         1, 3, 0, 0, 0, 0, 2, 5, 0,
+         0, 0, 0, 0, 0, 0, 0, 7, 4,
+         0, 0, 5, 2, 0, 6, 3, 0, 0]
+
 
 from collections import Counter
 import sys
@@ -59,8 +72,8 @@ def allowedDigitsCol (index):
 
 def allowedDigitsBox (index):
     allowed = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    box_x = getCol(i) // 3 * 3    # left upper corner in box
-    box_y = getRow(i) // 3 * 3
+    box_x = getCol(index) // 3 * 3    # left upper corner in box
+    box_y = getRow(index) // 3 * 3
 
     for y in range(box_y, box_y+3):
         for x in range(y*9 + box_x, y*9 + box_x+3):
@@ -70,39 +83,57 @@ def allowedDigitsBox (index):
 
 def allowedDigits (index):
     allowed = []
-    for digit, count in dict(Counter(allowedDigitsRow(i) + allowedDigitsCol(i) + allowedDigitsBox(i))).items():
+    for digit, count in dict(Counter(allowedDigitsRow(index) +
+                                     allowedDigitsCol(index) +
+                                     allowedDigitsBox(index))).items():
         if count == 3:
             allowed.append(digit)
-
     return allowed
 
 
 ###############################################################################
 # solve #######################################################################
 def solve (index):
-    if index >= 81:
-        return true
+    DONE = 1
+    END_PATH = 2
+
+    def nextFreeCell (index):
+        for i in range(81):
+            if board[i] == 0:
+                return i
+        return None
+
+    index = nextFreeCell(index)
+    if index is None:
+        return DONE
 
     allowed = allowedDigits(index)
     if len(allowed) == 0:
-        pass
-        # Zeile/Spalte/Box fertig?
-        # oder fehler?
+        return END_PATH
 
-    if len(allowed) == 1:
-        pass
-        # match
+    for i in allowed:
+        board[index] = i
+        if solve(index) == DONE:
+            return DONE
+        board[index] = 0
+
+    return END_PATH
 
 
-    for i in allowedDigits(index):
-        pass
-
+###############################################################################
+# printBoard ##################################################################
+def printBoard (board):
+    for line in range(9):
+        print(f"{board[line*9:line*9+3]} {board[line*9+3:line*9+6]} {board[line*9+6:line*9+9]}")
+        if (line+1) % 3 == 0:
+            print()
 
 
 ###############################################################################
 ## main #######################################################################
 if __name__ == "__main__":
     solve(0)
+    printBoard(board)
 
 # eof #
 
