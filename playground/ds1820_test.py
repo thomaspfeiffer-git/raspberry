c1de88ea80a9ba0585b9cc926de43c8e3edc7c48
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2025-04-27 07:42:23 +0200

    changed bus id

--- playground/ds1820_test.py
+++ playground/ds1820_test.py
@@ -9,12 +9,12 @@ sys.path.append('../libs/sensors')
 from Logging import Log
 from DS1820 import DS1820
 
-ds1820_1 = DS1820("/sys/bus/w1/devices/28-030c979407ac/w1_slave")
+ds1820_1 = DS1820("/sys/bus/w1/devices/28-000006de80e2/w1_slave")
 # ds1820_2 = DS1820("/sys/bus/w1/devices/28-000008386a83/w1_slave")
 
 while True:
     Log("1: {}".format(ds1820_1.read_temperature()))
     # Log("2: {}".format(ds1820_2.read_temperature()))
-    time.sleep(10)
+    time.sleep(1)
 
 
