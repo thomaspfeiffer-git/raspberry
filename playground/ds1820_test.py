259c5301bb15f73a5bc9bdda96fa7a4f5cea5c89
thomaspfeiffer-git <thomas.pfeiffer@smtp.at>
2025-05-13 21:22:43 +0200

    major refactoring

--- playground/ds1820_test.py
+++ playground/ds1820_test.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python3 -u
+# -*- coding: utf-8 -*-
+###############################################################################
+# ds1820_test.py                                                              #
+# (c) https://github.com/thomaspfeiffer-git/raspberry, 2025                   #
+###############################################################################
 
+"""
+Test application for temperature sensor DS1820.
+"""
+
+### Usage ###
+# ./ds1820_test.py sensor id
+# ./ds1820_test.py 28-030297946b71
+
+
+import argparse
 import sys
 import time
 
 sys.path.append('../libs')
-sys.path.append('../libs/sensors')
 
 from Logging import Log
-from DS1820 import DS1820
+from sensors.DS1820 import DS1820
 
-ID = "28-030297946b71"
+parser = argparse.ArgumentParser()
+parser.add_argument("sensor id")
+ID = vars(parser.parse_args())['sensor id']
 
+Log(f"Reading temperature from sensor {ID}.")
 ds1820 = DS1820(f"/sys/bus/w1/devices/{ID}/w1_slave")
 
 while True:
     Log(f"{ds1820.read_temperature()} °C")
-    time.sleep(1)
+    for _ in range(100):  # interruptible sleep
+        time.sleep(0.01)
 
 # eof #
 
